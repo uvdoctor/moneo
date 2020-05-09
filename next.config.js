60f0978898c4d14c1eae48698405d6cc799eca58
Umang Doctor <emailumangdoctor@gmail.com>
2020-05-09 19:09:44 +0530

    Added PWA

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const withPWA = require('next-pwa');
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = withPWA({
+    pwa: {
+        disable: !isProd,
+        dest: 'public'
+    }
+});
