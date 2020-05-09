c68ab82f01a606f82529d2f40f58c49d69240e2c
Umang Doctor <emailumangdoctor@gmail.com>
2020-05-09 23:01:48 +0530

    Typescript configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 const withPWA = require('next-pwa')
-
+const withTypescript = require('@zeit/next-typescript')
 const isProd = process.env.NODE_ENV === "production"
 
+module.exports = withTypescript()
 module.exports = withPWA({
     pwa: {
         disable: !isProd,
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/index.css'
 
-export default function DDApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+    return (
+        <div>
+            <p className="font-black text-4xl">Your Financial Analyst</p>
+        </div>
+    );
+
+    //return <Component {...pageProps} />
 }
