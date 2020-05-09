1429ef5d71fba4b609f4065eac0a5b919128f701
Umang Doctor <emailumangdoctor@gmail.com>
2020-05-09 19:48:32 +0530

    Fixed purgecss error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-const withPWA = require('next-pwa');
+const withPWA = require('next-pwa')
+const withCss = require('@zeit/next-css')
+const withPurgeCss = require('next-purgecss')
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === "production"
+
+module.exports = withCss(withPurgeCss())
 
 module.exports = withPWA({
     pwa: {
