41d20ad34985c25593cf47c1379972fdbc47d493
Umang Doctor <emailumangdoctor@gmail.com>
2020-06-14 00:11:39 +0530

    Fixed typescript errors

--- src/components/goals/linechart.tsx
+++ src/components/goals/linechart.tsx
@@ -14,9 +14,12 @@ export function LineChart(props: LineChartProps) {
 
     return (
         <div className="mt-4 mb-4">
+            {/*@ts-ignore*/}
             <Plot layout={{title: props.title}}
             data={[
+                //@ts-ignore: Object is possible undefined
                 {type: 'scatter', mode: 'lines+markers', x: props.data[0].values.x, y: props.data[0].values.y, name: props.data[0].name}, 
+                //@ts-ignore: Object is possible undefined
                 {type: 'scatter', mode: 'lines+markers', x: props.data[1].values.x, y: props.data[1].values.y, name: props.data[1].name} 
             ]} 
             config={{responsive: true, editable: false, displayModeBar: false, scrollZoom: true}} />
