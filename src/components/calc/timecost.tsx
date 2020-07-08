dfa2d5344868fe4ef5d9e13cb71826abd62fd3f9
Umang Doctor <emailumangdoctor@gmail.com>
2020-07-08 17:49:40 +0530

    Fixed

--- src/components/calc/timecost.tsx
+++ src/components/calc/timecost.tsx
@@ -55,7 +55,7 @@ export default function TimeCost(props: TimeCostProps) {
             }
             right={
                 <NumberInput name="savings" pre="Save" note="Every Year" currency={props.currency} value={annualSavings}
-                    changeHandler={setAnnualSavings} min={1000} max={200000} step={1000} rangeFactor={getRangeFactor} />
+                    changeHandler={setAnnualSavings} min={1000} max={200000} step={1000} rangeFactor={props.rangeFactor} />
             }  showOnLoad={true} />
 
     );
