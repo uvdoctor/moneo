942741a826b936635ccdc6288fe1c1eea4946bb0
Umang Doctor <emailumangdoctor@gmail.com>
2020-08-10 13:46:17 +0530

    Updated index test

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 import Index from '../src/pages/index';
+
+afterEach(cleanup)
 
 describe("IndexPage", () => {
     describe("when index page loaded", () => {
       it("has login button", async () => {
         const container = await render(<Index />);
-        expect(container.findByText("LOGIN")).toMatchSnapshot()
+        expect(container).toMatchSnapshot()
       });
     });
   });
