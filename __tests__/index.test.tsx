ed82bd10077f7c797fc1c5054e91acea4295ab3c
Umang Doctor <emailumangdoctor@gmail.com>
2020-08-10 13:34:13 +0530

    Updated jest config

---
+++ __tests__/index.test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from "@testing-library/react";
+import Index from '../src/pages/index';
+
+describe("IndexPage", () => {
+    describe("when index page loaded", () => {
+      it("has login button", async () => {
+        const container = await render(<Index />);
+        expect(container.findByText("LOGIN")).toMatchSnapshot()
+      });
+    });
+  });
--- jest.config.js
+++ jest.config.js
@@ -23,5 +23,14 @@ module.exports = {
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
 
   // Module file extensions for importing
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+
+  globals: {
+    // we must specify a custom tsconfig for tests because we need the typescript transform
+    // to transform jsx into js rather than leaving it jsx such as the next build requires.  you
+    // can see this setting in tsconfig.jest.json -> "jsx": "react"
+    "ts-jest": {
+      tsConfig: "tsconfig.jest.json"
+    }
+  }
 };
