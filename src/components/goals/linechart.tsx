c6fcf86a9dbd9ed3a99203a02450d931c91ea23e
Umang Doctor <emailumangdoctor@gmail.com>
2020-08-12 10:48:56 +0530

    Updated chart config

--- src/components/goals/linechart.tsx
+++ src/components/goals/linechart.tsx
@@ -39,7 +39,7 @@ export default function LineChart(props: LineChartProps) {
             <Plot layout={layout} useResizeHandler={true}
                 style={{ width: "100%", height: "100%", minHeight: "450px" }}
                 data={[track]}
-                config={{ responsive: true, editable: false, displayModeBar: false }} />
+                config={{ responsive: true, editable: false, displayModeBar: false, scrollZoom: true }} />
         </div>
     )
 }
