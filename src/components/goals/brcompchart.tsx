665e0f09a0edcfdb85c5381ad5d013cbebb615b4
Umang Doctor <emailumangdoctor@gmail.com>
2020-08-13 19:41:56 +0530

    Updated chart

--- src/components/goals/brcompchart.tsx
+++ src/components/goals/brcompchart.tsx
@@ -17,12 +17,13 @@ export function BRCompChart(props: BRCompChartProps) {
     const customWidth = props.data[0].values.x.length * 30
 
     return (
-        <div className="w-full flex justify-center overflow-x-scroll scrolling-touch hide-scrollbar">
+        <div className="w-full h-full flex justify-center">
             {/*@ts-ignore*/}
             <Plot layout={{
                 font:{family: "'Quicksand', sans-serif", color: "#4a5568", size: 15}, 
-                width: customWidth < 600 ? 600 : customWidth,
+                width: customWidth < 800 ? 800 : customWidth,
                 aspectratio: 0,
+                autosize: true,
                 title: {x:0.05, text:props.title, font:{size: 20}}, 
                 xaxis: {type:'category', fixedrange: true, showgrid: false, range: [1, props.data[0].values.x.length]},
                 yaxis: {fixedrange: true, tickformat: ',', showgrid: false},
@@ -33,6 +34,7 @@ export function BRCompChart(props: BRCompChartProps) {
                 }, margin:{t:40, r:10}
             }} 
             style={{width: "100%", height:"100%", minHeight: "400px"}}
+            useResizeHandler={true}
             data={[
                 //@ts-ignore: Object is possible undefined
                 {type: 'scatter', fill: 'tozeroy', mode: 'none', x: props.data[0].values.x, y: props.data[0].values.y, 
@@ -40,7 +42,7 @@ export function BRCompChart(props: BRCompChartProps) {
                 //@ts-ignore: Object is possible undefined
                 {type: 'scatter', fill: 'tonexty', mode:'none', x: props.data[1].values.x, y: props.data[1].values.y, name: props.data[1].name} 
             ]} 
-            config={{displayModeBar: false, scrollZoom: true}} 
+            config={{responsive: true, displayModeBar: false, scrollZoom: true}} 
              />
         </div>
     )
