a3ea255d7265811a2c1995ffcb3906ae4a5d4b69
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-01 09:26:50 +0530

    Implemented header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,9 +7,9 @@ import { AwesomeButton } from "react-awesome-button";
 
 const Header = () => {
   return (
-    <nav className="md:text-lg lg:text-xl fixed flex w-full bg-white items-center justify-between flex-wrap py-1 top-0 animated items-center cursor font-bold">
+    <nav className="w-1/12 md:text-lg lg:text-xl fixed flex w-full bg-white items-center justify-between flex-wrap py-1 top-0 cursor font-bold">
       <Logo />
-      <div className="flex items-center">
+      <div className="w-11/12 flex items-center justify-around">
         <div className="hoverable relative inline-block hover:text-green-600 cursor">
           <label>About</label>
           <ul className="px-2 md:px-4 py-1 dropdown-menu absolute shadow-xl bg-green-600 text-white font-normal text-base md:text-lg">
@@ -18,22 +18,20 @@ const Header = () => {
             <li className="py-1 hover:text-black">Company</li>
           </ul>
         </div>
-        <div className="ml-3 md:ml-12 hover:text-green-600">
+        <div className="hover:text-green-600">
           <Link href="#">
             <a>Learn</a>
           </Link>
         </div>
-        <div className="ml-3 sm:ml-8 md:ml-12 flex items-center">
-          <div className="hover:text-green-600 pr-5">
-            <Link href={ROUTES.CALCULATE}>
-              <a>Calculate</a>
-            </Link>
-          </div>
-          <div className="hover:text-green-600 mr-1">
-            <Link href={ROUTES.DASHBOARD}>
-              <a>Login</a>
-            </Link>
-          </div>
+        <div className="hover:text-green-600">
+          <Link href={ROUTES.CALCULATE}>
+            <a>Calculate</a>
+          </Link>
+        </div>
+        <div className="hover:text-green-600">
+          <Link href={ROUTES.DASHBOARD}>
+            <a>Login</a>
+          </Link>
         </div>
       </div>
     </nav>
