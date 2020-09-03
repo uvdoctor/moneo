4cec8dd2c45830a5902ed809fae168ce4cec0568
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-04 02:38:01 +0530

    Changed scroll behaviour

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,23 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Features from "./features/features";
 import Landing from "./landing";
-import { useScroll, useFullScreenBrowser } from "react-browser-hooks";
+import { useScroll } from "react-browser-hooks";
 import Header from "./header";
-import { getLandingPageHeight } from "./utils";
 export default function Main() {
   const { top } = useScroll();
-  const fsb = useFullScreenBrowser();
-  const [landingHeight, setLandingHeight] = useState<number>(
-    getLandingPageHeight(fsb)
-  );
-
-  useEffect(() => {
-    setLandingHeight(getLandingPageHeight(fsb));
-  }, [fsb.info.innerWidth]);
 
   return (
     <div className="overflow-x-none overflow-y-auto">
-      {top > landingHeight && <Header />}
+      {top > 0 && <Header />}
       <Landing />
       <Features />
     </div>
