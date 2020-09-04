d8818fd57aa132abe37352da4e384f406ae037be
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-04 15:14:55 +0530

    fixed about menu

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -3,19 +3,23 @@ import React from "react";
 import { COLORS, ROUTES } from "../CONSTANTS";
 import ExpandCollapse from "./form/expandcollapse";
 
-export default function Menu() {
+interface MenuProps {
+  fixed?: boolean
+}
+
+export default function Menu({fixed}: MenuProps) {
   return (
     <div className="w-3/4 md:w-2/3 lg:w-1/2 xl:w-1/3 flex justify-between">
       <div className="w-full flex justify-around">
-        <div className="flex flex-col relative inline-block">
+        <div className="flex flex-col relative">
           <ExpandCollapse
             title="About"
             hoverColor="text-green-primary"
-            defaultSVGColor={COLORS.SILVER}
+            defaultSVGColor={fixed ? COLORS.DEFAULT : COLORS.SILVER}
             hoverSVGColor={COLORS.GREEN}
           >
             <ul
-              className={`inline-block px-2 md:px-4 py-1 absolute shadow-xl`}
+              className={`${fixed && 'bg-white'} mt-32 pt-8 px-2 md:px-4 py-1 absolute shadow-xl`}
             >
               <li className="py-1 hover:text-green-primary">Features</li>
               <li className="py-1 hover:text-green-primary">Price</li>
