71cd24ad3e49e48941fbf1e489060e9818a428a8
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-06 19:56:50 +0530

    Calculators page

--- src/components/calc/calculators.tsx
+++ src/components/calc/calculators.tsx
@@ -33,7 +33,7 @@ export default function Calculators({
     GREEN: "#9ae6b4",
   };
 
-  const calcItems = [
+  const calcItems1 = [
     {
       title: "Financial Freedom",
       svg: <SVGFreedom />,
@@ -50,20 +50,23 @@ export default function Calculators({
       title: "Buy v/s Rent",
       svg: <SVGScale selected />,
       questions: {
-        'Which is Cheaper?': <SVGBalance />,
-        'For how many Years?': <SVGHourGlass />
+        "Which is Cheaper?": <SVGBalance />,
+        "For how many Years?": <SVGHourGlass />,
       },
       color: LIGHT_COLORS.BROWN,
       link: ROUTES.DASHBOARD,
       img: "images/step2.png",
-      video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
+      video: `https://vimeo.com/455216214`,
     },
+  ];
+
+  const calcItems2 = [
     {
       title: "Mortgage Loan",
       svg: <SVGLoan selected />,
       questions: {
-        'Pay by Self or Loan?': <SVGBalance />,
-        'How much Total Interest?': <SVGMoneyBag />
+        "Pay by Self or Loan?": <SVGBalance />,
+        "How much Total Interest?": <SVGMoneyBag />,
       },
       color: LIGHT_COLORS.ORANGE,
       link: ROUTES.DASHBOARD,
@@ -74,52 +77,94 @@ export default function Calculators({
       title: "Education Loan",
       svg: <SVGLoan selected />,
       questions: {
-        'Pay by Self or Loan?': <SVGBalance />,
-        'How much Total Interest?': <SVGMoneyBag />
+        "Pay by Self or Loan?": <SVGBalance />,
+        "How much Total Interest?": <SVGMoneyBag />,
       },
       color: LIGHT_COLORS.PINK,
       link: ROUTES.DASHBOARD,
       img: "images/step3.png",
       video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
-    }
-  ];
+    },
+  ]
 
   return (
     <div className="w-full flex flex-wrap items-start justify-around pb-1 text-white">
-      {calcItems.map((item) => (
-        <Section
-          title={item.title}
-          titleSVG={item.svg}
-          left={
-            <div
-              className={`flex ${
-                insideMenu
-                  ? "flex-col"
-                  : "flex-wrap items-center justify-around"
-              }`}
-            >
-              {Object.keys(
-                //@ts-ignore
-                item.questions
-              ).map((key) => (
-                <div className="flex items-center mb-1">
-                  {
-                    //@ts-ignore
-                    item.questions[key]
-                  }
-                  <span className="ml-1">{key}</span>
-                </div>
-              ))}
-            </div>
-          }
-          hasResult
-          insideForm={insideForm}
-          insideMenu={insideMenu}
-          color={item.color}
-          imgSrc={item.img}
-          videoSrc={item.video}
-        />
-      ))}
+      <div className="flex flex-col text-default">
+        <div className="w-full text-center h-24">
+        <p>Calculators</p>
+          <p>Estimates Only. No Advice.</p>
+        </div>
+        {calcItems2.map((item) => (
+          <Section
+            title={item.title}
+            titleSVG={item.svg}
+            left={
+              <div
+                className={`flex ${
+                  insideMenu
+                    ? "flex-col"
+                    : "flex-wrap items-center justify-around"
+                }`}
+              >
+                {Object.keys(
+                  //@ts-ignore
+                  item.questions
+                ).map((key) => (
+                  <div className="flex items-center mb-1">
+                    {
+                      //@ts-ignore
+                      item.questions[key]
+                    }
+                    <span className="ml-1">{key}</span>
+                  </div>
+                ))}
+              </div>
+            }
+            hasResult
+            insideForm={insideForm}
+            insideMenu={insideMenu}
+            color={item.color}
+            imgSrc={item.img}
+            videoSrc={item.video}
+          />
+        ))}
+      </div>
+      <div className="flex flex-col">
+        {calcItems1.map((item) => (
+          <Section
+            title={item.title}
+            titleSVG={item.svg}
+            left={
+              <div
+                className={`flex ${
+                  insideMenu
+                    ? "flex-col"
+                    : "flex-wrap items-center justify-around"
+                }`}
+              >
+                {Object.keys(
+                  //@ts-ignore
+                  item.questions
+                ).map((key) => (
+                  <div className="flex items-center mb-1">
+                    {
+                      //@ts-ignore
+                      item.questions[key]
+                    }
+                    <span className="ml-1">{key}</span>
+                  </div>
+                ))}
+              </div>
+            }
+            hasResult
+            insideForm={insideForm}
+            insideMenu={insideMenu}
+            color={item.color}
+            imgSrc={item.img}
+            videoSrc={item.video}
+          />
+        ))}
+      </div>
     </div>
   );
 }
