2fe3ded84366866068f6c14963d82ec8b758ff23
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-08 15:01:01 +0530

    Fixed calc layout

--- src/components/calc/calculators.tsx
+++ src/components/calc/calculators.tsx
@@ -11,27 +11,27 @@ interface CalculatorsProps {
 
 export default function Calculators({ insideMenu }: CalculatorsProps) {
   const LIGHT_COLORS = {
-    RED: "#EEA894",
+    RED: "#FEB2B2",
     ORANGE: "#fbd38d",
-    BLUE: "#90cdf4",
-    TEAL: "#81e6d9",
+    BLUE: "#BEE3F8",
+    TEAL: "#B2F5EA",
     INDIGO: "#a3bffa",
     YELLOW: "#faf089",
     GRAY: "#cbd5e0",
-    PINK: "#f687b3",
+    PINK: "#FBB6CE",
     BROWN: "#D3B193",
     //GRAY: "#EEEBEA",
     //GREEN: "DBEAE9"
-    GREEN: "#9ae6b4",
+    GREEN: "#F0FFF4",
+    PURPLE: "#E9D8FD"
   };
 
   const calcItems = [
     {
       title1: "Financial",
       title2: "Freedom",
-      svg: <SVGFreedom />,
-      desc: "How much Money is Needed? When will I achieve it?",
-      color: LIGHT_COLORS.RED,
+      desc: "How much Money is Needed?",
+      color: LIGHT_COLORS.GRAY,
       link: ROUTES.DASHBOARD,
       img: "images/step1.png",
       video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
@@ -39,8 +39,7 @@ export default function Calculators({ insideMenu }: CalculatorsProps) {
     {
       title1: "Buy v/s",
       title2: "Rent",
-      svg: <SVGScale selected />,
-      desc: "Is it cheaper to Buy or Rent? Till When?",
+      desc: "Is it cheaper to Buy or Rent?",
       color: LIGHT_COLORS.BROWN,
       link: ROUTES.DASHBOARD,
       img: "images/step2.png",
@@ -49,8 +48,7 @@ export default function Calculators({ insideMenu }: CalculatorsProps) {
     {
       title1: "Mortgage",
       title2: "Loan",
-      svg: <SVGLoan selected />,
-      desc: "Should I pay by self or loan? How much total interest will I pay?",
+      desc: "Should I pay by self or loan?",
       color: LIGHT_COLORS.INDIGO,
       link: ROUTES.DASHBOARD,
       img: "images/step3.png",
@@ -59,8 +57,7 @@ export default function Calculators({ insideMenu }: CalculatorsProps) {
     {
       title1: "Education",
       title2: "Loan",
-      svg: <SVGLoan selected />,
-      desc: "Should I pay by self or loan? How much total interest will I pay?",
+      desc: "Should I pay by self or loan?",
       color: LIGHT_COLORS.ORANGE,
       link: ROUTES.DASHBOARD,
       img: "images/step3.png",
@@ -69,9 +66,8 @@ export default function Calculators({ insideMenu }: CalculatorsProps) {
     {
       title1: "Debt",
       title2: "Buster",
-      svg: <SVGLoan selected />,
-      desc: "Should I pay by self or loan? How much total interest will I pay?",
-      color: LIGHT_COLORS.TEAL,
+      desc: "Should I pay by self or loan?",
+      color: LIGHT_COLORS.PINK,
       link: ROUTES.DASHBOARD,
       img: "images/step3.png",
       video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
@@ -79,9 +75,8 @@ export default function Calculators({ insideMenu }: CalculatorsProps) {
     {
       title1: "Credit",
       title2: "Impact",
-      svg: <SVGLoan selected />,
       desc: "How much will be credit score impact if I do not pay my bills?",
-      color: LIGHT_COLORS.GRAY,
+      color: LIGHT_COLORS.RED,
       link: ROUTES.DASHBOARD,
       img: "images/step3.png",
       video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
