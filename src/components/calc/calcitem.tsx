7969195112db3277a0af6fd9dda00805b4d4ae15
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-08 13:12:46 +0530

    Updated calc layout

---
+++ src/components/calc/calcitem.tsx
@@ -0,0 +1,79 @@
+import Link from "next/link";
+import React, { Fragment, useState } from "react";
+import SVGPlay from "../svgplay";
+import SVGStop from "../svgstop";
+import VideoPlayer from "../videoplayer";
+
+interface CalcItemProps {
+  item: any;
+  insideMenu?: boolean;
+}
+
+export default function CalcItem({ item, insideMenu }: CalcItemProps) {
+  const [videoUrl, setVideoUrl] = useState<string>("");
+  const [showDesc, setShowDesc] = useState<boolean>(false);
+
+  return (
+    <div
+      className={`w-full flex flex-col text-white cursor-pointer ${
+        insideMenu ? "max-w-xs" : "max-w-sm lg:max-w-md xl:max-w-lg shadow-xl"
+      } rounded-lg m-1`}
+      style={{
+        backgroundColor: item.color,
+        minHeight: !insideMenu ? "24rem" : "6rem",
+      }}
+      onMouseEnter={() => setShowDesc(true)}
+      onMouseLeave={() => setShowDesc(false)}
+    >
+        <Link href={item.link}>
+          <a>
+            <div className="flex w-full"
+            style={{ minHeight: !insideMenu ? "9rem" : "6rem" }}>
+              <div
+                className={`${
+                  insideMenu ? "w-full" : "w-1/3 text-xl"
+                } flex flex-col items-center self-center font-bold hover:text-green-primary`}
+              >
+                {item.svg}
+                <p>{item.title1}</p>
+                <p>{item.title2}</p>
+              </div>
+              <div className="w-full flex justify-center items-center text-base">
+                {(insideMenu || showDesc) && item.desc}
+              </div>
+            </div>
+          </a>
+        </Link>
+        {!insideMenu && (
+          <Fragment>
+            <div
+              className="w-full p-1 flex items-end justify-end"
+              onClick={() =>
+                //@ts-ignore
+                setVideoUrl(!videoUrl ? item.video : "")
+              }
+            >
+              {!videoUrl ? <SVGPlay /> : <SVGStop />}
+              <label
+                className={`ml-1 cursor-pointer ${
+                  videoUrl && "text-red-600"
+                } hover:text-red-600`}
+              >
+                Video
+              </label>
+            </div>
+            <div className="w-full flex justify-end items-end">
+              <div className="w-1/6"></div>
+              <div className="w-full h-full">
+                {!videoUrl ? (
+                  <img src={item.img} />
+                ) : (
+                  <VideoPlayer url={videoUrl} urlHandler={setVideoUrl} />
+                )}
+              </div>
+            </div>
+          </Fragment>
+        )}
+    </div>
+  );
+}
--- src/components/calc/calculators.tsx
+++ src/components/calc/calculators.tsx
@@ -1,25 +1,21 @@
 import React from "react";
-import Section from "../form/section";
 import SVGScale from "../svgscale";
 import { ROUTES } from "../../CONSTANTS";
 import SVGFreedom from "../svgfreedom";
 import SVGLoan from "../svgloan";
+import CalcItem from "./calcitem";
 
 interface CalculatorsProps {
   insideMenu?: boolean;
-  insideForm?: boolean;
 }
 
-export default function Calculators({
-  insideMenu,
-  insideForm,
-}: CalculatorsProps) {
+export default function Calculators({ insideMenu }: CalculatorsProps) {
   const LIGHT_COLORS = {
     RED: "#EEA894",
     ORANGE: "#fbd38d",
     BLUE: "#90cdf4",
     TEAL: "#81e6d9",
-    PURPLE: "#d6bcfa",
+    INDIGO: "#a3bffa",
     YELLOW: "#faf089",
     GRAY: "#cbd5e0",
     PINK: "#f687b3",
@@ -32,10 +28,10 @@ export default function Calculators({
   const calcItems = [
     {
       title1: "Financial",
-      title2:"Freedom",
+      title2: "Freedom",
       svg: <SVGFreedom />,
       desc: "How much Money is Needed? When will I achieve it?",
-      color: LIGHT_COLORS.BLUE,
+      color: LIGHT_COLORS.RED,
       link: ROUTES.DASHBOARD,
       img: "images/step1.png",
       video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
@@ -45,7 +41,7 @@ export default function Calculators({
       title2: "Rent",
       svg: <SVGScale selected />,
       desc: "Is it cheaper to Buy or Rent? Till When?",
-      color: LIGHT_COLORS.GREEN,
+      color: LIGHT_COLORS.BROWN,
       link: ROUTES.DASHBOARD,
       img: "images/step2.png",
       video: `https://vimeo.com/455216214`,
@@ -55,7 +51,7 @@ export default function Calculators({
       title2: "Loan",
       svg: <SVGLoan selected />,
       desc: "Should I pay by self or loan? How much total interest will I pay?",
-      color: LIGHT_COLORS.BROWN,
+      color: LIGHT_COLORS.INDIGO,
       link: ROUTES.DASHBOARD,
       img: "images/step3.png",
       video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
@@ -65,7 +61,7 @@ export default function Calculators({
       title2: "Loan",
       svg: <SVGLoan selected />,
       desc: "Should I pay by self or loan? How much total interest will I pay?",
-      color: LIGHT_COLORS.RED,
+      color: LIGHT_COLORS.ORANGE,
       link: ROUTES.DASHBOARD,
       img: "images/step3.png",
       video: `https://www.youtube.com/watch?v=RH7t3p5zXNA&t=0s`,
@@ -95,27 +91,7 @@ export default function Calculators({
   return (
     <div className="w-full flex flex-wrap items-start justify-center pb-1">
       {calcItems.map((item, i) => (
-        <Section
-          key={"citem" + i}
-          title={
-            <div className="w-full flex h-full ">
-            <div className="w-1/3 h-full flex flex-col items-center justify-center bg-silver">
-              {item.svg}
-              <p>{item.title1}</p>
-              <p>{item.title2}</p>
-            </div>
-            <div className="w-2/3 p-2 flex justify-center items-center text-base text-white font-normal">
-            {item.desc}
-          </div>
-          </div>
-          }
-          hasResult
-          insideForm={insideForm}
-          insideMenu={insideMenu}
-          color={item.color}
-          imgSrc={item.img}
-          videoSrc={item.video}
-        />
+        <CalcItem key={"ci" + i} item={item} insideMenu={insideMenu} />
       ))}
     </div>
   );
