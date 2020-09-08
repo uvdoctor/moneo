60a0a5d50bd4e17c667336d40b9c0f767063ba6e
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-08 13:25:20 +0530

    Updated calc item img for link

--- src/components/calc/calcitem.tsx
+++ src/components/calc/calcitem.tsx
@@ -25,55 +25,64 @@ export default function CalcItem({ item, insideMenu }: CalcItemProps) {
       onMouseEnter={() => setShowDesc(true)}
       onMouseLeave={() => setShowDesc(false)}
     >
-        <Link href={item.link}>
-          <a>
-            <div className="flex w-full"
-            style={{ minHeight: !insideMenu ? "9rem" : "6rem" }}>
-              <div
-                className={`${
-                  insideMenu ? "w-full" : "w-1/3 text-xl"
-                } flex flex-col items-center self-center font-bold hover:text-green-primary`}
-              >
-                {item.svg}
-                <p>{item.title1}</p>
-                <p>{item.title2}</p>
-              </div>
-              <div className="w-full flex justify-center items-center text-base font-normal">
-                {(insideMenu || showDesc) && item.desc}
-              </div>
-            </div>
-          </a>
-        </Link>
-        {!insideMenu && (
-          <Fragment>
+      <Link href={item.link}>
+        <a>
+          <div
+            className="flex w-full"
+            style={{ minHeight: !insideMenu ? "9rem" : "6rem" }}
+          >
             <div
-              className="w-full p-1 flex items-end justify-end"
-              onClick={() =>
-                //@ts-ignore
-                setVideoUrl(!videoUrl ? item.video : "")
-              }
+              className={`${
+                insideMenu ? "w-full" : "w-1/3 text-xl"
+              } flex flex-col items-center self-center font-bold hover:text-green-primary`}
             >
-              {!videoUrl ? <SVGPlay /> : <SVGStop />}
-              <label
-                className={`ml-1 cursor-pointer ${
-                  videoUrl && "text-red-600"
-                } hover:text-red-600`}
-              >
-                Video
-              </label>
+              {item.svg}
+              <p>{item.title1}</p>
+              <p>{item.title2}</p>
             </div>
-            <div className="w-full flex justify-end items-end">
-              <div className="w-1/6"></div>
-              <div className="w-full h-full">
-                {!videoUrl ? (
-                  <img src={item.img} />
-                ) : (
-                  <VideoPlayer url={videoUrl} urlHandler={setVideoUrl} />
-                )}
-              </div>
+            <div className="w-full flex justify-center items-center text-base font-normal">
+              {(insideMenu || showDesc) && item.desc}
             </div>
-          </Fragment>
-        )}
+          </div>
+        </a>
+      </Link>
+      {!insideMenu && (
+        <Fragment>
+          <div
+            className="w-full p-1 flex items-end justify-end"
+            onClick={() =>
+              //@ts-ignore
+              setVideoUrl(!videoUrl ? item.video : "")
+            }
+          >
+            {!videoUrl ? <SVGPlay /> : <SVGStop />}
+            <label
+              className={`ml-1 cursor-pointer ${
+                videoUrl && "text-red-600"
+              } hover:text-red-600`}
+            >
+              Video
+            </label>
+          </div>
+          <div className="w-full flex justify-end items-end">
+            <div className="w-1/6"></div>
+            <div className="w-full h-full">
+              {!videoUrl ? (
+                <Link href={item.link}>
+                  <a>
+                    <img
+                      className="object-contain cursor-pointer"
+                      src={item.img}
+                    />
+                  </a>
+                </Link>
+              ) : (
+                <VideoPlayer url={videoUrl} urlHandler={setVideoUrl} />
+              )}
+            </div>
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 }
