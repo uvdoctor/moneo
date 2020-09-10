d68232c82967520fb075d2a0d90361e2b8b358a8
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-10 17:46:50 +0530

    Updated header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { Fragment } from "react";
 //@ts-ignore
 import { AwesomeButton } from "react-awesome-button";
 import Menu from "./menu";
 import LogoWithName from "./logowithname";
+import SVGMenu from "./svgmenu";
+import { isMobileDevice } from "./utils";
+import { useFullScreenBrowser } from "react-browser-hooks";
 
-const Header = () => {
+interface HeaderProps {
+  parentStyleDiff: boolean;
+  parentStyleDiffHandler: Function;
+}
+
+export default function Header({
+  parentStyleDiff,
+  parentStyleDiffHandler,
+}: HeaderProps) {
+  const fsb = useFullScreenBrowser();
   return (
     <nav
-      className="fixed top-0 bg-white md:text-lg lg:text-xl flex w-full items-end 
-      justify-between flex-wrap py-1 cursor font-bold z-10">
+      className={`top-0 ${
+        !parentStyleDiff
+          ? "fixed bg-white text-default"
+          : "bg-transparent text-silver"
+      } text-base md:text-lg lg:text-xl flex w-full items-end 
+      justify-between flex-wrap cursor font-bold`}
+    >
       <LogoWithName />
-      <Menu />
+      {isMobileDevice(fsb) ? (
+        <Fragment>
+          <label className="whitespace-no-wrap">Your Financial Analyst</label>
+          <div className="cursor-pointer" onClick={() => {}}>
+            <SVGMenu coverPage={parentStyleDiff} />
+          </div>
+        </Fragment>
+      ) : (
+        <Menu
+          parentStyleDiff={parentStyleDiff}
+          parentStyleDiffHandler={parentStyleDiffHandler}
+        />
+      )}
     </nav>
   );
-};
-
-export default Header;
+}
--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,12 +1,10 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 //@ts-ignore
 import { AwesomeButton } from "react-awesome-button";
 import { useFullScreenBrowser } from "react-browser-hooks";
-import Menu from "./menu";
 import { getLandingPageHeight, isMobileDevice } from "./utils";
 import GoalImages from "./goalimages";
-import LogoWithName from "./logowithname";
-import SVGMenu from "./svgmenu";
+import Header from "./header";
 
 const Landing = () => {
   const fsb = useFullScreenBrowser();
@@ -25,44 +23,22 @@ const Landing = () => {
         backgroundImage: `url('images/relaxedwoman.png')`,
       }}
     >
-      <nav
-        className={`${
-          !coverPage ? "bg-white text-default" : "bg-transparent text-silver"
-        } text-base md:text-lg lg:text-xl flex w-full items-end 
-      justify-between flex-wrap cursor font-bold`}
-      >
-        <LogoWithName />
-        {isMobileDevice(fsb) ? (
-          <Fragment>
-            <label className="whitespace-no-wrap">Your Financial Analyst</label>
-            <div className="cursor-pointer" onClick={() => {}}>
-              <SVGMenu coverPage={coverPage} />
-            </div>
-          </Fragment>
-        ) : (
-          <Menu
-            parentStyleDiff={coverPage}
-            parentStyleDiffHandler={setCoverPage}
-          />
-        )}
-      </nav>
+      <Header
+        parentStyleDiff={coverPage}
+        parentStyleDiffHandler={setCoverPage}
+      />
       <div
-        className={`w-full flex flex-col md:flex-row
+        className={`w-full flex flex-col mt-1 md:mt-2 lg:mt-4 ml-2 md:ml-4 lg:ml-8 
         text-silver font-bold xs:text-xs sm:text-base md:text-2xl lg:text-3xl xl:text-4xl`}
       >
-        <div className="w-full mt-2 lg:mt-4 ml-2 md:ml-4 lg:ml-8 flex flex-col">
-          {!isMobileDevice(fsb) && <label>Your Financial Analyst for</label>}
-          <label className="text-green-primary">Stress-free Savings & Investments to</label>
-          <div className="w-full flex items-center text-green-primary">
-            Meet Your Goals            
-            <GoalImages />
-          </div>
+        {!isMobileDevice(fsb) && <label>Your Financial Analyst for</label>}
+        <label className="text-green-secondary">
+          Stress-free Savings & Investments to
+        </label>
+        <div className="w-full flex items-center text-green-primary">
+          Meet Your Goals
+          <GoalImages />
         </div>
-        {isMobileDevice(fsb) && (
-          <div className="w-full flex justify-end md:pr-4 mt-10">
-            <GoalImages />
-          </div>
-        )}
       </div>
     </div>
   );
