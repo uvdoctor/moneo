0a9a6ee37b9a84270de527a83339f806f08fef16
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-11 15:02:19 +0530

    Updated landing

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -19,9 +19,9 @@ const Landing = () => {
   return (
     <Fragment>
       <div
-        className="flex flex-col bg-contain bg-no-repeat w-screen h-screen"
+        className="flex flex-col bg-contain xl:bg-cover bg-no-repeat w-screen xl:h-screen"
         style={{
-          height: isMobileDevice(fsb) ? coverHeight + "px" : "",
+          height: coverHeight + "px",
           backgroundImage: `url('images/cover.jpg')`,
         }}
       >
@@ -30,7 +30,7 @@ const Landing = () => {
           parentStyleDiffHandler={setCoverPage}
         />
         <div
-          className={`w-full flex flex-col mt-1 ml-2 
+          className={`w-full flex flex-col md:mt-1 ml-2 
         font-bold xs:text-xs sm:text-base md:text-xl lg:text-2xl`}
         >
           <div className="w-full flex flex-wrap md:text-2xl lg:text-3xl xl:text-4xl">
