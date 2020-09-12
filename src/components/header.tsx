946c9bc63dde02df4bf37a27ca4309cf95e0cb44
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-12 15:27:00 +0530

    fixed mobile header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -43,8 +43,8 @@ export default function Header({
     >
       <LogoWithName />
       {isMobileDevice(fsb) ? (
-        <div className="w-full max-w-xs flex justify-between">
-          <label className="whitespace-no-wrap">Your Financial Analyst</label>
+        <div className="max-w-xs flex justify-between">
+          <label className="whitespace-no-wrap mr-2">Your Financial Analyst</label>
           <div
             className="cursor-pointer pr-1"
             onClick={() => setShowMobileMenu(!showMobileMenu)}
