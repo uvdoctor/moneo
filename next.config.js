f060d5e05101ba0dd4ab7498bad36a4005eaeb32
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-18 22:16:35 +0530

    removed mdx config

--- next.config.js
+++ next.config.js
@@ -2,14 +2,6 @@ const withPWA = require("next-pwa");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const { default: Slider } = require("rc-slider");
 const isProd = process.env.NODE_ENV === "production";
-const withMDX = require("@next/mdx")({
-  extension: /\.mdx?$/
-});
-
-module.exports = withMDX({
-  // Pick up MDX files in the /pages/ directory
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-});
 
 module.exports = withPWA({
   pwa: {
