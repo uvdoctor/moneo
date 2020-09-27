c664d396ecb5f04ebaa11e789237b060eeed3711
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-27 22:05:21 +0530

    Updated meta tags

--- src/components/ddpage.tsx
+++ src/components/ddpage.tsx
@@ -33,6 +33,14 @@ export default function DDPage(props: DDPageProps) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta
+          name="apple-mobile-web-app-title"
+          content="DollarDarwin - Your Financial Analyst"
+        />
+        <meta name='application-name' content='DollarDarwin' />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="description" content="Your Financial Analyst" />
         <meta
           name="Personal finance management app"
@@ -40,8 +48,11 @@ export default function DDPage(props: DDPageProps) {
 Personal finance for Millennial, personal finance advisor, personal finance, Digital Banking, Social Banking, money management apps,
 personal finance app, Budget goals, Budgeting,  Mutual Funds, Stock, Equity trading, personal money management, online money management, personal
 finance plan, personal finance management, Banking App, Mobile Banking, Budgeting, Financial planning, Investing, Neo Banking Canada, Neo Banking USA"
-        ></meta>
-        <meta property="og:title" content="DollarDarwin - Your Financial Analyst" />
+        />
+        <meta
+          property="og:title"
+          content="DollarDarwin - Your Financial Analyst"
+        />
         <meta
           property="og:description"
           content="
@@ -50,13 +61,25 @@ finance plan, personal finance management, Banking App, Mobile Banking, Budgetin
           Did You? 😎"
         />
         <meta
-          property="og:image" itemProp="image"
+          property="og:image"
+          itemProp="image"
           content="https://dollardarwin.com/images/icons/tlogo256.png"
         />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en-US" />
-        <meta property="og:locale:alternate"  content="en-CA" />
+        <meta property="og:locale:alternate" content="en-CA" />
         <meta property="og:url" content="https://dollardarwin.com" />
+        <meta name="twitter:card" content="Your Financial Analyst" />
+        <meta name="twitter:url" content="https://dollardarwin.com" />
+        <meta name="twitter:title" content="DollarDarwin" />
+        <meta name="twitter:description" content="Stress-free Savings & Investments to Meet Your Goals." />
+        <meta
+          name="twitter:image"
+          content="https://dollardarwin.com/images/icons/tlogo192.png"
+        />
+        <meta name="twitter:creator" content="@dollardarwin" />
+        <meta name='msapplication-tap-highlight' content='no' />
+        <meta name='format-detection' content='telephone=no' />
         <title>{props.title}</title>
       </Head>
       <main>
