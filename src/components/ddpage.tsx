c5789046fa899471ac258259ebb9a860157e49b7
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-27 22:07:20 +0530

    updated meta config

--- src/components/ddpage.tsx
+++ src/components/ddpage.tsx
@@ -31,6 +31,10 @@ export default function DDPage(props: DDPageProps) {
     <Fragment>
       <Head>
         <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
         <meta name="theme-color" content="#000000" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,22 +7,9 @@ import "../styles/index.css";
 import * as gtag from "../lib/gtag";
 import { Router } from "next/router";
 import React from "react";
-import { Head, Html } from "next/document";
 
 Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Html>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
-      </Head>
-      <body>
-        <Component {...pageProps} />
-      </body>
-    </Html>
-  );
+  return <Component {...pageProps} />
 }
