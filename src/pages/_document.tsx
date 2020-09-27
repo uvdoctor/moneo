5682467394e0ce0372121d515ba5d3e3672222b9
Umang Doctor <emailumangdoctor@gmail.com>
2020-09-27 23:15:46 +0530

    favicon icon path updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,8 +28,8 @@ class DDDocument extends NextDocument<DDDocumentProps> {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="images/icons/favicon.ico" />
-          <link rel='shortcut icon' href='images/icons/favicon.ico' />
+          <link rel="icon" href="favicon.ico" />
+          <link rel='shortcut icon' href='favicon.ico' />
           <link rel="apple-touch-icon" href="images/icons/logo57.png" />
           <link href="https://fonts.googleapis.com/css2?family=Jost:wght@400;700&display=swap" rel="stylesheet" />
           <link rel="manifest" href="manifest.json" />
