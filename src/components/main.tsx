cb698475618ef40afcaf6310f5ea8c8339f4ef00
RSRawat <ravinder.singh.rawat2008@gmail.com>
2020-09-28 08:49:40 +0530

    Navigation fixed in all divices

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 import { useScroll } from "react-browser-hooks";
-import LogoWithName from "./logowithname";
+import Nav from "./Nav";
 import { CALC_NAMES, COLORS, ROUTES } from "../CONSTANTS";
 import GoalImages from "./goalimages";
 import FunSVG from "./features/svgfun";
@@ -180,76 +180,11 @@ export default function Main() {
                 : "linear-gradient(to bottom, #dbedca, rgba(246,246,246,0))",
           }}
         ></div>
-        <header
-          className="sm:flex sm:justify-between sm:items-center sm:px-4 sm:py-3 fixed w-full sm:pl-0 z-20"
-          style={{
-            maxWidth: "1280px",
-            paddingRight: "1rem",
-            paddingLeft: "1rem",
-          }}
-        >
-          <div className="flex items-center justify-between px-4 py-3 sm:p-0">
-            <LogoWithName />
-            <div className="sm:hidden">
-              <button
-                type="button"
-                className="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
-              >
-                <svg className="h-6 w-6 fill-current" viewBox="0 0 24 24">
-                  <path
-                    v-if="isOpen"
-                    fillRule="evenodd"
-                    d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-                  />
-                  <path
-                    v-if="!isOpen"
-                    fillRule="evenodd"
-                    d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-          <nav className="site-nav px-2 pt-2 pb-4 sm:flex sm:p-0">
-            <a
-              className="cursor-pointer block px-2 py-1 font-semibold rounded"
-              onClick={() => {
-                calculateRef.current.scrollIntoView({ behavior: "smooth" });
-              }}
-            >
-              Calculate
-            </a>
-            <div className="site-nav-dropdown relative">
-              <span className="mt-1 cursor-pointer block px-2 py-1 font-semibold rounded sm:mt-0 sm:ml-2">
-                About
-              </span>
-              <div className="absolute bg-white hidden p-5 pl-0 shadow-lg">
-                <div
-                  className="cursor-pointer hover:text-green-primary mt-1 block px-2 py-1 font-semibold rounded sm:mt-0 sm:ml-2"
-                  onClick={() =>
-                    featuresRef.current.scrollIntoView({ behavior: "smooth" })
-                  }
-                >
-                  Features
-                </div>
-                <div className="cursor-pointer hover:text-green-primary mt-1 block px-2 py-1 font-semibold rounded sm:mt-0 sm:ml-2">
-                  Pricing
-                </div>
-                <div className="cursor-pointer hover:text-green-primary mt-1 block px-2 py-1 font-semibold rounded sm:mt-0 sm:ml-2">
-                  Company
-                </div>
-              </div>
-            </div>
-            <a
-              className="cursor-pointer mt-1 block px-2 py-1 font-semibold rounded sm:mt-0 sm:ml-2"
-              onClick={() =>
-                joinRef.current.scrollIntoView({ behavior: "smooth" })
-              }
-            >
-              Earn up to $200 credit*
-            </a>
-          </nav>
-        </header>
+        <Nav
+        	joinRef={joinRef}
+        	calculateRef={calculateRef}
+        	featuresRef={featuresRef}
+        />
         <div className="relative">
           <div
             className="absolute w-full mt-20"
