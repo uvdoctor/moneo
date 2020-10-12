7a8d0d5cf07994e9c54b23f902011671a2b7057b
Umang Doctor <emailumangdoctor@gmail.com>
2020-10-12 19:50:36 +0530

    fixed radial slider

--- src/components/form/radialinput.tsx
+++ src/components/form/radialinput.tsx
@@ -26,24 +26,24 @@ export default function RadialInput(props: RadialInputProps) {
 		<Space align="center" direction="vertical">
 			{props.info && <Tooltip title={props.info} />}
 			<p>{props.pre}</p>
-			<p>
-			<CircularSlider
-				onChange={(val: string) => props.changeHandler(props.step < 1 ? parseFloat(val) : parseInt(val))}
-				label={props.label}
-				trackColor={COLORS.DEFAULT}
-				data={props.data}
-				dataIndex={(props.value - getVal(props.data[0])) / props.step}
-				appendToValue={props.unit}
-				width={width}
-				labelColor={COLORS.DEFAULT}
-				labelBottom={props.labelBottom}
-				valueFontSize="1.25rem"
-				labelFontSize="1.25rem"
-				progressColorFrom={props.colorFrom ? props.colorFrom : COLORS.GREEN}
-				progressColorTo={props.colorTo ? props.colorTo : COLORS.GREEN}
-				knobColor="#cbd5e0"
+			<p className="radial-input">
+				<CircularSlider
+					onChange={(val: string) => props.changeHandler(props.step < 1 ? parseFloat(val) : parseInt(val))}
+					label={props.label}
+					trackColor={COLORS.LIGHT_GRAY}
+					data={props.data}
+					dataIndex={(props.value - getVal(props.data[0])) / props.step}
+					appendToValue={props.unit}
+					width={width}
+					labelColor={COLORS.DEFAULT}
+					labelBottom={props.labelBottom}
+					valueFontSize="1.25rem"
+					labelFontSize="1.25rem"
+					progressColorFrom={props.colorFrom ? props.colorFrom : COLORS.GREEN}
+					progressColorTo={props.colorTo ? props.colorTo : COLORS.GREEN}
+					knobColor="#cbd5e0"
 				/>
-				</p>
+			</p>
 			<p>{props.post}</p>
 		</Space>
 	);
