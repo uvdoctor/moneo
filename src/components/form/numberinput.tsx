a16b29ee79b1adcdb87744363a29b04233a47144
Umang Doctor <emailumangdoctor@gmail.com>
2020-11-26 22:22:29 +0530

    updated number input

--- src/components/form/numberinput.tsx
+++ src/components/form/numberinput.tsx
@@ -1,14 +1,9 @@
-import React, { useEffect, useRef, useState } from "react";
-import {
-	getRangeFactor,
-	parseNumber,
-	toCurrency,
-	toReadableNumber,
-} from "../utils";
-import { Slider } from "antd";
-import { COLORS } from "../../CONSTANTS";
-import { Tooltip, InputNumber, Row, Col } from "antd";
-import { InfoCircleOutlined } from "@ant-design/icons";
+import React, { useEffect, useRef, useState } from 'react';
+import { getRangeFactor, parseNumber, toCurrency, toReadableNumber } from '../utils';
+import { Slider } from 'antd';
+import { COLORS } from '../../CONSTANTS';
+import { Tooltip, InputNumber, Row, Col } from 'antd';
+import { InfoCircleOutlined } from '@ant-design/icons';
 interface NumberInputProps {
 	info?: string;
 	pre: any;
@@ -27,53 +22,47 @@ interface NumberInputProps {
 
 export default function NumberInput(props: NumberInputProps) {
 	const inputRef = useRef(null);
-	const [rangeFactor, setRangeFactor] = useState<number>(
-		props.currency ? getRangeFactor(props.currency) : 1
-	);
-	const [sliderBorderColor, setSliderBorderColor] = useState<string>(
-		COLORS.GREEN
-	);
-	const [feedbackText, setFeedbackText] = useState<string>("");
-	const [minNum, setMinNum] = useState<number>(props.min * rangeFactor);
-	const [maxNum, setMaxNum] = useState<number>(props.max * rangeFactor);
-	const [stepNum, setStepNum] = useState<number>(
-		props.step ? props.step * rangeFactor : 1
-	);
+	const [ rangeFactor, setRangeFactor ] = useState<number>(props.currency ? getRangeFactor(props.currency) : 1);
+	const [ sliderBorderColor, setSliderBorderColor ] = useState<string>(COLORS.GREEN);
+	const [ feedbackText, setFeedbackText ] = useState<string>('');
+	const [ minNum, setMinNum ] = useState<number>(props.min * rangeFactor);
+	const [ maxNum, setMaxNum ] = useState<number>(props.max * rangeFactor);
+	const [ stepNum, setStepNum ] = useState<number>(props.step ? props.step * rangeFactor : 1);
 
 	const getSliderMarks = (min: number, max: number) => {
 		let marks: any = {
-			[min]: toReadableNumber(min, props.step < 1 ? 2 : 0),
+			[min]: toReadableNumber(min, props.step < 1 ? 2 : 0)
 		};
-		if (min < 0) marks[0] = "0";
+		if (min < 0) marks[0] = toReadableNumber(0, props.step < 1 ? 2 : 0);
 		if (props.additionalMarks)
-			props.additionalMarks.forEach((val: number) => {
-				marks[val] = toReadableNumber(val, props.step < 1 ? 2 : 0);
-			});
-		marks[max] = { label: toReadableNumber(max) };
+			props.additionalMarks.forEach(
+				(val: number) => (marks[val] = toReadableNumber(val, props.step < 1 ? 2 : 0))
+			);
+		marks[max] = toReadableNumber(max, props.step < 1 ? 2 : 0);
 		return marks;
 	};
 
-	const [marks, setMarks] = useState<any>(getSliderMarks(props.min, props.max));
+	const marks = getSliderMarks(props.min, props.max);
 
-	useEffect(() => {
-		if (props.currency)
-			setRangeFactor(getRangeFactor(props.currency as string));
-	}, [props.currency]);
+	useEffect(
+		() => {
+			let rf = getRangeFactor(props.currency as string);
+			if (rf !== rangeFactor) setRangeFactor(rf);
+		},
+		[ props.currency ]
+	);
 
-	useEffect(() => {
-		let newMin = props.min * rangeFactor;
-		let newMax = props.max * rangeFactor;
-		let newStep = props.step * rangeFactor;
-		setMinNum(newMin);
-		setMaxNum(newMax);
-		setStepNum(newStep);
-		let newMarks: any = getSliderMarks(newMin, newMax);
-		setMarks(newMarks);
-	}, [rangeFactor, props.min, props.max]);
-
-	useEffect(() => {
-		setMarks(getSliderMarks(props.min, props.max));
-	}, [props.additionalMarks]);
+	useEffect(
+		() => {
+			let newMin = props.min * rangeFactor;
+			let newMax = props.max * rangeFactor;
+			let newStep = props.step * rangeFactor;
+			setMinNum(newMin);
+			setMaxNum(newMax);
+			setStepNum(newStep);
+		},
+		[ rangeFactor, props.min, props.max ]
+	);
 
 	const getClosestKey = (value: number, keys: Array<number>) => {
 		let result: number = keys[0];
@@ -87,13 +76,11 @@ export default function NumberInput(props: NumberInputProps) {
 	const provideFeedback = (val: number) => {
 		if (props.feedback) {
 			let allKeys = Object.keys(props.feedback);
-			let allSortedKeys = allKeys
-				.map((k) => parseFloat(k))
-				.sort((a, b) => a - b);
+			let allSortedKeys = allKeys.map((k) => parseFloat(k)).sort((a, b) => a - b);
 			let feedback: any = props.feedback[getClosestKey(val, allSortedKeys)];
 			if (!feedback || !feedback.label) {
-				setSliderBorderColor("white");
-				setFeedbackText("");
+				setSliderBorderColor('white');
+				setFeedbackText('');
 			} else {
 				setSliderBorderColor(feedback.color);
 				setFeedbackText(feedback.label);
@@ -102,7 +89,7 @@ export default function NumberInput(props: NumberInputProps) {
 	};
 
 	return (
-		<div style={{ minWidth: "250px" }}>
+		<div style={{ minWidth: '250px' }}>
 			<Col span={24}>
 				<Row justify="space-between" align="middle">
 					<Col>
@@ -117,19 +104,12 @@ export default function NumberInput(props: NumberInputProps) {
 						)}
 					</Col>
 					{!props.currency && (
-						<b>{`${toReadableNumber(
-							props.value,
-							props.step && props.step < 1 ? 2 : 0
-						)} ${props.unit}`}</b>
+						<b>{`${toReadableNumber(props.value, props.step && props.step < 1 ? 2 : 0)} ${props.unit}`}</b>
 					)}
 				</Row>
 			</Col>
 			<Col span={24}>
-				<Row
-					justify="space-between"
-					align="top"
-					style={{ marginBottom: "1.5rem" }}
-				>
+				<Row justify="space-between" align="top" style={{ marginBottom: '1.5rem' }}>
 					{props.currency && (
 						<Col span={10}>
 							<InputNumber
@@ -142,9 +122,7 @@ export default function NumberInput(props: NumberInputProps) {
 									provideFeedback(val as number);
 									props.changeHandler(val as number);
 								}}
-								formatter={(val) =>
-									toCurrency(val as number, props.currency as string)
-								}
+								formatter={(val) => toCurrency(val as number, props.currency as string)}
 								parser={(val) => parseNumber(val as string, props.currency)}
 								onPressEnter={(e: any) => {
 									e.preventDefault();
@@ -152,12 +130,10 @@ export default function NumberInput(props: NumberInputProps) {
 									inputRef.current.blur();
 								}}
 								onBlur={(e: any) => {
-									let num = parseInt(
-										parseNumber(e.currentTarget.value, props.currency)
-									);
+									let num = parseInt(parseNumber(e.currentTarget.value, props.currency));
 									if (!num || num < props.min) props.changeHandler(props.min);
 								}}
-								style={{ width: "100%", marginBottom: "0px" }}
+								style={{ width: '100%', marginBottom: '0px' }}
 							/>
 						</Col>
 					)}
@@ -174,12 +150,12 @@ export default function NumberInput(props: NumberInputProps) {
 								props.changeHandler(val);
 							}}
 							handleStyle={{
-								cursor: "grab",
-								borderColor: sliderBorderColor,
+								cursor: 'grab',
+								borderColor: sliderBorderColor
 							}}
 						/>
 						{feedbackText && (
-							<Col span={24} style={{ textAlign: "center" }}>
+							<Col span={24} style={{ textAlign: 'center' }}>
 								{feedbackText}
 							</Col>
 						)}
