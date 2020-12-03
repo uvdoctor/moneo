d4a73d2105ecfc6d91c7d67b8ef8d22ab8bdb5c6
RSRawat <ravinder.singh.rawat2008@gmail.com>
2020-12-03 18:22:03 +0530

    Updated st

--- src/components/form/selectinput.tsx
+++ src/components/form/selectinput.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, Fragment } from "react";
 import { getCurrencyList } from "../utils";
 import { Tooltip, Select, Row, Col } from "antd";
 import { InfoCircleOutlined } from "@ant-design/icons";
@@ -20,41 +20,36 @@ export default function SelectInput(props: SelectInputProps) {
 	const selectOptions = props.currency ? getCurrencyList() : props.options;
 
 	return (
-		<Row align="middle" justify="space-between">
+		<Fragment>
 			{props.pre && (
-				<Col>
+				<Fragment>
 					{props.pre}
 					{props.info && (
 						<Tooltip title={props.info}>
 							<InfoCircleOutlined />
 						</Tooltip>
 					)}
-				</Col>
-			)}
-			<Col>
-				<Row align="middle">
-					<Select
-						showSearch
-						optionFilterProp="children"
-						value={props.currency ? props.value : selectOptions[props.value]}
-						onChange={(value: string) => props.changeHandler(value)}
-						filterOption={(input, option) =>
-							option?.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-						}
-						disabled={props.disabled}
-						defaultValue={selectOptions[props.value]}
-						style={{minWidth: '60px'}}
-					>
-						{Object.keys(selectOptions).map((key) => (
-							<Option key={key} value={key}>
-								{props.currency ? key : selectOptions[key]}
-							</Option>
-						))}
-					</Select>
-					{props.unit}
-					<Row>{props.post}</Row>
-				</Row>
-			</Col>
-		</Row>
+				</Fragment>
+			)}{" "}
+			<Select
+				showSearch
+				optionFilterProp="children"
+				value={props.currency ? props.value : selectOptions[props.value]}
+				onChange={(value: string) => props.changeHandler(value)}
+				filterOption={(input, option) =>
+					option?.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+				}
+				disabled={props.disabled}
+				defaultValue={selectOptions[props.value]}
+			>
+				{Object.keys(selectOptions).map((key) => (
+					<Option key={key} value={key}>
+						{props.currency ? key : selectOptions[key]}
+					</Option>
+				))}
+			</Select>{" "}
+			{props.unit}
+			{props.post}
+		</Fragment>
 	);
 }
