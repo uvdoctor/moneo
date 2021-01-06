fe7f70a51d3a58dc95d8ed8880fabd9f54264fc1
Umang Doctor <emailumangdoctor@gmail.com>
2021-01-06 14:21:38 +0530

    updated banner content

--- src/components/DDBasicPage.tsx
+++ src/components/DDBasicPage.tsx
@@ -31,50 +31,50 @@ export default function DDBasicPage(props: DDBasicPageProps) {
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta
           name="apple-mobile-web-app-title"
-          content="Moneo - Your Money Analyst"
+          content="Moneo - Your Money Coach"
         />
         <meta name="application-name" content="Moneo" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="description" content="Your Money Analyst" />
+        <meta name="description" content="Your Money Coach" />
         <meta
           name="Personal finance management app"
-          content="Automated Banking, Investing and Financial Planning, Moneo,
+          content="Automated Banking, Investing and Financial Planning, Moneo, Finance coach, Money Coach,
 Personal finance for Millennial, personal finance advisor, personal finance, Digital Banking, Social Banking, money management apps,
 personal finance app, Budget goals, Budgeting,  Mutual Funds, Stock, Equity trading, personal money management, online money management, personal
 finance plan, personal finance management, Banking App, Mobile Banking, Budgeting, Financial planning, Investing, Neo Banking Canada, Neo Banking USA, Neo Banking India"
         />
         <meta
           property="og:title"
-          content="Moneo - Your Money Analyst"
+          content="Moneo - Your Money Coach"
         />
         <meta
           property="og:description"
           content="
           Hello! 🖐 
-          I just got up to $200 credit for Stress-free Savings & Investments to Meet My Goals! 😄
+          I just got up to $200 credit for Stress-free Financial Independence and Meeting My Goals! 😄
           Did You? 😎"
         />
         <meta
           property="og:image"
           itemProp="image"
-          content="https://gomoneo.com/images/icons/tlogo256.png"
+          content="https://moneo.money/images/icons/tlogo256.png"
         />
         <meta property="og:type" content="website" />
         <meta property="og:locale" content="en-US" />
         <meta property="og:locale:alternate" content="en-CA" />
-        <meta property="og:url" content="https://gomoneo.com" />
-        <meta name="twitter:card" content="Your Money Analyst" />
-        <meta name="twitter:url" content="https://gomoneo.com" />
+        <meta property="og:url" content="https://moneo.money" />
+        <meta name="twitter:card" content="Your Money Coach" />
+        <meta name="twitter:url" content="https://moneo.money" />
         <meta name="twitter:title" content="Moneo" />
         <meta
           name="twitter:description"
-          content="Stress-free Savings & Investments to Meet Your Goals."
+          content="Your Money Coach for Stress-free Financial Independence & Meeting Your Goals."
         />
         <meta
           name="twitter:image"
-          content="https://gomoneo.com/images/icons/tlogo192.png"
+          content="https://moneo.money/images/icons/tlogo192.png"
         />
-        <meta name="twitter:creator" content="@getmoneo" />
+        <meta name="twitter:creator" content="@moneocoach" />
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="format-detection" content="telephone=no" />
         <title>{props.title}</title>
--- src/components/landing/BannerContent.tsx
+++ src/components/landing/BannerContent.tsx
@@ -6,10 +6,10 @@ import RollingImages from "../RollingImages";
 const BannerContent = () => {
 	return (
 		<Fragment>
-			<h3>Your Financial Analyst for</h3>
-			<h2>Stress-free Savings &amp; Investments</h2>
+			<h3>Your Money Coach for</h3>
+			<h2>Stress-free Financial Independence</h2>
 			<h3 className="with-icon-anim">
-				to Meet Your Goals <RollingImages />
+				& Meeting Your Goals <RollingImages />
 			</h3>
 			<h4>
 				Join Waitlist &amp; Earn up to $200 credit*
