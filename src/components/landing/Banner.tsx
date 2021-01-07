500328bdf9caa056e05e227cde18cf00847bc098
Umang Doctor <emailumangdoctor@gmail.com>
2021-01-08 01:12:05 +0530

    updated banner

--- src/components/landing/Banner.tsx
+++ src/components/landing/Banner.tsx
@@ -12,12 +12,8 @@ export default function Banner() {
 			<div className="site-banner-content">
 				<BannerContent />
 				<Join />
+				<h3 style={{ marginBottom: '10px' }}>No Commissions. No boring Budgets.</h3>
 				<SocialShare />
-				<p style={{marginTop: '30px'}}>
-					<h3>No Commissions! No boring Budgets!</h3>
-					<h3>Personalized, Easy-to-use & Unbiased.</h3>
-					<h3>Respects Data Privacy. Fully Secure.</h3>
-				</p>
 			</div>
 		</div>
 	);
