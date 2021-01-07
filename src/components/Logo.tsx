3f8d69d9456e68b759d45db4476db8be3c272bf7
Umang Doctor <emailumangdoctor@gmail.com>
2021-01-08 00:15:27 +0530

    updated logo back

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,19 +1,21 @@
 /* eslint-disable jsx-a11y/alt-text */
-import React from "react";
-import { ROUTES } from "../CONSTANTS";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { Button } from "antd";
-import { ArrowLeftOutlined } from "@ant-design/icons";
-import LogoImg from "./LogoImg";
+import React from 'react';
+import { ROUTES } from '../CONSTANTS';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { Button } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import LogoImg from './LogoImg';
 interface LogoProps {
-	onBack?: Function
+	onBack?: Function;
 }
 export default function Logo({ onBack }: LogoProps) {
 	const router = useRouter();
 	return (
-			<div style={{cursor: 'pointer'}} className="logo">
-			<Button type="text" icon={<ArrowLeftOutlined />} onClick={() => onBack ? onBack() : router.back()} />
+		<div style={{ cursor: 'pointer' }} className="logo">
+			{router.pathname !== ROUTES.HOME && (
+				<Button type="text" icon={<ArrowLeftOutlined />} onClick={() => (onBack ? onBack() : router.back())} />
+			)}
 			<Link href={ROUTES.HOME}>
 				<a>
 					<LogoImg />
