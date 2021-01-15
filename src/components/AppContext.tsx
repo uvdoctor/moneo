5bd0957dcaca28404afa6e62af9f3328c42918fc
Umang Doctor <emailumangdoctor@gmail.com>
2021-01-15 21:50:01 +0530

    updated app context

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 
 const AppContext = createContext({});
 
@@ -10,22 +10,13 @@ function AppContextProvider({ children }: AppContextProviderProps) {
 	const [ defaultCountry, setDefaultCountry ] = useState<string>('US');
 	const [ defaultCurrency, setDefaultCurrency ] = useState<string>('USD');
 
-	useEffect(() => {
-		const host = window.location.hostname;
-		if (host.endsWith('.in')) {
-			setDefaultCountry('IN');
-			setDefaultCurrency('INR');
-		} else if (host.endsWith('.uk')) {
-			setDefaultCountry('UK');
-			setDefaultCurrency('GBP');
-		}
-	}, []);
-
 	return (
 		<AppContext.Provider
 			value={{
 				defaultCountry,
-				defaultCurrency
+				setDefaultCountry,
+				defaultCurrency,
+				setDefaultCurrency
 			}}
 		>
 			{children}
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,45 +1,53 @@
-import React, { createElement, Fragment, useState } from "react";
-import { Layout, Drawer, Button, Affix } from "antd";
-import { useScroll } from "react-browser-hooks";
-import { MenuOutlined } from "@ant-design/icons";
-import MainMenu from "./MainMenu";
-import Logo from "./Logo";
+import React, { createElement, Fragment, useContext, useEffect, useState } from 'react';
+import { Layout, Drawer, Button, Affix } from 'antd';
+import { useScroll } from 'react-browser-hooks';
+import { MenuOutlined } from '@ant-design/icons';
+import MainMenu from './MainMenu';
+import Logo from './Logo';
 
-import "./Nav.less";
+import './Nav.less';
+import { AppContext } from './AppContext';
 
 interface NavProps {
-  onBack?: Function | undefined | null;
-  isFixed?: boolean;
-  scrollable?: boolean;
+	onBack?: Function | undefined | null;
+	isFixed?: boolean;
+	scrollable?: boolean;
 }
 
 const Nav = ({ onBack, isFixed, scrollable }: NavProps) => {
-  const { top } = useScroll();
-  const { Header } = Layout;
-  const [showDrawer, setShowDrawer] = useState(false);
-  const onShowDrawer = () => setShowDrawer(true);
-  const onCloseDrawer = () => setShowDrawer(false);
+	const { setDefaultCountry, setDefaultCurrency }: any = useContext(AppContext);
+	const { top } = useScroll();
+	const { Header } = Layout;
+	const [ showDrawer, setShowDrawer ] = useState(false);
+	const onShowDrawer = () => setShowDrawer(true);
+	const onCloseDrawer = () => setShowDrawer(false);
 
-  return createElement(
-    scrollable ? Fragment : Affix,
-    //@ts-ignore
-    scrollable ? {} : { offsetTop: scrollable ? "" : 0 },
-    <Header className={`dd-header ${top > 10 || isFixed ? "fixed-nav" : ""}`}>
-      <Logo onBack={onBack} />
-      <MainMenu />
-      <Button type="text" onClick={onShowDrawer}>
-        <MenuOutlined />
-      </Button>
-      <Drawer
-        placement="right"
-        closable
-        onClose={onCloseDrawer}
-        visible={showDrawer}
-      >
-        <MainMenu mode="inline" />
-      </Drawer>
-    </Header>
-  );
+	useEffect(() => {
+    const host = window.location.hostname;
+		if (host.endsWith('.in')) {
+			setDefaultCountry('IN');
+			setDefaultCurrency('INR');
+		} else if (host.endsWith('.uk')) {
+			setDefaultCountry('UK');
+			setDefaultCurrency('GBP');
+		}
+	}, []);
+
+	return createElement(
+		scrollable ? Fragment : Affix,
+		//@ts-ignore
+		scrollable ? {} : { offsetTop: scrollable ? '' : 0 },
+		<Header className={`dd-header ${top > 10 || isFixed ? 'fixed-nav' : ''}`}>
+			<Logo onBack={onBack} />
+			<MainMenu />
+			<Button type="text" onClick={onShowDrawer}>
+				<MenuOutlined />
+			</Button>
+			<Drawer placement="right" closable onClose={onCloseDrawer} visible={showDrawer}>
+				<MainMenu mode="inline" />
+			</Drawer>
+		</Header>
+	);
 };
 
 export default Nav;
