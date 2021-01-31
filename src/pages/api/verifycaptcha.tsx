277375c16a83478e99830c5f2f610cc09f64fdc2
ramit21 <21.ramit@gmail.com>
2021-01-31 17:36:14 +0530

    Captcha request to Google working

--- src/pages/api/verifycaptcha.tsx
+++ src/pages/api/verifycaptcha.tsx
@@ -1,24 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-	const { method } = req;
-	const { token } = req.body;
+	const { method, body: {token} } = req;
 	const secret = '6LdTyd8ZAAAAAEB3B2-P2swyDqrqpBQEcY4m0sOf';
-	console.log('Going to recaptcha with token: ', token);
 	if (method === 'POST') {
 		fetch('https://www.google.com/recaptcha/api/siteverify', {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json;charset=utf-8'
+				'Content-Type': 'application/x-www-form-urlencoded'
 			},
-			body: JSON.stringify({
-				secret: secret,
-				response: token
-			})
+			body: `secret=${secret}&response=${token}`
 		}).then((captchRes: any) => 
 			captchRes.json()
 		).then((data: any) => {
-			console.log('Server response: ', data);
+			console.log('Server response: ', data.score);
 			res.setHeader('Content-Type', 'application/json');
 			res.status(200).json({ success: data.success });
 		})
