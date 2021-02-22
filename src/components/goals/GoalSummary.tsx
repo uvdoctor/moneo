83d6d53af6144132c271a5018497dafe934bed84
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-02-22 18:58:40 +0530

    Fixed goals UI

--- src/components/goals/GoalSummary.tsx
+++ src/components/goals/GoalSummary.tsx
@@ -1,10 +1,10 @@
-import React, { useContext } from 'react';
-import { PlanContext } from './PlanContext';
-import { Row, Col } from 'antd';
-import { UpdateGoalInput } from '../../api/goals';
-import Summary from './SummaryView';
-import { CalcContextProvider } from '../calc/CalcContext';
-import { GoalContextProvider } from './GoalContext';
+import React, { useContext } from "react";
+import { PlanContext } from "./PlanContext";
+import { Row, Col } from "antd";
+import { UpdateGoalInput } from "../../api/goals";
+import Summary from "./SummaryView";
+import { CalcContextProvider } from "../calc/CalcContext";
+import { GoalContextProvider } from "./GoalContext";
 
 interface GoalSummaryProps {
 	impFilter: string;
@@ -13,11 +13,17 @@ interface GoalSummaryProps {
 export default function GoalSummary({ impFilter }: GoalSummaryProps) {
 	const { allGoals }: any = useContext(PlanContext);
 	return (
-		<Row justify="space-around">
+		<Row
+			justify="space-around"
+			gutter={[
+				{ xs: 0, sm: 15, md: 30, lg: 50 },
+				{ xs: 15, sm: 15, md: 30, lg: 50 },
+			]}
+		>
 			{allGoals.map(
 				(g: UpdateGoalInput, i: number) =>
 					(!impFilter || impFilter === g.imp) && (
-						<Col key={ "g" + i } xs={24} sm={24} md={24} lg={10}>
+						<Col key={"g" + i} xs={24} sm={24} md={24} lg={10}>
 							<CalcContextProvider goal={g}>
 								<GoalContextProvider>
 									<Summary />
--- src/components/goals/SummaryView.tsx
+++ src/components/goals/SummaryView.tsx
@@ -1,38 +1,56 @@
-import React, { Fragment, useContext } from 'react';
-import BasicLineChart from './BasicLineChart';
-import { getGoalTypes, getImpLevels } from './goalutils';
-import { GoalType, LMH } from '../../api/goals';
-import { COLORS } from '../../CONSTANTS';
-import { Card, Row, Col } from 'antd';
-import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import DefaultOppCostResult from '../calc/DefaultOppCostResult';
-import FIImpact from './FIImpact';
-import { PlanContext } from './PlanContext';
-import { CalcContext } from '../calc/CalcContext';
+import React, { Fragment, useContext } from "react";
+import BasicLineChart from "./BasicLineChart";
+import { getGoalTypes, getImpLevels } from "./goalutils";
+import { GoalType, LMH } from "../../api/goals";
+import { COLORS } from "../../CONSTANTS";
+import { Card, Row, Col, Badge } from "antd";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import DefaultOppCostResult from "../calc/DefaultOppCostResult";
+import FIImpact from "./FIImpact";
+import { PlanContext } from "./PlanContext";
+import { CalcContext } from "../calc/CalcContext";
+
+import "./SummaryView.less";
 
 export default function SummaryView() {
 	const { removeGoal, editGoal }: any = useContext(PlanContext);
 	const { goal }: any = useContext(CalcContext);
-	const bgColor = goal.imp === LMH.H ? COLORS.BLUE : goal.imp === LMH.M ? COLORS.ORANGE : COLORS.GREEN;
+	const bgColor =
+		goal.imp === LMH.H
+			? COLORS.BLUE
+			: goal.imp === LMH.M
+			? COLORS.ORANGE
+			: COLORS.GREEN;
 	const nowYear = new Date().getFullYear();
 	const goalTypes: any = getGoalTypes();
 	const impLevels: any = getImpLevels();
 
 	return (
 		<Card
-			headStyle={{ backgroundColor: bgColor, color: COLORS.WHITE }}
+			className="goals-card"
+			//headStyle={{ backgroundColor: bgColor, color: COLORS.WHITE }}
 			title={
 				<Row justify="space-between">
-					<Col>{`${goalTypes[goal.type as GoalType]} ${goal.name}`}</Col>
+					<Col>
+						{`${goalTypes[goal.type as GoalType]} ${goal.name}`}{" "}
+						<Badge
+							count={impLevels[goal.imp]}
+							style={{ backgroundColor: bgColor, color: COLORS.WHITE }}
+						/>
+					</Col>
 					<Col>
 						<Row justify="space-around">
-							<Col>{impLevels[goal.imp]}</Col>
-							<Col>&nbsp;&nbsp;</Col>
-							<Col style={{ cursor: 'pointer' }} onClick={() => editGoal(goal.id)}>
+							<Col
+								style={{ cursor: "pointer" }}
+								onClick={() => editGoal(goal.id)}
+							>
 								<EditOutlined />
 							</Col>
 							<Col>&nbsp;&nbsp;</Col>
-							<Col style={{ cursor: 'pointer' }} onClick={() => removeGoal(goal.id)}>
+							<Col
+								style={{ cursor: "pointer" }}
+								onClick={() => removeGoal(goal.id)}
+							>
 								<DeleteOutlined />
 							</Col>
 						</Row>
@@ -51,7 +69,7 @@ export default function SummaryView() {
 						</Col>
 					</Row>
 				)}
-				<Row justify="center" style={{ marginTop: '20px' }}>
+				<Row justify="center" style={{ marginTop: "20px" }}>
 					<Col span={24}>
 						<BasicLineChart chartTitle={`Cash Flows in ${goal.ccy}`} />
 					</Col>
