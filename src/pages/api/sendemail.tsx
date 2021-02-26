d07f97deb8f17b04aee06c572ee513510a6b2ba1
ramit21 <21.ramit@gmail.com>
2021-02-27 00:23:54 +0530

    email fix

--- src/pages/api/sendemail.tsx
+++ src/pages/api/sendemail.tsx
@@ -1,14 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import AWS from 'aws-sdk';
+/*
 import Rollbar from 'rollbar';
-
-AWS.config.update({ region: 'us-east-1' });
 
 const rollbar = new Rollbar({
   accessToken: '47ae7a3be02a49689265afcb4f720a75',
   captureUncaught: true,
   captureUnhandledRejections: true
 });
+
+rollbar.log('rollbar doesn't work inside promise.then');
+*/
+
+AWS.config.update({
+  'accessKeyId': "AKIAROHCVNGYBO5Y6IPU",
+  'secretAccessKey': "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",
+  'region': 'us-east-1' ,
+})
 
 type Data = {
     status: string
@@ -32,11 +40,9 @@ export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
     sendPromise
       .then(function(data: any) {
         console.log('Mail sent with id = '+data.MessageId);
-        rollbar.log('Mail sent with id = '+data.MessageId);
         res.status(200).json({status: 'Mail sent with id = '+data.MessageId});
       })
       .catch(function(err: any) {
-        rollbar.error('Mail send error', err, err.stack);
         console.error('Mail send error: ' + err + err.stack);
         res.status(200).json({status: 'Error when sending mail: '+err.stack})
       });
