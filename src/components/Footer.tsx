28f789ae24dd23ab365c4d9f47bc0b40a90ac7d1
Umang Doctor <emailumangdoctor@gmail.com>
2021-02-27 22:18:18 +0530

    updated footer icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Col, Row } from "antd";
 import {
-	FacebookShareButton,
 	FacebookIcon,
-	InstapaperShareButton,
-	InstapaperIcon,
-	TwitterShareButton,
-	TwitterIcon,
+	LinkedinIcon,
+	TwitterIcon
 } from "react-share";
+import { YoutubeFilled, InstagramFilled } from '@ant-design/icons';
 import Content from "./Content";
 import { calcList } from "./landing/Calculator";
 
@@ -15,11 +13,11 @@ import "./Footer.less";
 import { PlanContext } from "./goals/PlanContext";
 
 export default function Footer() {
-	const websiteUrl = "https://gomoneo.com";
+	const websiteUrl = "https://moneo.money";
 	const [url, setUrl] = useState(websiteUrl);
 	const { goal }: any = useContext(PlanContext);
 	
-	useEffect(() => setUrl(`${websiteUrl}${window.location.pathname}`), [null]);
+	useEffect(() => setUrl(`${websiteUrl}${window.location.pathname}`), []);
 
 	return (
 		!goal ? <Content className="footer">
@@ -40,15 +38,21 @@ export default function Footer() {
 						remaining essentially unchanged.
 					</p>
 					<div className="social-icons">
-						<FacebookShareButton url={url}>
+						<a target="_blank" href={url} rel="noopener noreferrer">
 							<FacebookIcon round />
-						</FacebookShareButton>
-						<InstapaperShareButton url={url}>
-							<InstapaperIcon round />
-						</InstapaperShareButton>
-						<TwitterShareButton url={url}>
+						</a>
+						<a target="_blank" href={url} rel="noopener noreferrer">
+							<LinkedinIcon round />
+						</a>
+						<a target="_blank" href={url} rel="noopener noreferrer">
 							<TwitterIcon round />
-						</TwitterShareButton>
+						</a>
+						<a target="_blank" href={url} rel="noopener noreferrer">
+							<YoutubeFilled />
+						</a>
+						<a target="_blank" href={url} rel="noopener noreferrer">
+							<InstagramFilled />
+						</a>
 					</div>
 				</Col>
 				<Col xs={24} sm={24} md={12}>
@@ -72,10 +76,10 @@ export default function Footer() {
 							<h3>Quick Links</h3>
 							<ul>
 								<li>
-									<a href="#">About US</a>
+									<a href="#">About Us</a>
 								</li>
 								<li>
-									<a href="#">Contact US</a>
+									<a href="#">Contact Us</a>
 								</li>
 								<li>
 									<a href="#">Terms and Conditions</a>
