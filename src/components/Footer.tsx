23cde1da1dcf15b9780a5190b886013d91834dd8
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-02-27 19:32:24 +0530

    Updated contact us and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,24 +1,91 @@
-import React from 'react';
-import { Col, Layout, Row } from 'antd';
-import Content from './Content';
+import React from "react";
+import { Col, Row } from "antd";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faTwitter,
+	faFacebook,
+	faInstagramSquare,
+} from "@fortawesome/free-brands-svg-icons";
+import Content from "./Content";
+import { calcList } from "./landing/Calculator";
 
-import './Footer.less';
+import "./Footer.less";
 
 export default function Footer() {
-	const { Footer } = Layout;
 	return (
 		<Content className="footer">
-			<Footer>
-			<Row justify="center"><Col>Estimates Only. No Advice.</Col></Row>
-				<Row justify="center"><Col>&copy; 2021 Moneo. All rights reserved.</Col></Row>
-				<Row justify="center">
-					<Col>This site is protected by reCAPTCHA and the Google<span>&nbsp;</span></Col>
-					<Col><a href="https://policies.google.com/privacy">Privacy Policy</a></Col>
-					<Col><span>&nbsp;</span>and<span>&nbsp;</span></Col>
-					<Col><a href="https://policies.google.com/terms">Terms of Service</a></Col>
-					<Col><span>&nbsp;</span>apply.</Col>
-				</Row>
-			</Footer>
+			<Row
+				gutter={[
+					{ xs: 20, sm: 20, md: 25 },
+					{ xs: 20, sm: 20, md: 25 },
+				]}
+			>
+				<Col xs={24} sm={24} md={12}>
+					<h3>About US</h3>
+					<p>
+						Lorem Ipsum is simply dummy text of the printing and typesetting
+						industry. Lorem Ipsum has been the industry's standard dummy text
+						ever since the 1500s, when an unknown printer took a galley of type
+						and scrambled it to make a type specimen book. It has survived not
+						only five centuries, but also the leap into electronic typesetting,
+						remaining essentially unchanged.
+					</p>
+					<div className="social-icons">
+						<a href="#" title="twitter">
+							<FontAwesomeIcon icon={faTwitter} size="2x" />
+						</a>
+						<a href="#" title="facebook">
+							<FontAwesomeIcon icon={faFacebook} size="2x" />
+						</a>
+						<a href="#" title="instagram">
+							<FontAwesomeIcon icon={faInstagramSquare} size="2x" />
+						</a>
+					</div>
+				</Col>
+				<Col xs={24} sm={24} md={12}>
+					<Row
+						gutter={[
+							{ xs: 0, sm: 0, md: 30 },
+							{ xs: 0, sm: 0, md: 30 },
+						]}
+					>
+						<Col span={12}>
+							<h3>Calculators</h3>
+							<ul>
+								{calcList.map(({ name, link }) => (
+									<li key={name}>
+										<a href={link}>{name}</a>
+									</li>
+								))}
+							</ul>
+						</Col>
+						<Col span={12}>
+							<h3>Quick Links</h3>
+							<ul>
+								<li>
+									<a href="#">About US</a>
+								</li>
+								<li>
+									<a href="#">Contact US</a>
+								</li>
+								<li>
+									<a href="#">Terms and Conditions</a>
+								</li>
+								<li>
+									<a href="#">Privacy Policy</a>
+								</li>
+							</ul>
+						</Col>
+					</Row>
+				</Col>
+				<Col>&copy; 2021 Moneo. All rights reserved.</Col>
+				<Col className="disclaimer" xs={24}>
+					Disclaimer: This site is protected by reCAPTCHA and the Google{" "}
+					<a href="https://policies.google.com/privacy">Privacy Policy</a> and{" "}
+					<a href="https://policies.google.com/terms">Terms of Service</a>{" "}
+					apply.
+				</Col>
+			</Row>
 		</Content>
 	);
 }
--- src/components/feedback/Feedback.tsx
+++ src/components/feedback/Feedback.tsx
@@ -1,4 +1,4 @@
-import { Button, Form, Input, Radio, Divider } from "antd";
+import { Button, Form, Input, Radio, Divider, Row, Col } from "antd";
 import React, { useContext, useEffect, useRef } from "react";
 import Content from "../Content";
 import { FeedbackType } from "../../api/goals";
@@ -6,7 +6,16 @@ import { FormInstance } from "antd/lib/form";
 import { FeedbackContext } from "./FeedbackContext";
 import TextArea from "antd/lib/input/TextArea";
 import { useGoogleReCaptcha } from "react-google-recaptcha-v3";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faMapMarkerAlt,
+  faMobile,
+  faEnvelope,
+  faTools,
+} from "@fortawesome/free-solid-svg-icons";
 import { validateCaptcha } from "../utils";
+
+import "./Feedback.less";
 
 export default function Feedback() {
   const { executeRecaptcha } = useGoogleReCaptcha();
@@ -43,110 +52,172 @@ export default function Feedback() {
   };
 
   return (
-    <Content className="about">
+    <Content className="feedback">
       <h2>Contact US</h2>
       <Divider />
-      <Form
-        form={form}
-        ref={formRef}
-        name="submit"
-        layout="vertical"
-        initialValues={{
-          ["feedbackType"]: FeedbackType.C,
-        }}
-        onFinish={onFormSubmit}
-        size="large"
+      <Row
+        gutter={[
+          { xs: 0, sm: 0, md: 30, lg: 30 },
+          { xs: 0, sm: 0, md: 30, lg: 30 },
+        ]}
       >
-        <Form.Item name="feedbackType">
-          <Radio.Group value={feedbackType}>
-            <Radio.Button value={FeedbackType.C}>Comment</Radio.Button>
-            <Radio.Button value={FeedbackType.S}>Suggestion</Radio.Button>
-            <Radio.Button value={FeedbackType.Q}>Question</Radio.Button>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item
-          name="feedback"
-          label="Describe Your Feedback"
-          rules={[
-            {
-              required: true,
-              min: 10,
-              type: "string",
-              message: "Please enter valid feedback (min 10 characters)",
-            },
-          ]}
-        >
-          <TextArea rows={6} placeholder="Enter feedback" />
-        </Form.Item>
-        <Form.Item
-          name="firstName"
-          label="First Name"
-          style={{
-            display: "inline-block",
-            width: "calc(50% - 12px)",
-            marginRight: 10,
-          }}
-          rules={[
-            {
-              required: true,
-              type: "string",
-              message: "Please enter valid first name!",
-            },
-          ]}
-        >
-          <Input placeholder="Enter first name" />
-        </Form.Item>
-
-        <Form.Item
-          name="lastName"
-          label="Last Name"
-          style={{
-            display: "inline-block",
-            width: "calc(50% - 12px)",
-          }}
-          rules={[
-            {
-              required: false,
-              type: "string",
-              message: "Please enter valid last name!",
-            },
-          ]}
-        >
-          <Input placeholder="Enter last name" />
-        </Form.Item>
-
-        <Form.Item
-          name="email"
-          label="Email"
-          rules={[
-            {
-              required: true,
-              type: "email",
-              message: "Please enter valid email address!",
-            },
-          ]}
-        >
-          <Input placeholder="Enter email address" />
-        </Form.Item>
-        <Form.Item shouldUpdate={true}>
-          {() => (
-            <Button
-              type="primary"
-              htmlType="submit"
-              disabled={
-                form.getFieldsError().filter(({ errors }: any) => errors.length)
-                  .length > 0 ||
-                !formRef.current?.getFieldValue("feedback") ||
-                !formRef.current?.getFieldValue("firstName") ||
-                !formRef.current?.getFieldValue("email")
-              }
-              loading={isLoading}
+        <Col xs={24} sm={24} md={24} lg={12}>
+          <p>
+            If you have any quires or have any project fill free to contact us.
+            Our support team is available for you 24/7.
+          </p>
+          <Row
+            className="support-types"
+            gutter={[
+              { xs: 0, sm: 0, md: 20, lg: 20 },
+              { xs: 0, sm: 0, md: 20, lg: 20 },
+            ]}
+          >
+            <Col className="support-type" xs={24} sm={12}>
+              <FontAwesomeIcon icon={faMapMarkerAlt} size="3x" />
+              <div>
+                <strong>Our Location</strong>
+                <div>
+                  <p>907 N Randolph street, Denvar, United States</p>
+                </div>
+              </div>
+            </Col>
+            <Col className="support-type" xs={24} sm={12}>
+              <FontAwesomeIcon icon={faMobile} size="3x" />
+              <div>
+                <strong>Our Phone</strong>
+                <div>
+                  <p>+123 625 254 965</p>
+                  <p> +854 526 481 856</p>
+                </div>
+              </div>
+            </Col>
+            <Col className="support-type" xs={24} sm={12}>
+              <FontAwesomeIcon icon={faEnvelope} size="3x" />
+              <div>
+                <strong>Our E-mail</strong>
+                <div>
+                  <p>demo@gmail.com</p>
+                  <p> esample@gmail.com</p>
+                </div>
+              </div>
+            </Col>
+            <Col className="support-type" xs={24} sm={12}>
+              <FontAwesomeIcon icon={faTools} size="3x" />
+              <div>
+                <strong>Our Support</strong>
+                <div>
+                  <p>support@gmail.com</p>
+                  <p> example@gmail.com</p>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs={24} sm={24} md={24} lg={12}>
+          <Form
+            form={form}
+            ref={formRef}
+            name="submit"
+            layout="vertical"
+            initialValues={{
+              ["feedbackType"]: FeedbackType.C,
+            }}
+            onFinish={onFormSubmit}
+          >
+            <Form.Item name="feedbackType">
+              <Radio.Group value={feedbackType}>
+                <Radio.Button value={FeedbackType.C}>Comment</Radio.Button>
+                <Radio.Button value={FeedbackType.S}>Suggestion</Radio.Button>
+                <Radio.Button value={FeedbackType.Q}>Question</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+            <Form.Item
+              name="feedback"
+              label="Describe Your Feedback"
+              rules={[
+                {
+                  required: true,
+                  min: 10,
+                  type: "string",
+                  message: "Please enter valid feedback (min 10 characters)",
+                },
+              ]}
             >
-              Submit
-            </Button>
-          )}
-        </Form.Item>
-      </Form>
+              <TextArea rows={6} placeholder="Enter feedback" />
+            </Form.Item>
+            <Form.Item
+              name="firstName"
+              label="First Name"
+              style={{
+                display: "inline-block",
+                width: "calc(50% - 12px)",
+                marginRight: 10,
+              }}
+              rules={[
+                {
+                  required: true,
+                  type: "string",
+                  message: "Please enter valid first name!",
+                },
+              ]}
+            >
+              <Input placeholder="Enter first name" />
+            </Form.Item>
+
+            <Form.Item
+              name="lastName"
+              label="Last Name"
+              style={{
+                display: "inline-block",
+                width: "calc(50% - 12px)",
+              }}
+              rules={[
+                {
+                  required: false,
+                  type: "string",
+                  message: "Please enter valid last name!",
+                },
+              ]}
+            >
+              <Input placeholder="Enter last name" />
+            </Form.Item>
+
+            <Form.Item
+              name="email"
+              label="Email"
+              rules={[
+                {
+                  required: true,
+                  type: "email",
+                  message: "Please enter valid email address!",
+                },
+              ]}
+            >
+              <Input placeholder="Enter email address" />
+            </Form.Item>
+            <Form.Item shouldUpdate={true}>
+              {() => (
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  disabled={
+                    form
+                      .getFieldsError()
+                      .filter(({ errors }: any) => errors.length).length > 0 ||
+                    !formRef.current?.getFieldValue("feedback") ||
+                    !formRef.current?.getFieldValue("firstName") ||
+                    !formRef.current?.getFieldValue("email")
+                  }
+                  loading={isLoading}
+                >
+                  Submit
+                </Button>
+              )}
+            </Form.Item>
+          </Form>
+        </Col>
+      </Row>
     </Content>
   );
 }
