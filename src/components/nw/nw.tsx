a38375c6993e713e672864cfb26d60d8de698445
Umang Doctor <emailumangdoctor@gmail.com>
2021-03-01 03:08:34 +0530

    NSDL CAS parser

--- src/components/nw/nw.tsx
+++ src/components/nw/nw.tsx
@@ -1,40 +1,133 @@
-import { Col, Row, Upload } from 'antd';
-import React from 'react';
-import { PlusOutlined } from '@ant-design/icons';
+import { Col, Row, Tabs } from 'antd';
+import React, { Fragment, useState } from 'react';
 import * as pdfjsLib from 'pdfjs-dist';
 //@ts-ignore
 import pdfjsWorker from 'pdfjs-dist/build/pdf.worker.entry';
+import { appendValue } from '../utils';
+
+const isISIN = (val: string) => val.length === 12 && val.startsWith("IN") && !val.includes(" ");
+
+const getQty = (val: string, isMF: boolean = false) => {
+	val = val.replace(/,/g, "");
+	let result = parseInt(val);
+	if(Number.isNaN(result)) return null;
+	if (!isMF && val.includes(".")) return null;
+	if (val.includes(".")) {
+		if (val.split(".")[0].length > 6) return null;
+		return parseFloat(val);
+	} else {
+		if (val.length > 6) return null;
+		return result;
+	}
+}
 
 export default function NW() {
-	const processPDF = async ({ file }: any) => {
+	const [allEquities, setAllEquities] = useState<any>({});
+	const [allBonds, setAllBonds] = useState<any>({});
+	const [allMFs, setAllMFs] = useState<any>({});
+	const [fileParsing, setFileParsing] = useState<boolean>(false);
+	const { TabPane } = Tabs;
+
+	const parseHoldings = async (pdf: any) => {
+		let equities: any = {};
+		let mfs: any = {};
+		let bonds: any = {};
+		let mode = '';
+		for (let i = 1; i <= pdf.numPages; i++) {
+			const page = await pdf.getPage(i);
+			const textContent = await page.getTextContent();
+			if (i > 2 && i < pdf.numPages - 1) {
+				let isin = '';
+				for (let i = 0; i < textContent.items.length; i++) {
+					let value = textContent.items[i].str.trim();
+					if (value.length && value.length < 40 && value.split(" ").length < 5) {
+						if (value.includes("Page") || value.includes("Statement")
+							|| value.includes("NSDL") || value.includes("Your") ||
+							value.includes("Note") || value.includes(":") || value.includes("-"))
+							continue;
+						if (value.includes("Equity Shares")) {
+							mode = 'E';
+						} else if (mode && value.includes(" Bonds (")) {
+							mode = 'B';
+						} else if (isISIN(value)) {
+							isin = value;
+							if (isin.startsWith("INF")) {
+								mode = 'M';
+							}
+						} else if (isin) {
+							let qty = getQty(value, mode === 'M');
+							if (qty) {
+								appendValue(mode === 'E' ? equities : mode === 'B' ? bonds : mfs, isin, qty);
+								isin = '';
+							}
+						}
+					}
+				}
+			}
+		}
+		setAllBonds(bonds);
+		setAllEquities(equities);
+		setAllMFs(mfs);
+	};
+
+	const processPDF = (file: File) => {
+		setFileParsing(true);
 		const reader = new FileReader();
-		await reader.readAsArrayBuffer(file.originFileObj);
-		reader.onload = async () => {
-			let uintFile = new Uint8Array(reader.result as ArrayBuffer);
+		reader.readAsArrayBuffer(file);
+		reader.onload = () => {
 			pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-			const pdfLoadingTask = pdfjsLib.getDocument({ data: uintFile });
+			const pdfLoadingTask = pdfjsLib.getDocument({ data: new Uint8Array(reader.result as ArrayBuffer) });
 			pdfLoadingTask.onPassword = (pwdHandler: Function, response: any) => {
-				pwdHandler(
+				let retVal =
 					prompt(
 						response === pdfjsLib.PasswordResponses.INCORRECT_PASSWORD
 							? 'Invalid Password. Please try again.'
 							: 'Password'
-					)
-				);
+					);
+				if (retVal) pwdHandler(retVal);
+				else {
+					setFileParsing(false);
+				}
 			};
-			pdfLoadingTask.promise.then((pdf) => console.log(pdf));
+			pdfLoadingTask.promise.then(async (pdf) => {
+				await parseHoldings(pdf);
+				setFileParsing(false);
+			});
 		};
 		reader.onerror = (error: any) => console.log(error);
 	};
 
 	return (
+		<Fragment>
 		<Row justify="center">
 			<Col>
-				<Upload action="" listType="text" accept=".pdf" onChange={processPDF}>
-					<PlusOutlined />
-					<div style={{ marginTop: 8 }}>Upload</div>
-				</Upload>
+				<input id="fu" type="file" onChange={(event: any) => processPDF(event?.currentTarget.files[0])} accept=".pdf" />
 			</Col>
-		</Row>
+			</Row>
+			{!fileParsing &&
+				<Tabs defaultActiveKey="E" type="card">
+				<TabPane key="E" tab="Equities">
+					{Object.keys(allEquities)?.map((key: string, i: number) =>
+						<Row key={"stock" + i} justify="center">
+							{key}: {allEquities[key]}
+						</Row>
+					)}
+				</TabPane>
+				<TabPane key="B" tab="Bonds">
+					{Object.keys(allBonds)?.map((key: string, i: number) =>
+						<Row key={"bond" + i} justify="center">
+							{key}: {allBonds[key]}
+						</Row>
+					)}
+				</TabPane>
+				<TabPane key="M" tab="Mutual Funds">
+					{Object.keys(allMFs)?.map((key: string, i: number) =>
+						<Row key={"mf" + i} justify="center">
+							{key}: {allMFs[key]}
+						</Row>
+					)}
+				</TabPane>
+			</Tabs>}
+		</Fragment>
 	);
 }
