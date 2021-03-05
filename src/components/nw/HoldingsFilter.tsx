9f430a7c55ca8b9a19674a896ef748027dd9074a
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-03-05 17:16:30 +0530

    Updated holdings UI

---
+++ src/components/nw/HoldingsFilter.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Input } from "antd";
+
+export default function HoldingsFilter() {
+	return <Input placeholder="Filter holdings..." />;
+}
--- src/components/nw/HoldingsTable.tsx
+++ src/components/nw/HoldingsTable.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Empty } from "antd";
 import { toReadableNumber } from "../utils";
 import Holding from "./Holding";
+import HoldingsFilter from "./HoldingsFilter";
 
 import "./HoldingsTable.less";
 
@@ -13,13 +14,16 @@ interface HoldingsTableProp {
 export default function HoldingsTable({ data, insNames }: HoldingsTableProp) {
 	return Object.keys(data)?.length ? (
 		<>
+			<HoldingsFilter />
 			{Object.keys(data)?.map((key: string, i: number) => {
 				const qty = toReadableNumber(
 					data[key],
 					("" + data[key]).includes(".") ? 3 : 0
 				);
 
-				return <Holding key={i} assetName={insNames[key]} qty={qty} />;
+				return (
+					<Holding key={i} assetName={insNames[key]} qty={qty} isin={key} />
+				);
 			})}
 		</>
 	) : (
