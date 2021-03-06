3cf434ccab60a5142e930739ddb8ff7fc9950609
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-03-06 10:22:55 +0530

    Added add asset form

--- src/components/nw/AddHoldings.tsx
+++ src/components/nw/AddHoldings.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Modal, Button } from "antd";
+import { Modal, Button, Form, Select, Input } from "antd";
 import { PlusOutlined } from "@ant-design/icons";
 
 export default function AddHoldings() {
+	const { Option } = Select;
 	const [isModalVisible, setModalVisibility] = useState(false);
 
 	function showModal() {
@@ -21,12 +22,22 @@ export default function AddHoldings() {
 			<Modal
 				title="Add Asset"
 				visible={isModalVisible}
+				okText="Add"
 				onOk={onClose}
 				onCancel={onClose}
 			>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
-				<p>Some contents...</p>
+				<Form>
+					<Form.Item>
+						<Select placeholder="Search asset..." allowClear>
+							<Option value="reliance">Reliance</Option>
+							<Option value="sbin">SBIN</Option>
+							<Option value="bajajfinance">BAJAJFINANCE</Option>
+						</Select>
+					</Form.Item>
+					<Form.Item>
+						<Input placeholder="Enter quantity" />
+					</Form.Item>
+				</Form>
 			</Modal>
 		</div>
 	);
