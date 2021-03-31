18561f1c20e81e0d43b99b4d86997b4523f6e168
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-03-31 18:38:50 +0530

    Added dropdown in holding main heading

--- src/components/nw/HoldingsParser.tsx
+++ src/components/nw/HoldingsParser.tsx
@@ -4,7 +4,6 @@ import HoldingTabs from "./HoldingTabs";
 import HoldingsChart from "./HoldingsChart";
 import SearchFilter from "./SearchFilter";
 import DataSwitcher from "../DataSwitcher";
-import FilterHoldings from "./FilterHoldings";
 
 import "./nw.less";
 
@@ -16,7 +15,17 @@ export default function HoldingsParser() {
 		<div className="nw-container">
 			<div className="dd-stat">
 				<Statistic
-					title="Total Portfolio Value"
+					title={
+						<Select
+							defaultValue="total"
+							style={{ width: 200 }}
+							bordered={false}
+						>
+							<Option value="total">Total Portfolio Value</Option>
+							<Option value="ANZOIRU4526">ANZOIRU4526</Option>
+							<Option value="BMKJFI2578D">BMKJFI2578D</Option>
+						</Select>
+					}
 					value={213454654}
 					prefix={
 						<Select className="currency-selector" defaultValue="₹">
@@ -25,7 +34,6 @@ export default function HoldingsParser() {
 							<Option value="€">€</Option>
 						</Select>
 					}
-					suffix={<FilterHoldings />}
 				/>
 			</div>
 			<DataSwitcher title={<h3>Holdings details</h3>}>
