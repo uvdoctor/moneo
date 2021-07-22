6153e9733260ed7c73763d4ade4be0300e014485
uvdoctor <emailumangdoctor@gmail.com>
2021-07-22 14:44:16 +0530

    updated testimonials

--- src/components/landing/Testimonials.tsx
+++ src/components/landing/Testimonials.tsx
@@ -41,8 +41,7 @@ export default function Testimonials() {
 					imageLocation="images/testimonials/nipa.jpg"
 					content="I had taken a home loan of Rs. 25 lakhs over 20 years. After paying installments for 7
 				years, I was wondering if I should prepay the remaining loan of Rs. 17 lakhs to save interest or invest the money. By using
-				Moneo's loan calculator, I was instantly able to get answers to various scenarios. It even
-				allowed me to assess the impact of refinancing the loan. Finally, I decided to prepay the loan and get peace of mind. Moneo has now become my go-to coach for
+				Moneo's loan calculator, I was instantly able to get answers to various scenarios. I was also able to assess the impact of refinancing the loan. Finally, I decided to prepay the loan and get peace of mind. Moneo has now become my go-to coach for
 				financial analysis and planning."
 				/>
 
@@ -51,11 +50,8 @@ export default function Testimonials() {
 					name="Snehal"
 					location="Bangalore"
 					imageLocation="images/testimonials/snehal.jpg"
-					content="After working for 30 years, I have been keen to retire. While I am not
-				comfortable with financial jargon, using Moneo is a breeze. Within 15 minutes, I was able to
-				define my goals and understand the money needed for early retirement. I
-				also realized that I could retire within 5 years! Moneo presented detailed graphs and
-				information to give me a better understanding of my financial situation and the road ahead. My
+					content="While I am not comfortable with financial jargon, using Moneo is a breeze. Within 15 minutes, I was able to
+				define my goals and understand the money needed for retiring within 5 years! My
 				wife and I are so relaxed about our financial situation. I highly recommend Moneo to everyone."
 				/>
 
@@ -74,11 +70,8 @@ export default function Testimonials() {
 					name="Neha"
 					location="Bangalore"
 					imageLocation="images/testimonials/neha.jpg"
-					content="My family has been living on rent since 20 years, and my husband uses a rental car for his
-				office commute. I had been trying to convince him to buy an apartment and a car since sometime,
-				but he wasn't sure as it required us to spend most of our savings. This had become a hotly debated
-				topic in the family. Finally, Moneo came to our rescue. Using Moneo's Buy vs Rent and Invest
-				calculator, we realized that we are better off by Rs. 30 lakhs by continuing to rent the apartment and car."
+					content="My family is using a rental apartment and car. My husband and I were debating about whether to buy or continue renting. 
+					Moneo came to our rescue. Using Moneo's Buy vs Rent and Invest calculator, we realized that we are better off by Rs. 30 lakhs by continuing to rent."
 				/>
 
 			</Carousel>
