1b1e296be56cd6ed3a0432ec8da5d1fd5ae31167
mehzabeen000 <mehzabeen1526@gmail.com>
2021-08-13 15:11:11 +0530

    EODPrice Lambda

--- amplify/backend/function/EODPrice/src/index.js
+++ amplify/backend/function/EODPrice/src/index.js
@@ -1,19 +1,50 @@
-/* Amplify Params - DO NOT EDIT
-	ENV
-	REGION
-	EOD_API_KEY
-Amplify Params - DO NOT EDIT */
+const https = require("https");
+const AWS = require("aws-sdk");
+const urlParse = require("url").URL;
+const appsyncUrl = process.env.API_GOALS_GRAPHQLAPIENDPOINTOUTPUT;
+const region = process.env.REGION;
+const endpoint = new urlParse(appsyncUrl).hostname.toString();
+const graphqlQuery = require("./query.js").mutation;
+const apiKey = process.env.API_GOALS_GRAPHQLAPIKEYOUTPUT;
+const eodApiKey = process.env.EOD_API_KEY;
+const axios = require("axios");
+exports.handler = async (inputData) => {
+  const req = new AWS.HttpRequest(appsyncUrl, region);
+  const item = {
+    input: inputData,
+  };
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
+  req.method = "POST";
+  req.path = "/graphql";
+  req.headers.host = endpoint;
+  req.headers["Content-Type"] = "application/json";
+  req.body = JSON.stringify({
+    query: graphqlQuery,
+    variables: item,
+  });
+
+  if (apiKey) {
+    req.headers["x-api-key"] = apiKey;
+  } else {
+    const signer = new AWS.Signers.V4(req, "appsync", true);
+    signer.addAuthorization(AWS.config.credentials, AWS.util.date.getDate());
+  }
+
+  try {
+    const res = await axios.get(
+      `https://eodhistoricaldata.com/api/real-time/EUR.FOREX?api_token=${eodApiKey}&order=d&fmt=json`
+    );
+    console.log(res.data.close)
+    return {
+      statusCode: 200,
+      body: JSON.stringify(res.data.close)
     };
-    return response;
+  } catch (e) {
+    console.log(e);
+    return {
+      statusCode: 400,
+      body: JSON.stringify(e),
+    };
+  }
+  
 };
