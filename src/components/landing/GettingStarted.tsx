a7f8163e155fa9224778d083fff03b116813e195
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-08-17 20:01:58 +0530

    fixed getting started padding issue

--- src/components/landing/GettingStarted.tsx
+++ src/components/landing/GettingStarted.tsx
@@ -6,20 +6,22 @@ import { ROUTES } from "../../CONSTANTS";
 
 export default function GettingStarted() {
 	return (
-		<Col xs={24} sm={24} md={12} className="getting-started">
-			<h2>One-stop solution for Your family's financial concerns</h2>
-			<Image preview={false} src="images/kick-start.jpg" />
-			<p style={{ textAlign: "center" }}>
-				{/*defaultCountry === "IN" ? (*/}
+		<Col xs={24} sm={24} md={12}>
+			<div className="getting-started">
+				<h2>One-stop solution for Your family's financial concerns</h2>
+				<Image preview={false} src="images/kick-start.jpg" />
+				<p style={{ textAlign: "center" }}>
+					{/*defaultCountry === "IN" ? (*/}
 					<Link href={ROUTES.SET}>
 						<Button type="primary" size="large">
 							Get Started
 						</Button>
 					</Link>
-				{/*) : status !== Status.Y ? (
+					{/*) : status !== Status.Y ? (
 					<Join />
 				) : null}*/}
-			</p>
+				</p>
+			</div>
 		</Col>
 	);
 }
