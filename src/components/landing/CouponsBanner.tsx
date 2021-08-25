c915d71431840d0980ae88babe20b7d473813ad4
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-08-25 17:44:08 +0530

    coupons banners

--- src/components/landing/CouponsBanner.tsx
+++ src/components/landing/CouponsBanner.tsx
@@ -11,7 +11,7 @@ export default function CouponsBanner() {
 		<Content className="coupons-banner" whiteBg>
 			<h2>What Users Are Saying?</h2>
 			<Carousel
-				autoPlay={false}
+				autoPlay
 				interval={5000}
 				showThumbs={false}
 				showStatus={false}
@@ -41,6 +41,27 @@ export default function CouponsBanner() {
 				</div>
 				<div>
 					<img src="images/buy-banner.jpg" />
+					<div className="brand-coupon">
+						<Row
+							align="middle"
+							justify="center"
+							gutter={[
+								{ xs: 5, sm: 5, md: 15 },
+								{ xs: 10, sm: 10, md: 15 },
+							]}
+						>
+							{["amazon", "flipkart", "flipkart", "flipkart", "flipkart"].map(
+								(brand) => (
+									<Col>
+										<div className={`${brand}-coupon`}></div>
+									</Col>
+								)
+							)}
+						</Row>
+					</div>
+				</div>
+				<div>
+					<img src="images/event-banner.jpg" />
 					<div className="brand-coupon">
 						<Row
 							align="middle"
