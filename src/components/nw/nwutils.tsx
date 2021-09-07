eb9e546ca5c506b18237158e6c9e7ed8bc97c902
uvdoctor <emailumangdoctor@gmail.com>
2021-09-07 11:24:54 +0530

    fixed

--- src/components/nw/nwutils.tsx
+++ src/components/nw/nwutils.tsx
@@ -7,32 +7,6 @@ import { GOLD } from './NWContext';
 import { getFXRate } from '../utils';
 import { COLORS } from '../../CONSTANTS';
 
-export const createNewItem = async (item: APIt.CreateItemInput) => {
-	try {
-		const { data } = (await API.graphql(graphqlOperation(mutations.createItem, { input: item }))) as {
-			data: APIt.CreateItemMutation;
-		};
-		console.log('New item created in db: ', data ? data.createItem : '');
-		return data.createItem as APIt.CreateItemInput;
-	} catch (e) {
-		console.log('Error while creating item: ', e);
-		return null;
-	}
-};
-
-export const createNewAccount = async (account: APIt.CreateAccountInput) => {
-	try {
-		const { data } = (await API.graphql(graphqlOperation(mutations.createAccount, { input: account }))) as {
-			data: APIt.CreateAccountMutation;
-		};
-		console.log('New account created in db: ', data ? data.createAccount : '');
-		return data.createAccount as APIt.CreateAccountInput;
-	} catch (e) {
-		console.log('Error while creating account: ', e);
-		return null;
-	}
-};
-
 export const loadAllFamilyMembers = async () => {
 	const { data: { listFamilys } } = (await API.graphql(graphqlOperation(queries.listFamilys))) as {
 		data: APIt.ListFamilysQuery;
