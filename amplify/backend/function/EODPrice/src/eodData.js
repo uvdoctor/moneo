be3d56301aef6c37febd914d5f1a081b1683763f
Rahul Ranjan <rahulranjan@Rahuls-MacBook-Pro.local>
2021-09-10 12:11:49 +0530

    Updated condition for EODPrice update

--- amplify/backend/function/EODPrice/src/eodData.js
+++ amplify/backend/function/EODPrice/src/eodData.js
@@ -15,15 +15,17 @@ const getData = async (element, index) => {
     const { data } = await axios.get(eodURL(element.name, element.type, token));
     return data;
   } catch (error) {
-    const errMessage = error.response.statusText.toString();
-    const errCode = error.response.status.toString();
-    if (errMessage.includes("Payment Required") && errCode == 402) {
+    const { statusText, status } = error.response;
+    if (
+      statusText.toString().includes("Payment Required") &&
+      Number(status.toString()) == 402
+    ) {
       if (index <= apiKeys.eodhistoricaldata.length - 1) {
         index++;
         return await getData(element, index);
       }
     }
-    console.log(error.response.status);
+    console.log(status);
   }
 };
 
@@ -34,11 +36,12 @@ const pushData = async (data, table, index) => {
         [table]: data,
       },
     };
+    console.log(data)
     try {
       const updateRecord = await docClient.batchWrite(params).promise();
       resolve(updateRecord);
     } catch (error) {
-      reject(`Error in dynamoDB: ${JSON.stringify(error)} , ${index}`);
+      reject(`Error in dynamoDB: ${JSON.stringify(error)}, ${index}`);
     }
   });
 };
--- amplify/backend/function/EODPrice/src/index.js
+++ amplify/backend/function/EODPrice/src/index.js
@@ -2,21 +2,24 @@
 	AUTH_DDPWA0063633B_USERPOOLID
 	ENV
 	REGION
-Amplify Params - DO NOT EDIT */const eodData = require("./eodData");
+Amplify Params - DO NOT EDIT */
+
+const eodData = require("./eodData");
 const apiListData = require("./apiList");
+
 const { commodityAbbr, cryptoAbbr, currencyAbbr, apiToCall } = apiListData;
 const { getData, pushData } = eodData;
 const table = "EODPrices-4cf7om4zvjc4xhdn4qk2auzbdm-newdev";
+
 const eodPrice = () => {
   return new Promise(async (resolve, reject) => {
-    let batches = [];
     let batchRecords = [];
+    let batches = [];
+    let idsToLog = []
     let count = 0;
     await Promise.all(
       apiToCall.map(async (element) => {
-        index = 0;
-        const result = await getData(element, index);
-        let { code, close } = await result;
+        let { code, close } = await getData(element, 0);
 
         switch (element.type) {
           case commodityAbbr:
@@ -30,6 +33,7 @@ const eodPrice = () => {
             code = code.slice(0, code.lastIndexOf("."));
             break;
         }
+
         const dataToPush = {
           __typename: table.slice(0, table.indexOf("-")),
           id: code,
@@ -37,7 +41,9 @@ const eodPrice = () => {
           createdAt: new Date().toISOString(),
           updatedAt: new Date().toISOString(),
         };
+        idsToLog.push(code)
         batches.push({ PutRequest: { Item: dataToPush } });
+
         count++;
         if (count === 25) {
           batchRecords.push(batches);
@@ -46,9 +52,10 @@ const eodPrice = () => {
         }
       })
     );
-    if (count < 25 && batchRecords === []) {
+    if (count < 25) {
       batchRecords.push(batches);
     }
+    console.log("Data to update:", idsToLog);
     resolve(batchRecords);
   });
 };
