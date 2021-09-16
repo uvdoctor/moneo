d2ecb9cb5f6d0ac14fc13737ed7553b62b397e91
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-09-16 10:56:56 +0530

    updated insmeta to get and push data

---
+++ amplify/backend/function/insmeta/src/get&pushData.js
@@ -0,0 +1,59 @@
+const axios = require("axios");
+// const docClient = require("./insertIntoDB");
+const getData = async (url, mcap, indices, isinMap, table, batchRecords) => {
+  const schema = {};
+  let batches = [];
+  let count = 0;
+  const response = await axios.get(url);
+  response.data.data.map((item, index) => {
+    if (index === 0) return;
+    if (isinMap[item.meta.isin]) return;
+    schema.id = item.meta.isin;
+    schema.name = item.meta.companyName;
+    schema.mcap = mcap;
+    schema.yhigh = item.yearHigh;
+    schema.ylow = item.yearLow;
+    schema.ychg = item.perChange365d;
+    schema.mchg = item.perChange30d;
+    schema.ind = item.meta.industry;
+    schema.index = indices;
+    schema.__typename = table.slice(0, table.indexOf("-"));
+    schema.createdAt = new Date().toISOString();
+    schema.updatedAt = new Date().toISOString();
+    isinMap[item.meta.isin] = item.meta.isin;
+    batches.push({ PutRequest: { Item: schema } });
+    count++;
+    if (count === 25) {
+      batchRecords.push(batches);
+      batches = [];
+      count = 0;
+    }
+  });
+  if (count < 25) {
+    batchRecords.push(batches);
+  }
+
+  return batchRecords;
+};
+
+const pushData = async (data, table, index) => {
+  return new Promise(async (resolve, reject) => {
+    var params = {
+      RequestItems: {
+        [table]: data,
+      },
+    };
+    // try {
+    //   const updateRecord = await docClient.batchWrite(params).promise();
+    //   console.log(updateRecord);
+    //   resolve(updateRecord);
+    // } catch (error) {
+    //   reject(`Error in dynamoDB: ${JSON.stringify(error)}, ${index}`);
+    // }
+  });
+};
+
+module.exports = {
+  getData,
+  pushData,
+};
--- amplify/backend/function/insmeta/src/index.js
+++ amplify/backend/function/insmeta/src/index.js
@@ -4,16 +4,37 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
+const apiArray = require("./utils");
+const { getData, pushData } = require("./get&pushData");
+const table = "INExchgMeta-4cf7om4zvjc4xhdn4qk2auzbdm-newdev";
+const isinMap = {};
+const batchRecords = [];
+const getAndPushData = () => {
+  return new Promise(async (resolve, reject) => {
+    for (let i = 0; i < apiArray.length; i++) {
+      try {
+        const { url, mcap, indices } = apiArray[i];
+        const data = await getData(
+          url,
+          mcap,
+          indices,
+          isinMap,
+          table,
+          batchRecords
+        );
+        for (let batch in data) {
+          await pushData(data[batch], table, batch);
+        }
+      } catch (err) {
+        reject(err);
+      }
+    }
+    console.log(batchRecords.length);
+    resolve();
+  });
+};
+
+getAndPushData();
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  return await getAndPushData();
 };
