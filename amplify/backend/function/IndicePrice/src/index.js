bfc57cf7041276a08713f4da0d9108d659320a0a
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-09-21 14:20:20 +0530

    completed indiceprice function with bse and nse data

--- amplify/backend/function/IndicePrice/src/index.js
+++ amplify/backend/function/IndicePrice/src/index.js
@@ -3,16 +3,44 @@
 	ENV
 	REGION
 Amplify Params - DO NOT EDIT */
-const {getAllData, pushData} = require("/opt/nodejs/insertIntoDB"); 
-const apiArray = require("./utils");
+const fs = require("fs");
+const fsPromise = require("fs/promises");
+const { mkdir } = fsPromise;
+const { pushData } = require("/opt/nodejs/insertIntoDB");
+const { tempDir, apiArray, fileName, csvFile } = require("./utils");
 const getData = require("./getData");
+const calcInd = require("./calculate");
+const {
+  downloadZip,
+  extractDataFromCSV,
+  cleanDirectory,
+} = require("./bhavUtils");
 const table = "Indices-4cf7om4zvjc4xhdn4qk2auzbdm-newdev";
 
 const getAndPushData = async () => {
   for (let i = 0; i < apiArray.length; i++) {
     try {
-      const { url } = apiArray[i];
-      const data = await getData(url, table);
+      let dataFromNse;
+      const { url, cat, type, subt, schema, codes } = apiArray[i];
+      if (i === 0) {
+        if (fs.existsSync(tempDir)) {
+          await cleanDirectory(tempDir, "Initial cleaning completed");
+        }
+        await mkdir(tempDir);
+        await downloadZip(url, tempDir, csvFile);
+        dataFromNse = await extractDataFromCSV(tempDir, fileName);
+      }
+      const data = await getData(
+        dataFromNse,
+        table,
+        url,
+        cat,
+        type,
+        subt,
+        schema,
+        codes,
+        calcInd
+      );
       for (let batch in data) {
         const results = await pushData(data[batch], table, batch);
         console.log(results);
