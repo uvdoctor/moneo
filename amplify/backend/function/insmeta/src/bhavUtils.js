96455a3e1e54657b8318114631a4835c1f52217d
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-09-27 10:34:40 +0530

    updated insmeta with bhavutils layer file

--- amplify/backend/function/insmeta/src/bhavUtils.js
+++ amplify/backend/function/insmeta/src/bhavUtils.js
@@ -1,54 +1,13 @@
-const https = require("https");
 const fs = require("fs");
-const fsPromise = require("fs/promises");
-const { rmdir } = fsPromise;
-const extract = require("extract-zip");
 const excelToJson = require("convert-excel-to-json");
 
-const cleanDirectory = async (tempDir, msg) => {
-  await rmdir(tempDir, { recursive: true });
-  console.log(msg);
-};
-
-const downloadZip = (url, tempDir, file) => {
-  return new Promise((resolve, reject) => {
-    const req = https.get(url, async (res) => {
-      const { statusCode } = res;
-      if (statusCode < 200 || statusCode >= 300) {
-        await cleanDirectory(
-          tempDir,
-          `Unable to download zip file, ${statusCode}`
-        );
-        return reject(new Error("statusCode=" + statusCode));
-      }
-      res.pipe(fs.createWriteStream(file));
-      res.on("end", function () {
-        resolve();
-      });
-    });
-    req.on("error", async (e) => {
-      await cleanDirectory(
-        tempDir,
-        `Unable to download zip file, ${e.message}`
-      );
-      reject(e.message);
-    });
-    req.end();
-  });
-};
-
-const unzipDownloads = async (zipFile, tempDir) => {
-  try {
-    await extract(zipFile, {
-      dir: tempDir,
-    });
-  } catch (err) {
-    await cleanDirectory(tempDir, `Unable to extract zip file, ${err.message}`);
-    throw new Error(err.message);
-  }
-};
-
-const extractDataFromExcel = async (tempDir, fileName, table, isinMap) => {
+const extractDataFromExcel = async (
+  cleanDirectory,
+  tempDir,
+  fileName,
+  table,
+  isinMap
+) => {
   let batches = [];
   let count = 0;
   const batchRecords = [];
@@ -87,9 +46,4 @@ const extractDataFromExcel = async (tempDir, fileName, table, isinMap) => {
   return batchRecords;
 };
 
-module.exports = {
-  downloadZip,
-  unzipDownloads,
-  extractDataFromExcel,
-  cleanDirectory,
-};
+module.exports = extractDataFromExcel;
