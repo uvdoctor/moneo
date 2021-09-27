e1c6561104e6c5c4edb26597cb50f37d96bf3f54
Moneo <emailumangdoctor@gmail.com>
2021-09-27 13:27:45 +0530

    new layer moneoutils

---
+++ amplify/backend/function/moneoutils/lib/nodejs/insertIntoDB.js
@@ -0,0 +1,33 @@
+const AWS = require("aws-sdk");
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+const getAllData = async(table) =>{
+  const params = { TableName: table };
+  try {
+    const data = await docClient.scan(params).promise();
+    return data;
+  } catch (err) {
+    console.log(`Error in dynamoDB: ${JSON.stringify(err)}`);
+    return `Error in dynamoDB: ${JSON.stringify(err)}`;
+  }
+}
+
+const pushData = async (data, table) => {
+  return new Promise(async (resolve, reject) => {
+    const params = {
+      RequestItems: {
+        [table]: data,
+      },
+    };
+    try {
+      const updateRecord = await docClient.batchWrite(params).promise();
+      resolve(updateRecord);
+    } catch (error) {
+      reject(`Error in dynamoDB: ${JSON.stringify(error)}`)
+    }
+  });
+};
+
+const dummy = () => console.log("Dummy method");
+
+module.exports = {getAllData, pushData};
