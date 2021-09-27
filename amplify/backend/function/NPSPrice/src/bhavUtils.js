22383198d740c7880bb3836031bd6586407e195a
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-09-27 10:48:49 +0530

    updated npsprice with bhavutils  layer

--- amplify/backend/function/NPSPrice/src/bhavUtils.js
+++ amplify/backend/function/NPSPrice/src/bhavUtils.js
@@ -1,54 +1,13 @@
-const https = require("https");
 const fs = require("fs");
-const fsPromise = require("fs/promises");
-const { rmdir } = fsPromise;
-const extract = require("extract-zip");
 const split = require("split");
 
-const cleanDirectory = async (tempDir, msg) => {
-  await rmdir(tempDir, { recursive: true });
-  console.log(msg);
-};
-
-const downloadZip = (url, tempDir, file) => {
-  return new Promise((resolve, reject) => {
-    const req = https.get(url, async (res) => {
-      const { statusCode } = res;
-      if (statusCode < 200 || statusCode >= 300) {
-        await cleanDirectory(
-          tempDir,
-          `Unable to download zip file, ${statusCode}`
-        );
-        return reject(new Error("statusCode=" + statusCode));
-      }
-      res.pipe(fs.createWriteStream(file));
-      res.on("end", function () {
-        resolve();
-      });
-    });
-    req.on("error", async (e) => {
-      await cleanDirectory(
-        tempDir,
-        `Unable to download zip file, ${e.message}`
-      );
-      reject(e.message);
-    });
-    req.end();
-  });
-};
-
-const unzipDownloads = async (zipFile, tempDir) => {
-  try {
-    await extract(zipFile, {
-      dir: tempDir,
-    });
-  } catch (err) {
-    await cleanDirectory(tempDir, `Unable to extract zip file, ${err.message}`);
-    throw new Error(err.message);
-  }
-};
-
-const getDataFromTxtFile = async (tempDir, fileName, calc, table) => {
+const getDataFromTxtFile = async (
+  cleanDirectory,
+  tempDir,
+  fileName,
+  calc,
+  table
+) => {
   const end = new Promise((resolve, reject) => {
     let batches = [];
     let batchRecords = [];
@@ -103,9 +62,4 @@ const getDataFromTxtFile = async (tempDir, fileName, calc, table) => {
   return await end;
 };
 
-module.exports = {
-  downloadZip,
-  unzipDownloads,
-  cleanDirectory,
-  getDataFromTxtFile,
-};
+module.exports = getDataFromTxtFile;
