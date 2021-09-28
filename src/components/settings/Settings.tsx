3341f7db64e4b05eb574d0acde97c099436bee7f
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-09-28 17:14:21 +0530

    updated settings component

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,13 +1,37 @@
+import { Alert, Button, Col, Modal, Row, Tooltip, notification } from "antd";
 import React, { Fragment, useState } from "react";
 import TextInput from "../form/textinput";
-import { Button, Col, Alert, Row } from "antd";
+import { COLORS } from "../../CONSTANTS";
+import { SaveOutlined } from "@ant-design/icons";
 
 export default function Setting() {
-  const [email, setEmail] = useState<string>("mehz1526@gmail.com");
-  const [password, setPassword] = useState<string>("mehz@123");
+  const [mode, setMode] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
   const [name, setName] = useState<string>("Mehzabeen");
   const [contact, setContact] = useState<string>("8268552015");
   const [error, setError] = useState<string>("");
+  const [otp, setOtp] = useState<string>("");
+
+  const SAVE_MODE = "Save";
+
+  const save = () => {
+    try {
+      const otp = "12345";
+      if (otp === "12345") setMode("");
+      notification.success({
+        message: "Otp Updated",
+        description: `Success! Otp Updated.`,
+      });
+      return true;
+    } catch (err) {
+      notification.error({
+        message: "Invalid Otp",
+        description: "Sorry! Unable to update : " + err,
+      });
+      return false;
+    }
+  };
 
   return (
     <Fragment>
@@ -17,20 +41,34 @@ export default function Setting() {
           <p>&nbsp;</p>
         </Fragment>
       ) : null}
+      <Row justify="center">Settings</Row>
+      <p>&nbsp;</p>
       <Row justify="center">
-        <Col xs={24} sm={24} md={24} lg={12}>
-          <h2>Settings</h2>
+        <Col>
           <TextInput
             pre="Name"
             placeholder="Your Name"
             value={name}
             changeHandler={setName}
             fieldName="name"
-            pattern="^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$"
             setError={setError}
+            pattern="^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$"
             minLength={3}
           ></TextInput>
-          <p>&nbsp;</p>
+        </Col>
+        <Col>
+          <Tooltip title="Save">
+            <Button
+              type="link"
+              style={{ color: COLORS.GREEN }}
+              icon={<SaveOutlined />}
+            />
+          </Tooltip>
+        </Col>
+      </Row>
+      <p>&nbsp;</p>
+      <Row justify="center">
+        <Col>
           <TextInput
             pre="Contact"
             placeholder=""
@@ -40,36 +78,98 @@ export default function Setting() {
             setError={setError}
             minLength={8}
           ></TextInput>
-          <p>&nbsp;</p>
+        </Col>
+        <Col>
+          <Tooltip title="Save">
+            <Button
+              type="link"
+              style={{ color: COLORS.GREEN }}
+              icon={<SaveOutlined />}
+              onClick={() => setMode(SAVE_MODE)}
+            />
+          </Tooltip>
+        </Col>
+      </Row>
+      <p>&nbsp;</p>
+      <Row justify="center">
+        <Col>
           <TextInput
-            pre="Email Id"
+            pre="Email"
             placeholder=""
             value={email}
             changeHandler={setEmail}
             pattern="/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/"
             setError={setError}
-            fieldName="Email address"
+            fieldName="email"
             minLength={8}
           />
-          <p>&nbsp;</p>
+        </Col>
+        <Col>
+          <Tooltip title="Save">
+            <Button
+              type="link"
+              style={{ color: COLORS.GREEN }}
+              icon={<SaveOutlined />}
+              onClick={() => setMode(SAVE_MODE)}
+            />
+          </Tooltip>
+        </Col>
+      </Row>
+      <p>&nbsp;</p>
+      <Row justify="center">
+        <Col>
           <TextInput
             pre="Password"
-            placeholder="XXXXX1234X"
+            placeholder=""
             value={password}
             changeHandler={setPassword}
             fieldName="Password"
-            pattern="[A-Z]{5}[0-9]{4}[A-Z]{1}"
+            pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
             setError={setError}
             minLength={8}
             maxLength={20}
             password
           ></TextInput>
-          <p>&nbsp;</p>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
+        </Col>
+        <Col>
+          <Tooltip title="Save">
+            <Button
+              type="link"
+              style={{ color: COLORS.GREEN }}
+              icon={<SaveOutlined />}
+              onClick={() => setMode(SAVE_MODE)}
+            />
+          </Tooltip>
         </Col>
       </Row>
+      {mode && (
+        <Modal
+          title={`${mode} OTP`}
+          visible={mode.length > 0}
+          onCancel={() => setMode("")}
+          onOk={() => (mode ? save() : null)}
+          okText={"Save"}
+          okButtonProps={{
+            icon: <SaveOutlined />,
+          }}
+        >
+          {error ? (
+            <Fragment>
+              <Alert type="error" message={error} />
+              <p>&nbsp;</p>
+            </Fragment>
+          ) : null}
+          <TextInput
+            pre="OTP"
+            placeholder="Enter Otp"
+            value={otp}
+            changeHandler={setOtp}
+            fieldName="otp"
+            setError={setError}
+            minLength={5}
+          ></TextInput>
+        </Modal>
+      )}
     </Fragment>
   );
 }
