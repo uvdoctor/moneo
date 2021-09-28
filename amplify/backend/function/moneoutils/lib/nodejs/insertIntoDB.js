66ac60d0d7fc0e631f592bbfe1655b35115e33b2
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-09-28 10:46:40 +0530

    updated changes in insprice and layer

--- amplify/backend/function/moneoutils/lib/nodejs/insertIntoDB.js
+++ amplify/backend/function/moneoutils/lib/nodejs/insertIntoDB.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 const docClient = new AWS.DynamoDB.DocumentClient();
 
-const getAllData = async(table) =>{
+const getDataFromTable = async (table) => {
   const params = { TableName: table };
   try {
     const data = await docClient.scan(params).promise();
@@ -10,7 +10,7 @@ const getAllData = async(table) =>{
     console.log(`Error in dynamoDB: ${JSON.stringify(err)}`);
     return `Error in dynamoDB: ${JSON.stringify(err)}`;
   }
-}
+};
 
 const pushData = async (data, table) => {
   return new Promise(async (resolve, reject) => {
@@ -23,11 +23,33 @@ const pushData = async (data, table) => {
       const updateRecord = await docClient.batchWrite(params).promise();
       resolve(updateRecord);
     } catch (error) {
-      reject(`Error in dynamoDB: ${JSON.stringify(error)}`)
+      reject(`Error in dynamoDB: ${JSON.stringify(error)}`);
     }
   });
 };
 
-const dummy = () => console.log("Dummy method");
+const pushDataForFeed = async (table, data, identifier, url, exchg) => {
+  if (!identifier) identifier = "";
+  const feedData = [];
+  const tname = table.slice(0, table.indexOf("-"));
+  const tableName = "Feeds-4cf7om4zvjc4xhdn4qk2auzbdm-newdev";
+  const getLength = (arr) => {
+    const len = arr.flat(Infinity);
+    return len.length;
+  };
+  const schema = {
+    id: `${tname}_${identifier}`,
+    tname: tname,
+    count: getLength(data),
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+    __typename: tableName.slice(0, tableName.indexOf("-")),
+  };
+  if (exchg) schema.exchg = exchg;
+  if (url) schema.url = url;
+  feedData.push({ PutRequest: { Item: schema } });
+  const results = await pushData(feedData, tableName);
+  console.log(results, "Data Pushed into Feeds Table");
+};
 
-module.exports = {getAllData, pushData};
+module.exports = { getDataFromTable, pushData, pushDataForFeed };
