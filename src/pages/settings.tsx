a52f112081db7a6648b467ef2ecdabb30d651d19
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-09-29 12:18:32 +0530

    udate settings

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 import Amplify from 'aws-amplify';
 import awsmobile from '../aws-exports';
 import BasicPage from '../components/BasicPage';
-import Setting from '../components/settings/Settings';
+import UserSettings from '../components/settings/UserSettings';
 
 Amplify.configure(awsmobile);
 
 function Settings() {
 	return (
 		<BasicPage title="Settings">
-        <Setting />
+        <UserSettings />
 		</BasicPage>
 	);
 }
