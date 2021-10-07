b8d1e970690f936871671166224d8879c393eda3
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-10-07 11:48:12 +0530

    updated industry schema

--- amplify/backend/function/IndicePrice/src/calculate.js
+++ amplify/backend/function/IndicePrice/src/calculate.js
@@ -1,39 +1,37 @@
 const calcInd = (name) => {
-    name = name.toLowerCase().trim();
-    if (name.includes("basic materials")) return "BASM";
-    if (name.includes("consumer durables")) return "CD";
-    if (name.includes("energy")) return "E";
-    if (name.includes("fast moving") || name.includes("fmcg")) return "FMCG";
-    if (name.includes("financ")) return "F";
-    if (name.includes("healthcare")) return "H";
-    if (name.includes("pharma")) return "PH";
-    if (name.includes("industrials")) return "I";
-    if (name.includes("technology") || name.includes("nifty it")) return "IT";
-    if (name.includes("telecom")) return "TC";
-    if (name.includes("utilities")) return "U";
-    if (name.includes("auto")) return "A";
-    if (name.includes("bank")) return "B";
-    if (name.includes("capital goods")) return "CG";
-    if (name.includes("discretionary goods")) return "CDGS";
-    if (name.includes("metal")) return "MET";
-    if (name.includes("media")) return "MED";
-    if (name.includes("psu bank")) return "PSB";
-    if (name.includes("private bank")) return "PB";
-    if (name.includes("oil")) return "OG";
-    if (name.includes("power")) return "POW";
-    if (name.includes("realty")) return "R";
-    if (name.includes("teck")) return "TECH";
-    return null;
-  };
-  
-  const calcType = (name) => {
-    if (name.includes("G-Sec")) return "F";
-    return "E";
-  };
-  
-  const calcSubType = (name) => {
-    if (name.includes("G-Sec")) return "GB";
-    return "S";
-  };
-  
-  module.exports = { calcInd, calcType, calcSubType };
+	name = name.toLowerCase().trim();
+	if (name.includes('basic materials')) return 'BASM';
+	if (name.includes('consumer durables')) return 'CD';
+	if (name.includes('energy')) return 'E';
+	if (name.includes('fast moving') || name.includes('fmcg')) return 'CG';
+	if (name.includes('financ') || name.includes('bank') || name.includes('psu bank') || name.includes('private bank'))
+		return 'F';
+	if (name.includes('healthcare')) return 'H';
+	if (name.includes('pharma')) return 'PH';
+	if (name.includes('industrials')) return 'IM';
+	if (name.includes('technology') || name.includes('nifty it')) return 'IT';
+	if (name.includes('telecom')) return 'TC';
+	if (name.includes('utilities')) return 'U';
+	if (name.includes('auto')) return 'A';
+	if (name.includes('capital goods')) return 'CAPG';
+	if (name.includes('discretionary goods')) return 'CDGS';
+	if (name.includes('metal')) return 'MET';
+	if (name.includes('media')) return 'MED';
+	if (name.includes('oil')) return 'OG';
+	if (name.includes('power')) return 'POW';
+	if (name.includes('realty')) return 'C';
+	if (name.includes('teck')) return 'TECH';
+	return null;
+};
+
+const calcType = (name) => {
+	if (name.includes('G-Sec')) return 'F';
+	return 'E';
+};
+
+const calcSubType = (name) => {
+	if (name.includes('G-Sec')) return 'GB';
+	return 'S';
+};
+
+module.exports = { calcInd, calcType, calcSubType };
