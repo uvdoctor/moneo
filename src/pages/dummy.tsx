9a7685e10b81a3e5ccfc09fa2a436b9a1256ad4b
Moneo <emailumangdoctor@gmail.com>
2021-10-09 11:46:42 +0530

    new dummy page

---
+++ src/pages/dummy.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import BasicPage from '../components/BasicPage';
+
+export default function Dummy() {
+	return (
+		<BasicPage title="Dummy">
+			<h2>I am a dummy page</h2>
+		</BasicPage>
+	);
+}
