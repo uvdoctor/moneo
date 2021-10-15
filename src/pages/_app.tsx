d3a93c9fa3ebc315368699fad6dab3c37f1c2287
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-10-15 18:01:26 +0530

    updated changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,43 @@ import "../styles/index.less";
 import * as gtag from "../lib/gtag";
 import { Router } from "next/router";
 import React from "react";
+import {
+  AmplifyAuthenticator,
+  AmplifySignUp,
+} from "@aws-amplify/ui-react";
 
 Router.events.on("routeChangeComplete", (url) => gtag.pageview(url));
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+  return (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        // usernameAlias="email"
+        formFields={[
+					{
+            type: "username",
+            label: "Custom User Name",
+            placeholder: "Enter",
+						inputProps: { required: true, autocomplete: "username" },
+          },
+          {
+            type: "email",
+            label: "Custom Email Label",
+            placeholder: "Enter",
+            inputProps: { required: true, autocomplete: "email" , hint: "MAx length- 8"},
+          },
+          {
+            type: "password",
+            label: "Custom Password Label",
+            placeholder: "Enter",
+            inputProps: { required: true, autocomplete: "new-password" , hint: "MAx length- 8"},
+          },
+         
+        ]}
+      />
+      {/* <AmplifySignIn/> */}
+      <Component {...pageProps} />
+    </AmplifyAuthenticator>
+  );
 }
