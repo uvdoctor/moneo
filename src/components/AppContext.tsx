b40aa6efcf6096f75932c34bb24473430f9624b9
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-10-23 14:47:50 +0530

    update

--- src/components/AppContext.tsx
+++ src/components/AppContext.tsx
@@ -75,6 +75,8 @@ function AppContextProvider({ children }: AppContextProviderProps) {
 
 	const listener = async (capsule: any) => {
 		let eventType: string = capsule.payload.event;
+		console.log(eventType);
+		alert(eventType);
 		if (eventType === 'signIn') setUser(capsule.payload.data);
 		else if(eventType==="tokenRefresh")setUser(await Auth.currentAuthenticatedUser());
 		else setUser(null);
