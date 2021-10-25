921a8bb02b6b1dbd6a2ea19b93de7b0543d8bbbf
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-10-25 17:43:28 +0530

    updated getPage

--- src/pages/get.tsx
+++ src/pages/get.tsx
@@ -1,4 +1,9 @@
-import { AmplifyAuthenticator, AmplifySignUp, withAuthenticator } from "@aws-amplify/ui-react";
+import {
+  AmplifyAuthenticator,
+  AmplifyCheckbox,
+  AmplifyAuthFields,
+  AmplifyFormSection,
+} from "@aws-amplify/ui-react";
 import React from "react";
 import { NWContextProvider } from "../components/nw/NWContext";
 import Amplify from "aws-amplify";
@@ -7,35 +12,66 @@ import BasicPage from "../components/BasicPage";
 
 Amplify.configure(awsmobile);
 
-function Get() {
+function Get(this: any) {
+  // const [accept, setAccept] = useState<Boolean>(true);
+
+  // const handleRegistrationSubmit = (e: any) => {
+  //   console.log(e);
+
+  //   e.preventDefault();
+  //   const { username, password, email } = e.target;
+  //   if (accept) {
+  //     Auth.signUp({
+  //       username: username.value,
+  //       password: password.value,
+  //       attributes: {
+  //         email: email.value,
+  //       },
+  //     })
+  //       .then((response) => {
+  //         console.log("Auth.signIn success", response);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error.message);
+  //       });
+  //   } else alert("Please accept the terms and conditions.");
+  // };
+
   return (
     <AmplifyAuthenticator>
-      <AmplifySignUp
-        slot="sign-up"
-        formFields={[
-          {
-            type: "username",
-            label: "Username",
-            placeholder: "Enter",
-            inputProps: { required: true, autocomplete: "username" },
-            hint: "Your username is permanent and cannot be changed later",
-          },
-          {
-            type: "email",
-            label: "Email",
-            placeholder: "abc@xyz.com",
-            inputProps: { required: true, autocomplete: "email" },
-            hint: "Enter a valid email address",
-          },
-          {
-            type: "password",
-            label: "Password",
-            placeholder: "Enter",
-            inputProps: { required: true, autocomplete: "password" },
-            hint: "At least 8 characters. Must contain a number, lower and uppercase and a symbol.",
-          },
-        ]}
-      />
+      <AmplifyFormSection slot="sign-up">
+        <div slot="amplify-form-section-header">
+          <AmplifyAuthFields
+            formFields={[
+              {
+                type: "username",
+                label: "Username",
+                placeholder: "Enter",
+                inputProps: { required: true, autocomplete: "username" },
+                hint: "Your username is permanent and cannot be changed later",
+              },
+              {
+                type: "email",
+                label: "Email",
+                placeholder: "abc@xyz.com",
+                inputProps: { required: true, autocomplete: "email" },
+                hint: "Enter a valid email address",
+              },
+              {
+                type: "password",
+                label: "Password",
+                placeholder: "Enter",
+                inputProps: { required: true, autocomplete: "password" },
+                hint: "At least 8 characters. Must contain a number, lower and uppercase and a symbol.",
+              },
+            ]}
+          />
+        </div>
+        <div slot="amplify-form-section-footer">
+          <AmplifyCheckbox label="Terms and Conditions" checked={true} />
+          <AmplifyCheckbox label="Offer Letters" checked={true} />
+        </div>
+      </AmplifyFormSection>
       <BasicPage title="Get Real-time Analysis">
         <NWContextProvider />
       </BasicPage>
@@ -43,4 +79,4 @@ function Get() {
   );
 }
 
-export default withAuthenticator(Get);
+export default Get;
