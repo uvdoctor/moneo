a2c7564d8d40d0519a4b5a7fd0236bad91eeb670
Moneo <emailumangdoctor@gmail.com>
2021-11-01 23:53:18 +0530

    amplify push

--- amplify/backend/function/UserConfirmationTrigger/src/index.js
+++ amplify/backend/function/UserConfirmationTrigger/src/index.js
@@ -4,34 +4,29 @@ const table = 'RegEmail-fdun77s5lzbinkbgvnuidw6ihq-usdev';
 
 exports.handler = async (event, context) => {
 	let date = new Date();
-	console.log('Event request: ', event.request);
 	if (
-		event.request.userAttributes['cognito:user_status'] === 'CONFIRMED' &&
 		event.request.userAttributes['email_verified'] === 'true'
 	) {
 		let notify = event.request.userAttributes['custom:notify'];
+		let email = event.request.userAttributes.email
 		let params = {
 			Item      : {
 				__typename : { S: 'RegEmail' },
-				email      : { S: event.request.userAttributes.email },
+				email      : { S:  email},
 				notify     : { S: !notify || notify.length === 1 ? 'N' : 'Y' },
 				createdAt  : { S: date.toISOString() },
 				updatedAt  : { S: date.toISOString() }
 			},
 			TableName : table
 		};
-		// Call DynamoDB
 		try {
 			await ddb.putItem(params).promise();
-			console.log('Success');
+			console.log('Email has been inserted in the table: ', email);
 		} catch (err) {
-			console.log('Error', err);
+			console.log(`Error while inserting entry for ${email}: `, err);
 		}
-		console.log('Success: Everything executed correctly');
-		context.done(null, event);
 	} else {
-		// Nothing to do, the user's email ID is unknown
-		console.log('Error: Nothing was written to DynamoDB');
-		context.done(null, event);
+		console.log('Error: Nothing was written to table as email is not verified');
 	}
+	context.done(null, event);
 };
