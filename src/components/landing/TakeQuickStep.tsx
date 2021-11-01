167157f5e0561f77d7a84c248d1adbfc0f248392
Moneo <emailumangdoctor@gmail.com>
2021-11-01 10:20:54 +0530

    fixed

--- src/components/landing/TakeQuickStep.tsx
+++ src/components/landing/TakeQuickStep.tsx
@@ -1,22 +1,21 @@
-import React, { useContext } from "react";
-import { Row, Col } from "antd";
-import Content from "../Content";
-import { JoinContext } from "./JoinContext";
-import { Status } from "../../api/goals";
+import React from 'react';
+import { Row, Col } from 'antd';
+import Content from '../Content';
 
-import "./TakeQuickStep.less";
-import GetStartedButton from "./GetStartedButton";
+import './TakeQuickStep.less';
+import GetStartedButton from './GetStartedButton';
 
 export default function TakeQuickStep() {
-	const { status }: any = useContext(JoinContext);
-
-	return status !== Status.Y ? (
+	return (
 		<Content className="take-quick-step" whiteBg>
-			<Row align="middle" gutter={[50, 0]}>
+			<Row
+				align="middle"
+				gutter={[
+					50,
+					0
+				]}>
 				<Col xs={24} sm={24} md={12}>
-					<h2 className="text-green-primary">
-						Just 15 minutes for a personalized Financial Plan
-					</h2>
+					<h2 className="text-green-primary">Just 15 minutes for a personalized Financial Plan</h2>
 					<p>&nbsp;</p>
 					<GetStartedButton />
 				</Col>
@@ -25,5 +24,5 @@ export default function TakeQuickStep() {
 				</Col>
 			</Row>
 		</Content>
-	) : null;
+	);
 }
