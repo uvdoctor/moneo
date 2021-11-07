7bbf9c6fc44b7215bfc533aae22185d1144a3a66
Moneo <emailumangdoctor@gmail.com>
2021-11-08 00:15:01 +0530

    updated webpack config to include only src

--- next.config.js
+++ next.config.js
@@ -41,7 +41,8 @@ module.exports = withPlugins(
     //swcMinify: true,
     rules: [
       {
-        exclude: [path.resolve(__dirname, "./node_modules")], // in order to ignore all modules in node_modules folder
+        test: /\.tsx?$/,
+        include: [path.resolve(__dirname, "src")]
       },
     ],
     externalsPresets: {
