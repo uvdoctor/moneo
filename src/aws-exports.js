5aa5b02df1874d9bc8c9b43287460e33b16ad894
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-11-22 13:14:32 +0530

    update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -16,33 +16,6 @@ const awsmobile = {
             "region": "us-east-1"
         }
     ],
-    "aws_cognito_identity_pool_id": "us-east-1:7d526af8-e47d-4096-a54f-b16374b43418",
-    "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_3N0D6fw9V",
-    "aws_user_pools_web_client_id": "5lnl445333npfc9tj5rlcrd9vo",
-    "oauth": {},
-    "aws_cognito_username_attributes": [],
-    "aws_cognito_social_providers": [],
-    "aws_cognito_signup_attributes": [
-        "EMAIL"
-    ],
-    "aws_cognito_mfa_configuration": "OPTIONAL",
-    "aws_cognito_mfa_types": [
-        "SMS",
-        "TOTP"
-    ],
-    "aws_cognito_password_protection_settings": {
-        "passwordPolicyMinLength": 8,
-        "passwordPolicyCharacters": [
-            "REQUIRES_LOWERCASE",
-            "REQUIRES_UPPERCASE",
-            "REQUIRES_NUMBERS",
-            "REQUIRES_SYMBOLS"
-        ]
-    },
-    "aws_cognito_verification_mechanisms": [
-        "EMAIL"
-    ],
     "aws_cognito_login_mechanisms": [
         "PREFERRED_USERNAME"
     ]
