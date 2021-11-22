36675fd0c95705530a3918ff74465b0454e4f936
Moneo <emailumangdoctor@gmail.com>
2021-11-22 11:37:44 +0530

    amplify push

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,6 +3,19 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
+    "aws_appsync_graphqlEndpoint": "https://2x5orxpn4vgtdmtheb3q6h5tuu.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
+    "aws_user_files_s3_bucket": "goal-images104733-dev",
+    "aws_user_files_s3_bucket_region": "us-east-1",
+    "aws_bots": "enable",
+    "aws_bots_config": [
+        {
+            "name": "ScheduleAppointment_dev",
+            "alias": "$LATEST",
+            "region": "us-east-1"
+        }
+    ],
     "aws_cognito_identity_pool_id": "us-east-1:7d526af8-e47d-4096-a54f-b16374b43418",
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_3N0D6fw9V",
@@ -29,19 +42,6 @@ const awsmobile = {
     },
     "aws_cognito_verification_mechanisms": [
         "EMAIL"
-    ],
-    "aws_appsync_graphqlEndpoint": "https://j6qsr3lbtvchnl6yhykwelxj2m.appsync-api.us-east-1.amazonaws.com/graphql",
-    "aws_appsync_region": "us-east-1",
-    "aws_appsync_authenticationType": "AMAZON_COGNITO_USER_POOLS",
-    "aws_user_files_s3_bucket": "goal-images115157-usdev",
-    "aws_user_files_s3_bucket_region": "us-east-1",
-    "aws_bots": "enable",
-    "aws_bots_config": [
-        {
-            "name": "ScheduleAppointment_usdev",
-            "alias": "$LATEST",
-            "region": "us-east-1"
-        }
     ],
     "aws_cognito_login_mechanisms": [
         "PREFERRED_USERNAME"
