1319c35c73afa535834dbfb266357b83720ef19a
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-11-23 11:01:03 +0530

    update

--- amplify/backend/function/moneobacb277ebacb277ePostConfirmation/src/custom.js
+++ amplify/backend/function/moneobacb277ebacb277ePostConfirmation/src/custom.js
@@ -2,12 +2,11 @@ const { getDataFromEventAndPush } = require('./operation');
 
 exports.handler = async (event, context) => {
 	// insert code to be executed by your lambda trigger
+	if(context) console.log(context);
 	const email = event.request.userAttributes.email;
 	try {
 		await getDataFromEventAndPush(event);
 	} catch (err) {
 		console.log(`${err} in ${email}`);
-	} finally {
-		context.done(null, event);
 	}
 };
