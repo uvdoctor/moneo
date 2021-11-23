849f8bfa10b23f59ddb6fbc33d3e620cee386f11
Moneo <emailumangdoctor@gmail.com>
2021-11-23 11:17:44 +0530

    updated authenticator

--- src/components/BasicAuthenticator.tsx
+++ src/components/BasicAuthenticator.tsx
@@ -18,7 +18,6 @@ interface BasicAuthenticatorProps {
 }
 
 Amplify.configure(awsconfig);
-Auth.configure({authenticationFlowType: "USER_PASSWORD_AUTH"});
 
 export default function BasicAuthenticator({ children }: BasicAuthenticatorProps) {
   const { executeRecaptcha } = useGoogleReCaptcha();
