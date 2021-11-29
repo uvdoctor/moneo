e85400fbb3f5545f1e10365828e41dc1494cfd90
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-11-29 11:34:13 +0530

    Updated add property ui

--- src/components/form/numberinput.tsx
+++ src/components/form/numberinput.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { getRangeFactor, parseNumber, toCurrency, toHumanFriendlyCurrency, toReadableNumber } from '../utils';
-import { Slider } from 'antd';
-import { COLORS } from '../../CONSTANTS';
-import { Tooltip, InputNumber, Row, Col } from 'antd';
-import { InfoCircleOutlined } from '@ant-design/icons';
+import React, { useEffect, useRef, useState } from "react";
+import {
+	getRangeFactor,
+	parseNumber,
+	toCurrency,
+	toHumanFriendlyCurrency,
+	toReadableNumber,
+} from "../utils";
+import { Slider } from "antd";
+import { COLORS } from "../../CONSTANTS";
+import { Tooltip, InputNumber, Row, Col } from "antd";
+import { InfoCircleOutlined } from "@ant-design/icons";
 interface NumberInputProps {
 	info?: string;
 	pre: any;
@@ -24,48 +30,47 @@ interface NumberInputProps {
 
 export default function NumberInput(props: NumberInputProps) {
 	const inputRef = useRef(null);
-	const [ rangeFactor, setRangeFactor ] = useState<number>(
+	const [rangeFactor, setRangeFactor] = useState<number>(
 		props.currency && !props.noRangeFactor ? getRangeFactor(props.currency) : 1
 	);
-	const [ sliderBorderColor, setSliderBorderColor ] = useState<string>(COLORS.GREEN);
-	const [ feedbackText, setFeedbackText ] = useState<string>('');
-	const [ minNum, setMinNum ] = useState<number>(props.min * rangeFactor);
-	const [ maxNum, setMaxNum ] = useState<number>(props.max * rangeFactor);
-	const [ stepNum, setStepNum ] = useState<number>(props.step * rangeFactor);
+	const [sliderBorderColor, setSliderBorderColor] = useState<string>(
+		COLORS.GREEN
+	);
+	const [feedbackText, setFeedbackText] = useState<string>("");
+	const [minNum, setMinNum] = useState<number>(props.min * rangeFactor);
+	const [maxNum, setMaxNum] = useState<number>(props.max * rangeFactor);
+	const [stepNum, setStepNum] = useState<number>(props.step * rangeFactor);
 
 	const getSliderMarks = (min: number, max: number) => {
 		let marks: any = {
-			[min]: toReadableNumber(min)
+			[min]: toReadableNumber(min),
 		};
 		if (min < 0) marks[0] = toReadableNumber(0);
-		if (props.additionalMarks) props.additionalMarks.forEach((val: number) => (marks[val] = toReadableNumber(val)));
+		if (props.additionalMarks)
+			props.additionalMarks.forEach(
+				(val: number) => (marks[val] = toReadableNumber(val))
+			);
 		marks[max] = toReadableNumber(max);
 		return marks;
 	};
 
-	const [ marks, setMarks ] = useState<any>(getSliderMarks(props.min, props.max));
+	const [marks, setMarks] = useState<any>(getSliderMarks(props.min, props.max));
 
-	useEffect(
-		() => {
-			let minNum = props.min * rangeFactor;
-			let maxNum = props.max * rangeFactor;
-			let stepNum = props.step * rangeFactor;
-			setMinNum(minNum);
-			setMaxNum(maxNum);
-			setStepNum(stepNum);
-			setMarks(getSliderMarks(minNum, maxNum));
-		},
-		[ rangeFactor, props.min, props.max ]
-	);
+	useEffect(() => {
+		let minNum = props.min * rangeFactor;
+		let maxNum = props.max * rangeFactor;
+		let stepNum = props.step * rangeFactor;
+		setMinNum(minNum);
+		setMaxNum(maxNum);
+		setStepNum(stepNum);
+		setMarks(getSliderMarks(minNum, maxNum));
+	}, [rangeFactor, props.min, props.max]);
 
-	useEffect(
-		() => {
-			if (!props.currency || props.noRangeFactor) return;
-			let rf = getRangeFactor(props.currency as string);
-			if (rf !== rangeFactor) setRangeFactor(rf);
-		},
-		[ props.currency ]
-	);
+	useEffect(() => {
+		if (!props.currency || props.noRangeFactor) return;
+		let rf = getRangeFactor(props.currency as string);
+		if (rf !== rangeFactor) setRangeFactor(rf);
+	}, [props.currency]);
 
 	const getClosestKey = (value: number, keys: Array<number>) => {
 		let result: number = keys[0];
@@ -79,11 +84,13 @@ export default function NumberInput(props: NumberInputProps) {
 	const provideFeedback = (val: number) => {
 		if (props.feedback) {
 			let allKeys = Object.keys(props.feedback);
-			let allSortedKeys = allKeys.map((k) => parseFloat(k)).sort((a, b) => a - b);
+			let allSortedKeys = allKeys
+				.map((k) => parseFloat(k))
+				.sort((a, b) => a - b);
 			let feedback: any = props.feedback[getClosestKey(val, allSortedKeys)];
 			if (!feedback || !feedback.label) {
-				setSliderBorderColor('white');
-				setFeedbackText('');
+				setSliderBorderColor("white");
+				setFeedbackText("");
 			} else {
 				setSliderBorderColor(feedback.color);
 				setFeedbackText(feedback.label);
@@ -92,28 +99,36 @@ export default function NumberInput(props: NumberInputProps) {
 	};
 
 	return (
-		<div style={{ minWidth: '250px' }}>
+		<div style={{ minWidth: "250px" }}>
 			<Col span={24}>
-					{!props.noSlider && <Row justify="space-between" align="middle">
-					<Col>
-						{props.pre}
-						{props.post && ` ${props.post}`}
-						{props.info && (
-							<Tooltip title={props.info}>
-								<span>
-									<InfoCircleOutlined />
-								</span>
-							</Tooltip>
+				{!props.noSlider && (
+					<Row justify="space-between" align="middle">
+						<Col>
+							{props.pre}
+							{props.post && ` ${props.post}`}
+							{props.info && (
+								<Tooltip title={props.info}>
+									<span>
+										<InfoCircleOutlined />
+									</span>
+								</Tooltip>
+							)}
+						</Col>
+						{!props.currency && props.step >= 1 && (
+							<b>{`${toReadableNumber(
+								props.value,
+								props.step && props.step < 1 ? 2 : 0
+							)} ${props.unit}`}</b>
 						)}
-					</Col>
-					{!props.currency &&
-					props.step >= 1 && (
-						<b>{`${toReadableNumber(props.value, props.step && props.step < 1 ? 2 : 0)} ${props.unit}`}</b>
-					)}
-				</Row>}
+					</Row>
+				)}
 			</Col>
 			<Col span={24}>
-				<Row justify={props.noSlider?"center":"space-between"} align="top" style={{ marginBottom: '1.5rem' }}>
+				<Row
+					justify={props.noSlider ? "center" : "space-between"}
+					align="top"
+					style={{ marginBottom: "1.5rem" }}
+				>
 					{props.noSlider && <Col>{props.pre}&nbsp;</Col>}
 					{(props.currency || props.step < 1) && (
 						<Col span={10}>
@@ -132,11 +147,13 @@ export default function NumberInput(props: NumberInputProps) {
 										formatter={(val) =>
 											props.currency
 												? toCurrency(val as number, props.currency as string)
-												: toReadableNumber(val as number, 2)}
+												: toReadableNumber(val as number, 2)
+										}
 										parser={(val) =>
 											props.currency
 												? parseFloat(parseNumber(val as string, props.currency))
-												: parseFloat(val as string)}
+												: parseFloat(val as string)
+										}
 										onPressEnter={(e: any) => {
 											e.preventDefault();
 											//@ts-ignore
@@ -144,11 +161,14 @@ export default function NumberInput(props: NumberInputProps) {
 										}}
 										onBlur={(e: any) => {
 											let num = props.currency
-												? parseInt(parseNumber(e.currentTarget.value, props.currency))
+												? parseInt(
+														parseNumber(e.currentTarget.value, props.currency)
+												  )
 												: parseFloat(e.currentTarget.value);
-											if (!num || num < props.min) props.changeHandler(props.min);
+											if (!num || num < props.min)
+												props.changeHandler(props.min);
 										}}
-										style={{ width: '100%', marginBottom: '0px' }}
+										style={{ width: "100%", marginBottom: "0px" }}
 									/>
 								</Col>
 								{!props.currency ? (
@@ -160,13 +180,17 @@ export default function NumberInput(props: NumberInputProps) {
 							</Row>
 							{props.currency && props.value >= 100000 ? (
 								<Row>
-									<Col>{`~ ${toHumanFriendlyCurrency(props.value, props.currency)}`}</Col>
+									<Col>{`~ ${toHumanFriendlyCurrency(
+										props.value,
+										props.currency
+									)}`}</Col>
 								</Row>
 							) : null}
 						</Col>
 					)}
-					{props.noSlider ? null : (<Col span={props.currency || props.step < 1 ? 13 : 24}>
-						{/*@ts-ignore: JSX element class does not support attributes because it does not have a 'props' property.*/}
+					{props.noSlider ? null : (
+						<Col span={props.currency || props.step < 1 ? 13 : 24}>
+							{/*@ts-ignore: JSX element class does not support attributes because it does not have a 'props' property.*/}
 							<Slider
 								min={props.min * rangeFactor}
 								max={props.max * rangeFactor}
@@ -178,16 +202,17 @@ export default function NumberInput(props: NumberInputProps) {
 									props.changeHandler(val);
 								}}
 								handleStyle={{
-									cursor: 'grab',
-									borderColor: sliderBorderColor
+									cursor: "grab",
+									borderColor: sliderBorderColor,
 								}}
 							/>
-						{feedbackText && (
-							<Col span={24} style={{ textAlign: 'center' }}>
-								{feedbackText}
-							</Col>
-						)}
-					</Col>)}
+							{feedbackText && (
+								<Col span={24} style={{ textAlign: "center" }}>
+									{feedbackText}
+								</Col>
+							)}
+						</Col>
+					)}
 				</Row>
 			</Col>
 			{props.note && <Col span={24}>{props.note}</Col>}
--- src/components/nw/AddPropertiesInput.tsx
+++ src/components/nw/AddPropertiesInput.tsx
@@ -1,16 +1,29 @@
-import { PlusOutlined, DeleteOutlined, UserOutlined } from '@ant-design/icons';
-import { Form, Button, Checkbox, Col, InputNumber, Row, Alert, Tooltip } from 'antd';
-import React, { useContext, useEffect, useState } from 'react';
-import { OwnershipInput, PropertyInput } from '../../api/goals';
-import { COLORS } from '../../CONSTANTS';
-import { getCompoundedIncome } from '../calc/finance';
-import DatePickerInput from '../form/DatePickerInput';
-import NumberInput from '../form/numberinput';
-import SelectInput from '../form/selectinput';
-import TextInput from '../form/textinput';
-import { getMonthIndex } from '../utils';
-import { NWContext } from './NWContext';
-import { getDefaultMember, getFamilyOptions, getRemainingDuration } from './nwutils';
+import { PlusOutlined, DeleteOutlined, UserOutlined } from "@ant-design/icons";
+import {
+	Form,
+	Button,
+	Checkbox,
+	Col,
+	InputNumber,
+	Row,
+	Alert,
+	Tooltip,
+} from "antd";
+import React, { useContext, useEffect, useState } from "react";
+import { OwnershipInput, PropertyInput } from "../../api/goals";
+import { COLORS } from "../../CONSTANTS";
+import { getCompoundedIncome } from "../calc/finance";
+import DatePickerInput from "../form/DatePickerInput";
+import NumberInput from "../form/numberinput";
+import SelectInput from "../form/selectinput";
+import TextInput from "../form/textinput";
+import { getMonthIndex } from "../utils";
+import { NWContext } from "./NWContext";
+import {
+	getDefaultMember,
+	getFamilyOptions,
+	getRemainingDuration,
+} from "./nwutils";
 
 interface AddPropertiesInputProps {
 	setInput: Function;
@@ -18,33 +31,41 @@ interface AddPropertiesInputProps {
 	categoryOptions: any;
 }
 
-export default function AddPropertyInput({ setInput, disableOk, categoryOptions }: AddPropertiesInputProps) {
-	const { allFamily, selectedMembers, selectedCurrency }: any = useContext(NWContext);
-	const [ subtype, setSubtype ] = useState<string>('P');
-	const [ own, setOwn ] = useState<Array<OwnershipInput>>([]);
-	const [ pin, setPin ] = useState<any>('');
-	const [ memberKey, setMemberKey ] = useState<string>(getDefaultMember(allFamily, selectedMembers));
-	const [ rate, setRate ] = useState<number>(8);
-	const [ amount, setAmount ] = useState<number>(0);
-	const [ purchaseDate, setPurchaseDate ] = useState<string>('');
-	const [ city, setCity ] = useState<string>('');
-	const [ address, setAddress ] = useState<string>('');
-	const [ mv, setMv ] = useState<number>(0);
-	const [ mvy, setMvy ] = useState<number>(new Date().getFullYear());
-	const [ mvm, setMvm ] = useState<number>(new Date().getMonth() + 1);
-	const [ state, setState ] = useState<string>('');
-	const [ name, setName ] = useState<string>('');
-	const [ res, setRes ] = useState<boolean>(false);
-	const [ error, setError ] = useState<boolean>(false);
+export default function AddPropertyInput({
+	setInput,
+	disableOk,
+	categoryOptions,
+}: AddPropertiesInputProps) {
+	const { allFamily, selectedMembers, selectedCurrency }: any = useContext(
+		NWContext
+	);
+	const [subtype, setSubtype] = useState<string>("P");
+	const [own, setOwn] = useState<Array<OwnershipInput>>([]);
+	const [pin, setPin] = useState<any>("");
+	const [memberKey, setMemberKey] = useState<string>(
+		getDefaultMember(allFamily, selectedMembers)
+	);
+	const [rate, setRate] = useState<number>(8);
+	const [amount, setAmount] = useState<number>(0);
+	const [purchaseDate, setPurchaseDate] = useState<string>("");
+	const [city, setCity] = useState<string>("");
+	const [address, setAddress] = useState<string>("");
+	const [mv, setMv] = useState<number>(0);
+	const [mvy, setMvy] = useState<number>(new Date().getFullYear());
+	const [mvm, setMvm] = useState<number>(new Date().getMonth() + 1);
+	const [state, setState] = useState<string>("");
+	const [name, setName] = useState<string>("");
+	const [res, setRes] = useState<boolean>(false);
+	const [error, setError] = useState<boolean>(false);
 
 	const duration = () => {
 		let rec = getNewRec();
-		if(rec.purchase) {
-			const data = getRemainingDuration(rec.purchase.year,rec.purchase.month);
+		if (rec.purchase) {
+			const data = getRemainingDuration(rec.purchase.year, rec.purchase.month);
 			return data?.years;
 		}
-	}
-	
+	};
+
 	const ownerPercent = () => {
 		let count = 0;
 		own.map((item: any) => (count += item.per));
@@ -92,7 +113,7 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 		setAmount(amt);
 		disableOk(amt <= 0);
 		let rec = getNewRec();
-		rec.purchase ? (rec.purchase.amt = amt) : '';
+		rec.purchase ? (rec.purchase.amt = amt) : "";
 		setInput(rec);
 	};
 
@@ -102,7 +123,7 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 		const month = getMonthIndex(val.substring(0, 3));
 		if (rec.purchase) {
 			rec.purchase.month = month;
-			rec.purchase.year = Number(val.substring(val.length-4));
+			rec.purchase.year = Number(val.substring(val.length - 4));
 		}
 		setInput(rec);
 	};
@@ -116,9 +137,11 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 
 	const changePin = async (val: any) => {
 		setPin(val);
-		if (selectedCurrency === 'INR') {
+		if (selectedCurrency === "INR") {
 			if (val.length === 6) {
-				const response = await fetch(`https://api.postalpincode.in/pincode/${val}`);
+				const response = await fetch(
+					`https://api.postalpincode.in/pincode/${val}`
+				);
 				const data = await response.json();
 				setState(data[0].PostOffice[0].State);
 				setCity(data[0].PostOffice[0].District);
@@ -143,12 +166,12 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 			purchase: {
 				amt: amount,
 				month: getMonthIndex(purchaseDate.substring(0, 3)),
-				year: Number(purchaseDate.substring(purchaseDate.length-4)),
-				qty: 1
+				year: Number(purchaseDate.substring(purchaseDate.length - 4)),
+				qty: 1,
 			},
 			address: address,
 			curr: selectedCurrency,
-			country: selectedCurrency === 'INR' ? 'India' : 'US',
+			country: selectedCurrency === "INR" ? "India" : "US",
 			state: state,
 			city: city,
 			own: own,
@@ -157,7 +180,7 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 			mvy: mvy,
 			mvm: mvm,
 			res: res,
-			name: name
+			name: name,
 		};
 		return newRec;
 	};
@@ -172,12 +195,12 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 	const changeMember = (i: number, key: string) => {
 		setMemberKey(key);
 		own[i].fId = key;
-		setOwn([ ...own ]);
+		setOwn([...own]);
 	};
 
 	const changePer = (i: number, val: number) => {
 		own[i].per = val;
-		setOwn([ ...own ]);
+		setOwn([...own]);
 		const count = ownerPercent();
 		if (count > 100) own[i].per = val - (count - 100);
 	};
@@ -186,7 +209,7 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 		const count = ownerPercent();
 		if (count < 100) {
 			own.push({ fId: memberKey, per: 100 - count });
-			setOwn([ ...own ]);
+			setOwn([...own]);
 			let rec = getNewRec();
 			rec.own = own;
 			setInput(rec);
@@ -196,30 +219,28 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 	useEffect(() => {
 		if (!own.length) {
 			own.push({ fId: memberKey, per: 100 });
-			setOwn([ ...own ]);
+			setOwn([...own]);
 			let rec = getNewRec();
 			rec.own = own;
 			setInput(rec);
 		}
 	}, []);
 
-	useEffect(
-		() => {
-			const count = ownerPercent();
-			setError(count < 100);
-			disableOk(count < 100);
-		},
-		[ own, disableOk ]
-	);
+	useEffect(() => {
+		const count = ownerPercent();
+		setError(count < 100);
+		disableOk(count < 100);
+	}, [own, disableOk]);
 
-	
 	useEffect(() => {
 		changeMv();
-	}, [amount, rate, purchaseDate])
+	}, [amount, rate, purchaseDate]);
 
 	const changeMv = (val?: number) => {
 		// @ts-ignore
-		val ? setMv(val) : Math.round(getCompoundedIncome(rate, amount, duration()));
+		val
+			? setMv(val)
+			: Math.round(getCompoundedIncome(rate, amount, duration()));
 		setMvm(new Date().getMonth() + 1);
 		setMvy(new Date().getFullYear());
 		let rec = getNewRec();
@@ -231,7 +252,7 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 
 	const removeOwner = (index: number) => {
 		own.splice(index, 1);
-		setOwn([ ...own ]);
+		setOwn([...own]);
 		let rec = getNewRec();
 		rec.own = own;
 		setInput(rec);
@@ -241,15 +262,30 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 
 	return (
 		<Form layout="vertical">
-			<Row gutter={[ { xs: 0, sm: 0, md: 35 }, { xs: 15, sm: 15, md: 15 } ]}>
+			<Row
+				gutter={[
+					{ xs: 0, sm: 0, md: 35 },
+					{ xs: 15, sm: 15, md: 15 },
+				]}
+			>
 				<Col xs={24} md={12}>
 					<FormItem label="Name">
-						<TextInput pre="" value={name} changeHandler={changeName} size={'middle'} />
+						<TextInput
+							pre=""
+							value={name}
+							changeHandler={changeName}
+							size={"middle"}
+						/>
 					</FormItem>
 				</Col>
 				<Col xs={24} md={12}>
 					<FormItem label="Type">
-						<Row gutter={[ { xs: 0, sm: 0, md: 15 }, { xs: 0, sm: 0, md: 15 } ]}>
+						<Row
+							gutter={[
+								{ xs: 0, sm: 0, md: 15 },
+								{ xs: 0, sm: 0, md: 15 },
+							]}
+						>
 							<Col xs={24} sm={12}>
 								{categoryOptions && (
 									<SelectInput
@@ -261,31 +297,13 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 								)}
 							</Col>
 							<Col xs={24} md={12}>
-								{subtype === 'O' || subtype === 'P' ? null : (
-									<Checkbox onChange={(e) => changeRes(e.target.checked)}>Residential</Checkbox>
+								{subtype === "O" || subtype === "P" ? null : (
+									<Checkbox onChange={(e) => changeRes(e.target.checked)}>
+										Residential
+									</Checkbox>
 								)}
 							</Col>
 						</Row>
-					</FormItem>
-				</Col>
-				<Col xs={24} md={12}>
-					<FormItem label="Address">
-						<TextInput pre="" value={address} changeHandler={changeAddress} size={'middle'} />
-					</FormItem>
-				</Col>
-				<Col xs={24} md={12}>
-					<FormItem label="City">
-						<TextInput pre="" value={city} changeHandler={changeCity} size={'middle'} disabled={true} />
-					</FormItem>
-				</Col>
-				<Col xs={24} md={12}>
-					<FormItem label="State">
-						<TextInput pre="" value={state} changeHandler={changeState} size={'middle'} disabled={true} />
-					</FormItem>
-				</Col>
-				<Col xs={24} md={12}>
-					<FormItem label="Pincode">
-						<TextInput pre="" value={pin} changeHandler={changePin} size={'middle'} />
 					</FormItem>
 				</Col>
 				<Col xs={24} md={12}>
@@ -309,13 +327,22 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 							picker="month"
 							changeHandler={changePurchaseDate}
 							defaultVal={purchaseDate}
-							size={'middle'}
+							size={"middle"}
 						/>
 					</FormItem>
 				</Col>
 				<Col xs={24} md={12}>
 					<FormItem label="Appreciation Rate">
-						<NumberInput pre={''} min={1} max={50} value={rate} changeHandler={changeRate} step={0.1} noSlider unit='%'/>
+						<NumberInput
+							pre={""}
+							min={1}
+							max={50}
+							value={rate}
+							changeHandler={changeRate}
+							step={0.1}
+							noSlider
+							unit="%"
+						/>
 					</FormItem>
 				</Col>
 				<Col xs={24} md={12}>
@@ -332,49 +359,86 @@ export default function AddPropertyInput({ setInput, disableOk, categoryOptions 
 						/>
 					</FormItem>
 				</Col>
-				{/*<Col xs={24} md={12}>*/}
-				{own &&
-					own[0] &&
-					own.map((own: OwnershipInput, i: number) => (
-						<Col key={'own' + i} xs={24} md={12}>
-							<Row gutter={[ 10, 10 ]}>
-								<Col key={'own' + i}>
-									<SelectInput
-										pre={<UserOutlined />}
-										value={own.fId as string}
-										options={getFamilyOptions(allFamily)}
-										changeHandler={(key: string) => changeMember(i, key)}
-									/>
-								</Col>
-								<Col>
-									{/* <NumberInput pre='' min={1} max={100} value={own.per} changeHandler={(val:number)=>changePer(i,val)} step={0.1} noSlider unit='%'/> */}
-									<InputNumber
-										placeholder="Percentage"
-										min={1}
-										max={100}
-										value={own.per}
-										onChange={(val: number) => changePer(i, val)}
-									/>
-								</Col>
-								<Button type="link" onClick={() => removeOwner(i)} danger>
-									<DeleteOutlined />
-								</Button>
-							</Row>
-						</Col>
-					))}
-					<Col xs={24}>
-						<Tooltip title='Add Owners'>
-							<Button 
-								shape={'circle'} 
-								onClick={onAddBtnClick} 
-								style={{background: COLORS.GREEN}} 
-								icon={<PlusOutlined />} 
-								disabled={Object.keys(allFamily).length === 1}/>
-						</Tooltip>
+				<Col xs={24} md={12}>
+					<FormItem label="Pincode">
+						<TextInput
+							pre=""
+							value={pin}
+							changeHandler={changePin}
+							size={"middle"}
+						/>
+					</FormItem>
+				</Col>
+				<Col xs={24} md={12}>
+					<FormItem label="Address">
+						<TextInput
+							pre=""
+							value={address}
+							changeHandler={changeAddress}
+							size={"middle"}
+						/>
+					</FormItem>
+				</Col>
+				{city && (
+					<Col xs={24} md={12}>
+						City <strong>{city}</strong>
 					</Col>
-				{/*</Col>*/}
+				)}
+				{state && (
+					<Col xs={24} md={12}>
+						State <strong>{state}</strong>
+					</Col>
+				)}
+				{own && own[0] && (
+					<>
+						<Col xs={24}>
+							Owners{" "}
+							<Tooltip title="Add Owners">
+								<Button
+									shape={"circle"}
+									onClick={onAddBtnClick}
+									icon={<PlusOutlined />}
+									disabled={Object.keys(allFamily).length === 1}
+								/>
+							</Tooltip>
+							{error && (
+								<Alert
+									type={"error"}
+									message={"Owner percentage combination should equal to 100"}
+								/>
+							)}
+						</Col>
+
+						{own.map((own: OwnershipInput, i: number) => (
+							<Col key={"own" + i} xs={24} md={12}>
+								<Row gutter={[10, 10]}>
+									<Col key={"own" + i}>
+										<SelectInput
+											pre={<UserOutlined />}
+											value={own.fId as string}
+											options={getFamilyOptions(allFamily)}
+											changeHandler={(key: string) => changeMember(i, key)}
+										/>
+									</Col>
+									<Col>
+										{/* <NumberInput pre='' min={1} max={100} value={own.per} changeHandler={(val:number)=>changePer(i,val)} step={0.1} noSlider unit='%'/> */}
+										<InputNumber
+											placeholder="Percentage"
+											min={1}
+											max={100}
+											value={own.per}
+											onChange={(val: number) => changePer(i, val)}
+										/>
+									</Col>
+									<Button type="link" onClick={() => removeOwner(i)} danger>
+										<DeleteOutlined />
+									</Button>
+								</Row>
+							</Col>
+						))}
+					</>
+				)}
 			</Row>
-			{error && <Alert type={'error'} message={'Owner percentage combination should equal to 100'} />}
 		</Form>
 	);
 }
