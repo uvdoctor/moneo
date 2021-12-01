3c034b769c5a3f00a8082106392b43782f0ee871
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-12-01 17:26:06 +0530

    updated calculate in Inbond

--- amplify/backend/function/INBond/src/calculate.js
+++ amplify/backend/function/INBond/src/calculate.js
@@ -138,6 +138,7 @@ const calcSchema = (record, codes, schema, typeExchg, isinMap, table) => {
   schema.ytm = calcYTM(record, codes);
   appendGenericFields(schema, table)
   isinMap[record[codes.id]] = record[codes.id];
+  if(!schema.rate) schema.rate = -1;
   return schema;
 };
 module.exports = { calcSchema, calc, calcYTM };
