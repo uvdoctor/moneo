1d193189a033800a0f937f2abab7e7ff9d0957df
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-12-06 15:33:26 +0530

    update

--- amplify/backend/function/moneobacb277ebacb277ePostConfirmation/src/operation.js
+++ amplify/backend/function/moneobacb277ebacb277ePostConfirmation/src/operation.js
@@ -1,33 +1,29 @@
-const AWS = require('aws-sdk');
-const ddb = new AWS.DynamoDB();
+const { DynamoDB } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient, PutCommand } = require('@aws-sdk/lib-dynamodb');
+const dynamodb = new DynamoDB({});
+const docClient = DynamoDBDocumentClient.from(dynamodb);
 
 const getTableNameFromInitialWord = (tableInitial) => {
-	return new Promise(async (resolve, reject) => {
-		var params = { ExclusiveStartTableName: tableInitial, Limit: 1 };
-		try {
-			const table = await ddb.listTables(params).promise();
-			resolve(table.TableNames[0]);
-		} catch (err) {
-			console.log(err);
-			reject(err);
-		}
+	return new Promise((resolve, reject) => {
+		dynamodb.listTables({}, (err, table) => {
+			if (err) reject(err);
+			const tableName = table.TableNames.find((item) => item.startsWith(tableInitial));
+			resolve(tableName);
+		});
 	});
 };
 
-const pushDataSingly = (params, email) => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			await ddb.putItem(params).promise();
-			resolve('Email has been inserted in the table: ', email);
-		} catch (err) {
-			reject(`Error while inserting entry for ${email}: `, err);
-		}
-	});
+const pushDataSingly = async (params, email) => {
+	try {
+		await docClient.send(new PutCommand(params));
+		return ('Email has been inserted in the table: ', email);
+	} catch (err) {
+		return(`Error while inserting entry for ${email}: `, err);
+	}
 };
 
 const getDataFromEventAndPush = (event) => {
 	return new Promise(async (resolve, reject) => {
-		// const table = 'Contacts-fdun77s5lzbinkbgvnuidw6ihq-dev';
 		let date = new Date();
 		if (event.request.userAttributes['email_verified'] === 'true') {
 			let notify = event.request.userAttributes.website;
