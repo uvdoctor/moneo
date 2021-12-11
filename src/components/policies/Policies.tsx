0061626d7accfa48bd4771ee81cb509ee2035139
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-12-11 12:37:58 +0530

    updated Policies

--- src/components/policies/Policies.tsx
+++ src/components/policies/Policies.tsx
@@ -1,44 +1,63 @@
-import { Col, Row, Tabs } from "antd";
-import React, { Fragment } from "react";
-import MajorAssumptions from "../calc/blog/MajorAssumptions";
-import { isMobileDevice } from "../utils";
-import { useFullScreenBrowser } from "react-browser-hooks";
-import privacyContent from "./PrivacyContent";
-import securityContent from "./SecurityContent";
-import tcConent from "./TCContent";
+import { Col, Row, Tabs } from 'antd';
+import React, { Fragment } from 'react';
+import MajorAssumptions from '../calc/blog/MajorAssumptions';
+import { isMobileDevice } from '../utils';
+import { useFullScreenBrowser } from 'react-browser-hooks';
+import privacyContent from './PrivacyContent';
+import securityContent from './SecurityContent';
+import tcConent from './TCContent';
+import Link from 'next/link';
+import { ROUTES } from '../../CONSTANTS';
 
 interface PoliciesProps {
-  type: string;
+	type: string;
 }
 
 export default function Policies({ type }: PoliciesProps) {
-  const fsb = useFullScreenBrowser();
-  const { TabPane } = Tabs;
+	const fsb = useFullScreenBrowser();
+	const { TabPane } = Tabs;
 
-  const sections: any = {
-    "Terms & Conditions": <MajorAssumptions elements={[...tcConent]} />,
-    Privacy: <MajorAssumptions elements={[...privacyContent]} />,
-    Security: <MajorAssumptions elements={[...securityContent]} />,
-  };
+	const sections: any = {
+		'Terms & Conditions': <MajorAssumptions elements={[ ...tcConent ]} />,
+		Privacy: <MajorAssumptions elements={[ ...privacyContent ]} />,
+		Security: <MajorAssumptions elements={[ ...securityContent ]} />
+	};
 
-  return (
-    <Fragment>
-      <Row className="steps-content">
-        <Col>
-          <Tabs
-            defaultActiveKey={type}
-            tabPosition={isMobileDevice(fsb) ? "top" : "left"}
-            type={isMobileDevice(fsb) ? "card" : "line"}
-            animated
-          >
-            {Object.keys(sections).map((key) => (
-              <TabPane key={key} tab={key}>
-                {sections[key]}
-              </TabPane>
-            ))}
-          </Tabs>
-        </Col>
-      </Row>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Row className="steps-content">
+				<Col>
+					<Tabs
+						defaultActiveKey={type}
+						tabPosition={isMobileDevice(fsb) ? 'top' : 'left'}
+						type={isMobileDevice(fsb) ? 'card' : 'line'}
+						animated
+					>
+						{Object.keys(sections).map((key: string) => (
+							<TabPane
+								key={key}
+								tab={
+									<Link
+										href={
+											key === 'Privacy' ? (
+												ROUTES.PRIVACY
+											) : key === 'Security' ? (
+												ROUTES.SECURITY
+											) : (
+												ROUTES.TC
+											)
+										}
+									>
+										<a>{key}</a>
+									</Link>
+								}
+							>
+								{sections[key]}
+							</TabPane>
+						))}
+					</Tabs>
+				</Col>
+			</Row>
+		</Fragment>
+	);
 }
