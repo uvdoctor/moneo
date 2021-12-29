84e63c40a80bf6c73bcea9d558076e8af312bddb
RSRawat <ravinder.singh.rawat2008@gmail.com>
2021-12-29 14:38:33 +0530

    Fixed you own and owe view

--- src/components/nw/NWView.tsx
+++ src/components/nw/NWView.tsx
@@ -1,13 +1,16 @@
-import React, { Fragment, useContext } from 'react';
-import HoldingTabView from './HoldingTabView';
-import { ASSETS_VIEW, LIABILITIES_VIEW, NWContext } from './NWContext';
-import { Col, PageHeader, Radio, Row, Skeleton } from 'antd';
-import SelectInput from '../form/selectinput';
+import React, { Fragment, useContext } from "react";
+import HoldingTabView from "./HoldingTabView";
+import { ASSETS_VIEW, LIABILITIES_VIEW, NWContext } from "./NWContext";
+import { Col, PageHeader, Radio, Row, Skeleton } from "antd";
+import { CheckOutlined } from "@ant-design/icons";
+import SelectInput from "../form/selectinput";
 
-require('./nw.less');
-import FamilyInput from './FamilyInput';
-import TotalNetWorth from './TotalNetWorth';
-import ItemDisplay from '../calc/ItemDisplay';
+require("./nw.less");
+import FamilyInput from "./FamilyInput";
+import TotalNetWorth from "./TotalNetWorth";
+import ItemDisplay from "../calc/ItemDisplay";
+
+require("./NWView.less");
 
 export default function NWView() {
 	const {
@@ -19,7 +22,7 @@ export default function NWView() {
 		totalAssets,
 		totalLiabilities,
 		view,
-		setView
+		setView,
 	}: any = useContext(NWContext);
 
 	return (
@@ -37,7 +40,7 @@ export default function NWView() {
 									changeHandler={setSelectedCurrency}
 									options={currencyList}
 									loading={loadingHoldings}
-								/>
+								/>,
 							]}
 						/>
 					</Col>
@@ -51,33 +54,61 @@ export default function NWView() {
 			<div className="nw-container">
 				{!loadingHoldings && !loadingFamily ? (
 					<Fragment>
-						<TotalNetWorth />
-						<Row justify="center">
-							<Radio.Group value={view} onChange={(e) => setView(e.target.value)} size="large">
-								<Row>
-									<Col>
-										<Radio.Button value={ASSETS_VIEW} className="dd-stat">
-											<ItemDisplay
-												label="You Own"
-												result={totalAssets}
-												currency={selectedCurrency}
-												info="This is the total valuation of the assets you own."
-											/>
-										</Radio.Button>
-									</Col>
-									<Col>
-										<Radio.Button value={LIABILITIES_VIEW} className="dd-stat">
-											<ItemDisplay
-												label="You Owe"
-												result={-totalLiabilities}
-												currency={selectedCurrency}
-												info="This is the total valuation of all the money you owe."
-												pl
-											/>
-										</Radio.Button>
-									</Col>
-								</Row>
-							</Radio.Group>
+						<Row justify="center" gutter={16}>
+							<Col xs={24} sm={24} md={16} lg={8}>
+								<TotalNetWorth />
+							</Col>
+							<Col
+								className="dd-stat you-own-owe"
+								xs={24}
+								sm={24}
+								md={16}
+								lg={8}
+							>
+								<Radio.Group
+									value={view}
+									onChange={(e) => setView(e.target.value)}
+									size="large"
+								>
+									<Row align="middle">
+										<Col span={12}>
+											<Radio.Button value={ASSETS_VIEW}>
+												<Row align="middle" justify="center" gutter={15}>
+													<Col>
+														<CheckOutlined />
+													</Col>
+													<Col>
+														<ItemDisplay
+															label="You Own"
+															result={totalAssets}
+															currency={selectedCurrency}
+															info="This is the total valuation of the assets you own."
+														/>
+													</Col>
+												</Row>
+											</Radio.Button>
+										</Col>
+										<Col span={12}>
+											<Radio.Button value={LIABILITIES_VIEW}>
+												<Row align="middle" justify="center" gutter={15}>
+													<Col>
+														<CheckOutlined />
+													</Col>
+													<Col>
+														<ItemDisplay
+															label="You Owe"
+															result={-totalLiabilities}
+															currency={selectedCurrency}
+															info="This is the total valuation of all the money you owe."
+															pl
+														/>
+													</Col>
+												</Row>
+											</Radio.Button>
+										</Col>
+									</Row>
+								</Radio.Group>
+							</Col>
 						</Row>
 						<HoldingTabView liabilities={view !== ASSETS_VIEW} />
 					</Fragment>
--- src/components/nw/TotalNetWorth.tsx
+++ src/components/nw/TotalNetWorth.tsx
@@ -1,22 +1,20 @@
-import { Col, Row } from 'antd';
-import React, { useContext } from 'react';
-import ItemDisplay from '../calc/ItemDisplay';
-import { NWContext } from './NWContext';
+import { Col, Row } from "antd";
+import React, { useContext } from "react";
+import ItemDisplay from "../calc/ItemDisplay";
+import { NWContext } from "./NWContext";
 
 export default function TotalNetWorth() {
 	const { nw, selectedCurrency }: any = useContext(NWContext);
 
 	return (
-		<Row justify="center">
-			<Col xs={24} sm={24} md={16} lg={8} className='dd-stat'>
-					<ItemDisplay
-						label="Net Worth"
-						result={nw}
-						currency={selectedCurrency}
-						pl
-						info={'Net Worth equals what you own minus what you owe.'}
-					/>
-			</Col>
-		</Row>
+		<div className="dd-stat">
+			<ItemDisplay
+				label="Net Worth"
+				result={nw}
+				currency={selectedCurrency}
+				pl
+				info={"Net Worth equals what you own minus what you owe."}
+			/>
+		</div>
 	);
 }
