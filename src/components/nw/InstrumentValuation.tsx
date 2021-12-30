44e822bc34eb173bcaf70b6b70552cca96e0c3d3
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2021-12-30 09:58:17 +0530

    updated Instrumnet Valuation

--- src/components/nw/InstrumentValuation.tsx
+++ src/components/nw/InstrumentValuation.tsx
@@ -2,43 +2,46 @@ import { Badge, Empty, Table, Tag } from 'antd';
 import React, { Fragment, useContext, useEffect, useState } from 'react';
 import { NWContext, TAB } from './NWContext';
 import Holding from './Holding';
-import { doesHoldingMatch, getAssetTypes, getColourForAssetType, getMarketCap, getFixedCategories, isFund, isBond } from './nwutils';
 import { toHumanFriendlyCurrency } from '../utils';
 import { COLORS } from '../../CONSTANTS';
 import { FilterTwoTone } from '@ant-design/icons';
 import { AppContext, LOCAL_INS_DATA_KEY } from '../AppContext';
 import { AssetSubType, AssetType, InstrumentInput, InsType, MCap, MFSchemeType } from '../../api/goals';
 import simpleStorage from 'simplestorage.js';
+import {
+	doesHoldingMatch,
+	getAssetTypes,
+	getColourForAssetType,
+	getMarketCap,
+	getFixedCategories,
+	isFund,
+	isBond
+} from './nwutils';
+
 export default function InstrumentValuation() {
 	const { insData }: any = useContext(AppContext);
-	const {
-		instruments,
-		setInstruments,
-		selectedCurrency,
-		childTab,
-		selectedMembers,
-	}: any = useContext(NWContext);
-	const { CheckableTag } = Tag;
-	const [ filteredInstruments, setFilteredInstruments ] = useState<Array<any>>([ ...instruments ]);
-	const [ filterByTag, setFilterByTag ] = useState<Array<any>>([]);
-	const [ nameFilterValues, setNameFilterValues ] = useState<Array<any>>([ {} ]);
+	const { instruments, setInstruments, selectedCurrency, childTab, selectedMembers }: any = useContext(NWContext);
+	const [ filteredInstruments, setFilteredInstruments ] = useState<Array<InstrumentInput>>([ ...instruments ]);
+	const [ filterByTag, setFilterByTag ] = useState<Array<InstrumentInput>>([]);
+	const [ nameFilterValues, setNameFilterValues ] = useState<Array<Object>>([ {} ]);
 	const [ filteredInfo, setFilteredInfo ] = useState<any | null>({});
-	const [ tags, setTags ] = useState<any>({});
+	const [ tags, setTags ] = useState<{[key: string]: string}>({});
 	const [ selectedTags, setSelectedTags ] = useState<Array<string>>([]);
-	const [ subtTags, setSubtTags ] = useState<any>({});
+	const [ subtTags, setSubtTags ] = useState<{[key: string]: string}>({});
 	const [ selectedSubtTags, setSelectedSubtTags ] = useState<Array<string>>([]);
 	const [ totalFilterAmt, setTotalFilterAmt ] = useState<number>(0);
+	const { CheckableTag } = Tag;
+	const { MF, STOCK, BOND, OIT, GOLDB} = TAB;
 
 	const bondTags = { CB: 'Corporate Bond', GB: 'Government Bond' };
-	const tagsData: any = {
+	const tagsData: { Stocks: { tags: {} }; 'Mutual Funds': { tags: {}; subt: {} }; Bonds: { tags: {} } } = {
 		Stocks: { tags: getMarketCap() },
 		'Mutual Funds': { tags: getAssetTypes(), subt: { E: getMarketCap(), F: getFixedCategories() } },
-		Bonds: { tags: bondTags },
+		Bonds: { tags: bondTags }
 	};
 
-	const hasTags = (childTab: string) => [ TAB.STOCK, TAB.MF, TAB.BOND ].includes(childTab);
-
-	const delRecord = (id: string) => setInstruments([ ...instruments.filter((record: any) => record.id !== id) ]);
+	const hasTags = (childTab: string): Boolean => [ STOCK, MF, BOND ].includes(childTab);
+	const delRecord = (id: string) => setInstruments([ ...instruments.filter((record: InstrumentInput) => record.id !== id) ]);
 
 	const columns = [
 		{
@@ -51,152 +54,148 @@ export default function InstrumentValuation() {
 			filterIcon: <FilterTwoTone twoToneColor={filteredInfo?.id ? COLORS.GREEN : COLORS.DEFAULT} style={{ fontSize: 20 }} />,
 			filteredValue: filteredInfo.id || null,
 			filters: nameFilterValues,
-			onFilter: (values: Array<string>, record: any) => values.indexOf(record.id)>-1,
-			render: (record: any) => {
-				return <Holding key={record.id} holding={record as InstrumentInput} onDelete={delRecord} onChange={()=>setTotal()}/>
+			onFilter: (values: Array<string>, record: InstrumentInput) => values.indexOf(record.id)>-1,
+			render: (record: InstrumentInput) => {
+				return <Holding key={record.id} holding={record as InstrumentInput} onDelete={delRecord} onChange={setTotal}/>
 			}
 		}
 	];
-	//@ts-ignore
-	const handleChange = (pagination: any, filters: any, sorters: any) => setFilteredInfo(filters);
 
-	useEffect(
-		() => {
-			let filteredNames: Array<any> = [];
-			let ids: Set<string> = new Set();
-			filteredInstruments.forEach((instrument: InstrumentInput) => {
-				if (!ids.has(instrument.id)) filteredNames.push({ text: insData[instrument.id] ? insData[instrument.id].name : instrument.id, value: instrument.id });
-				ids.add(instrument.id);
-			});
-			setNameFilterValues([ ...filteredNames ]);
-		},
-		[ filteredInstruments ]
-	);
+	const handleChange = (_pagination: any, filters: any, _sorters: any) => setFilteredInfo(filters);
 
 	const subtTagsData = () => {
 		// @ts-ignore
+		const { E, F } = tagsData[childTab].subt;
 		if (selectedTags.includes(AssetType.E) && selectedTags.includes(AssetType.F)) {
-			setSubtTags({ ...tagsData[childTab].subt.E, ...tagsData[childTab].subt.F });
-			setSelectedSubtTags([
-				...Object.keys(tagsData[childTab].subt.E),
-				...Object.keys(tagsData[childTab].subt.F)
-			]);
+			setSubtTags({ ...E, ...F });
+			setSelectedSubtTags([ ...Object.keys(E), ...Object.keys(F) ]);
 			return;
 		}
 		if (selectedTags.includes(AssetType.E)) {
-			setSubtTags(tagsData[childTab].subt.E);
-			setSelectedSubtTags([ ...Object.keys(tagsData[childTab].subt.E) ]);
+			setSubtTags(E);
+			setSelectedSubtTags([ ...Object.keys(E) ]);
 			return;
 		}
 		if (selectedTags.includes(AssetType.F)) {
-			setSubtTags(tagsData[childTab].subt.F);
-			setSelectedSubtTags([ ...Object.keys(tagsData[childTab].subt.F) ]);
+			setSubtTags(F);
+			setSelectedSubtTags([ ...Object.keys(F) ]);
 			return;
 		}
 		setSubtTags({});
 		setSelectedSubtTags([]);
 		return;
 	};
+
 	const setTotal = () => {
-		let total = 0;
-		let filterAmt = 0;
-		let cachedData = simpleStorage.get(LOCAL_INS_DATA_KEY);
-		if(!cachedData) cachedData = insData;
+		let [ total, filterAmt, cachedData ] = [ 0, 0, simpleStorage.get(LOCAL_INS_DATA_KEY) ];
+		if (!cachedData) cachedData = insData;
 		const dataToFilter = selectedTags.length ? filterByTag : filteredInstruments;
 		dataToFilter.map((instrument: InstrumentInput) => {
-			const price = instrument.qty * (cachedData[instrument.id] ? cachedData[instrument.id].price : 0);
+			const id = instrument.id;
+			const price = instrument.qty * (cachedData[id] ? cachedData[id].price : 0);
 			if (filteredInfo.id) {
-				const id = filteredInfo.id.some((item: string) => item === instrument.id);
-				if (id) filterAmt += price;
+				if (filteredInfo.id.some((item: string) => item === id)) filterAmt += price;
 			}
-			total += price
-		})
+			total += price;
+		});
 		filteredInfo.id ? setTotalFilterAmt(filterAmt) : setTotalFilterAmt(total);
-	}
+	};
+
+	useEffect(() => {
+		let filteredNames: Array<{ text: String; value: String }> = [];
+		let ids: Set<string> = new Set();
+		filteredInstruments.forEach((instrument: InstrumentInput) => {
+			const id = instrument.id;
+			if (!ids.has(id)) {
+				filteredNames.push({ text: insData[id] ? insData[id].name : id, value: id });
+			}
+			ids.add(id);
+		});
+		setNameFilterValues([ ...filteredNames ]);
+	},[ filteredInstruments, insData ]);
+
 	useEffect(() => {
 		setTotal();
 	},[ filteredInstruments, filteredInfo, instruments, selectedTags, filterByTag ]);
-	useEffect(
-		() => {
-			hasTags(childTab) ? setTags(tagsData[childTab].tags) : setTags({});
-		},
-		[ childTab ]
-	);
-	useEffect(
-		() => {
-			if (childTab === TAB.MF) subtTagsData();
-		},
-		[ selectedTags ]
-	);
+
+	useEffect(() => {
+		// @ts-ignore
+		hasTags(childTab) ? setTags(tagsData[childTab].tags) : setTags({});
+	},[ childTab ]);
+
+	useEffect(() => {
+		if (childTab === MF) subtTagsData();
+	},[ selectedTags ]);
+
+	useEffect(() => {
+		filterInstrumentsByTabs();
+	},[ childTab, instruments, selectedMembers, selectedCurrency ]);
+
+	useEffect(() => {
+		filterInstrumentsByTags();
+	},[ selectedTags, selectedSubtTags ]);
+
 	const filterInstrumentsByTabs = () => {
 		if (!instruments.length) return;
+		const { E, F } = AssetType;
+		const { REIT, InvIT, ETF } = InsType;
 		let filteredData: Array<InstrumentInput> = instruments.filter((instrument: InstrumentInput) => {
-			const data = insData[instrument.id];
-			if(!data && doesHoldingMatch(instrument, selectedMembers, selectedCurrency)) {
-				if (childTab === TAB.MF && isFund(instrument.id)) return instrument.id;
-				else if (childTab === TAB.STOCK && !isFund(instrument.id)) return instrument.id;
-			} 
-			if (data && doesHoldingMatch(instrument, selectedMembers, selectedCurrency)) {		
-				if (childTab === TAB.REIT) return data.itype === InsType.REIT;
-				else if (childTab === TAB.OIT) return data.itype === InsType.InvIT;
-				else if (childTab === TAB.ETF) return data.itype === InsType.ETF;
-				else if (childTab === TAB.MF) return isFund(instrument.id) && !data.itype;
-				else if (childTab === TAB.GOLDB) return data.subt === AssetSubType.GoldB;
-				else if (childTab === TAB.BOND) return data.type === AssetType.F;
-				else if (childTab === TAB.STOCK) return data.type === AssetType.E && !isFund(instrument.id) && !isBond(instrument.id);
+			const id = instrument.id;
+			const data = insData[id];
+			if (!data && doesHoldingMatch(instrument, selectedMembers, selectedCurrency)) {
+				if (childTab === MF && isFund(id)) return id;
+				if (childTab === STOCK && !isFund(id)) return id;
+			}
+			if (data && doesHoldingMatch(instrument, selectedMembers, selectedCurrency)) {
+				if (childTab === TAB.REIT) return data.itype === REIT;
+				if (childTab === OIT) return data.itype === InvIT;
+				if (childTab === TAB.ETF) return data.itype === ETF;
+				if (childTab === MF) return isFund(id) && !data.itype;
+				if (childTab === GOLDB) return data.subt === AssetSubType.GoldB;
+				if (childTab === BOND) return data.type === F;
+				if (childTab === STOCK) return data.type === E && !isFund(id) && !isBond(id);
 			}
 		});
 		setFilteredInstruments([ ...filteredData ]);
 	};
+
 	const filterInstrumentsByTags = () => {
 		if (!selectedTags.length) return;
 		let filterDataByTag = filteredInstruments.filter((instrument: InstrumentInput) => {
 			const data = insData[instrument.id];
-			if (childTab === TAB.MF) {
-				if (selectedSubtTags.length) {
-					if (selectedTags.indexOf(data.type as string) > -1) {
-						 return selectedSubtTags.indexOf(data.mcap as string) > -1 ||
-						 (selectedSubtTags.includes(AssetSubType.CB) && (data.subt === AssetSubType.CB)) || 
-						 (selectedSubtTags.includes(AssetSubType.I) && (data.type === AssetType.F && data.subt === AssetSubType.I)) || 
-						(selectedSubtTags.includes('GovB') && (data.subt === AssetSubType.GB || data.subt === AssetSubType.GBO)) || 
-						(selectedSubtTags.includes('IF') && (data.subt === AssetSubType.HB && data.mftype === MFSchemeType.I)) ||
-						(selectedSubtTags.includes('FMP') && (data.subt === AssetSubType.HB && data.mftype === MFSchemeType.C)) ||
-						(selectedSubtTags.includes('LF') && (data.subt === AssetSubType.L)) 
-					}
-				}
-			} 
-			else if (childTab === TAB.STOCK) {
-				if(data && data.meta) {
-					return (selectedTags.includes(MCap.L) && data?.meta?.mcap === MCap.L ) || 
-					(selectedTags.includes(MCap.M) && data?.meta?.mcap === MCap.M ) ||
-					(selectedTags.includes(MCap.H) && data?.meta?.mcap === MCap.H ) ||
-					(selectedTags.includes(MCap.S) && data?.meta?.mcap === null ) 
-				}
+			if (childTab === MF && selectedSubtTags.length && selectedTags.indexOf(data.type as string) > -1) {
+				const { CB, GBO, I, HB, GB, L } = AssetSubType;
+				const { subt, mftype, type, mcap } = data;
+				const { GovB, IF, FMP, LF } = getFixedCategories();
+				return (
+					selectedSubtTags.indexOf(mcap as string) > -1 ||
+					(selectedSubtTags.includes(CB) && subt === CB) ||
+					(selectedSubtTags.includes(I) && (type === AssetType.F && subt === I)) ||
+					(selectedSubtTags.includes(GovB) && (subt === GB || subt === GBO)) ||
+					(selectedSubtTags.includes(IF) && (subt === HB && mftype === MFSchemeType.I)) ||
+					(selectedSubtTags.includes(FMP) && (subt === HB && mftype === MFSchemeType.C)) ||
+					(selectedSubtTags.includes(LF) && subt === L)
+				);
 			}
-			else if (childTab === TAB.BOND && data) {
-				if (selectedTags.includes(AssetSubType.GB)) {
-					return data?.subt === AssetSubType.GB || data?.subt === AssetSubType.GBO;
-				}
-				else {
-					return data?.subt === AssetSubType.CB; 
-				}
+			if (childTab === STOCK && data && data.meta) {
+				const { L, M, H, S } = MCap;
+				const { meta: { mcap } } = data;
+				return (
+					(selectedTags.includes(L) && mcap === L) ||
+					(selectedTags.includes(M) && mcap === M) ||
+					(selectedTags.includes(H) && mcap === H) ||
+					(selectedTags.includes(S) && !mcap)
+				);
+			} else if (childTab === BOND && data) {
+				const { subt } = data;
+				const { GB, CB, GBO } = AssetSubType;
+				if (selectedTags.includes(GB)) return subt === GB || subt === GBO;
+				if (selectedTags.includes(CB)) return subt === CB;
 			}
 		});
-		
 		setFilterByTag([ ...filterDataByTag ]);
 	};
-	useEffect(
-		() => {
-			filterInstrumentsByTabs();
-		},
-		[ childTab, instruments, selectedMembers, selectedCurrency ]
-	);
-	useEffect(
-		() => {
-			filterInstrumentsByTags();
-		},
-		[ selectedTags, selectedSubtTags ]
-	);
+
 	return instruments.length ? (
 		<Fragment>
 			<p style={{ textAlign: 'center' }}>
@@ -220,7 +219,7 @@ export default function InstrumentValuation() {
 				))}
 			</p>
 			<p style={{ textAlign: 'center' }}>
-				{childTab === TAB.MF &&
+				{childTab === MF &&
 					subtTags &&
 					Object.keys(subtTags).map((tag: string) => (
 						<CheckableTag
