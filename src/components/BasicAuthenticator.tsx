8e2d4a0b5c4d29b85865b00651a793aa9ef78934
Moneo <emailumangdoctor@gmail.com>
2021-12-31 12:48:35 +0530

    updated confirm signup

--- src/components/BasicAuthenticator.tsx
+++ src/components/BasicAuthenticator.tsx
@@ -103,7 +103,7 @@ export default function BasicAuthenticator({ children }: BasicAuthenticatorProps
 				data: user
 			});
 		});
-		await createUserinfo(uname, email, notify, riskProfile, 0, new Date().toISOString(), TaxLiability.L);
+		await createUserinfo(uname, email, notify, riskProfile, 0, new Date().toISOString(), taxLiability);
 	};
 
 	const handleRegistrationSubmit = async () => {
