f9aaa36a9a2a0b0380e700e46d77b62ddf9d3586
Moneo <emailumangdoctor@gmail.com>
2022-01-03 00:49:03 +0530

    updated select input

--- src/components/form/selectinput.tsx
+++ src/components/form/selectinput.tsx
@@ -24,7 +24,7 @@ export default function SelectInput(props: SelectInputProps) {
 		<Fragment>
 			{props.pre && (
 				<Fragment>
-					{props.pre}{" "}
+					{props.pre}
 					{props.info && (
 						<Tooltip title={props.info}>
 							<InfoCircleOutlined />
