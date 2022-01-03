be7b051a1d8d480526639e24327b66498e211fa2
Moneo <emailumangdoctor@gmail.com>
2022-01-03 23:35:16 +0530

    updated loan cost

--- src/components/calc/LoanIntResult.tsx
+++ src/components/calc/LoanIntResult.tsx
@@ -8,7 +8,7 @@ export default function LoanIntResult() {
 	const { totalIntAmt, totalInsAmt }: any = useContext(GoalContext);
 
 	return totalIntAmt ? (
-		<ItemDisplay label='Loan Cost' result={totalIntAmt + totalInsAmt} currency={currency}
+		<ItemDisplay label='Loan Cost' result={-totalIntAmt - totalInsAmt} currency={currency} pl
 		info="Loan Cost includes loan interest and any insurance paid to protect repayment." />
 	) : (
 		<ItemDisplay label={`Total Loan Cost`} result="No Loan." />
