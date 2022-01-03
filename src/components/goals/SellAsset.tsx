ba68c1243e6a615f2088d8f165b5e7364a570b9b
Moneo <emailumangdoctor@gmail.com>
2022-01-03 20:54:04 +0530

    updated sell of buy rent

--- src/components/goals/SellAsset.tsx
+++ src/components/goals/SellAsset.tsx
@@ -1,65 +1,50 @@
 import React, { Fragment, useContext, useState } from 'react';
-import ItemDisplay from '../calc/ItemDisplay';
 import Section from '../form/section';
 import RadialInput from '../form/radialinput';
 import NumberInput from '../form/numberinput';
-import { toStringArr } from '../utils';
+import { toHumanFriendlyCurrency, toStringArr } from '../utils';
 import { GoalContext } from './GoalContext';
 import AnnualCF from './AnnualCF';
 import { Tabs } from 'antd';
 import { CalcContext } from '../calc/CalcContext';
 
 export default function SellAsset() {
-	const {
-		currency,
-	}: any = useContext(CalcContext);
-	const {
-		sellPrice,
-		assetChgRate,
-		setAssetChgRate,
-		sellAfter,
-		setSellAfter,
-	}: any = useContext(GoalContext);
-	const [ tabIndex, setTabIndex ] = useState<number>(0);
+	const { currency }: any = useContext(CalcContext);
+	const { sellPrice, assetChgRate, setAssetChgRate, sellAfter, setSellAfter }: any = useContext(GoalContext);
+	const [
+		tabIndex,
+		setTabIndex
+	] = useState<number>(0);
 	const { TabPane } = Tabs;
 
 	return (
 		<Fragment>
 			<Section title="Sell Details">
-				<NumberInput
-					info="Rate at which Price may change Yearly."
-					pre="Yearly Price Changes"
-					unit="%"
-					min={-20}
-					max={20}
-					step={0.5}
-					value={assetChgRate}
-					changeHandler={setAssetChgRate}
-				/>
 				<RadialInput
 					info="Years after which You Plan to Sell this Purchase."
 					label="Years"
 					pre="Sell After"
-					post={
-						<ItemDisplay
-							label="Sell Price"
-							result={Math.round(sellPrice)}
-							currency={currency}
-							footer="after taxes & fees"
-						/>
-					}
 					labelBottom={true}
 					data={toStringArr(3, 30)}
 					value={sellAfter}
 					step={1}
 					changeHandler={setSellAfter}
+					post={
+						<NumberInput
+							info="Rate at which Price may change yearly."
+							pre="Yearly price change"
+							unit="%"
+							post={`Sell price ${toHumanFriendlyCurrency(sellPrice, currency)}`}
+							min={-20}
+							max={20}
+							step={0.5}
+							value={assetChgRate}
+							changeHandler={setAssetChgRate}
+						/>
+					}
 				/>
 			</Section>
-			<Tabs
-				onTabClick={(key: string) => setTabIndex(parseInt(key))}
-				defaultActiveKey={'' + tabIndex}
-				type="card"
-			>
+			<Tabs onTabClick={(key: string) => setTabIndex(parseInt(key))} defaultActiveKey={'' + tabIndex} type="card">
 				<TabPane key={0} tab="Yearly Cost">
 					{tabIndex === 0 && <AnnualCF />}
 				</TabPane>
