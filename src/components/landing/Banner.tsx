e5aa8b2ed15e8a16febb73a36609818912e4150b
Moneo <emailumangdoctor@gmail.com>
2022-01-24 01:58:06 +0530

    removed share from landing page

--- src/components/landing/Banner.tsx
+++ src/components/landing/Banner.tsx
@@ -1,10 +1,8 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Row, Col } from "antd";
 import ResImg from "../ResImg";
 import BannerContent from "./BannerContent";
-import SocialShare from "../SocialShare";
 require("./Banner.less");
-import { AppContext } from "../AppContext";
 import GetStartedButton from "./GetStartedButton";
 import WalletSVG from "../svgs/2d/wallet";
 import LockSVG from "../svgs/2d/lock";
@@ -14,44 +12,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { COLORS } from "../../CONSTANTS";
 
 export default function Banner() {
-	const { defaultCountry }: any = useContext(AppContext);
-
-	return (
-		<div className="site-banner">
-			<ResImg name="cover" width={1264} height={650}/>
-			<div className="site-banner-content">
-				<BannerContent />
-				<p>
-					<GetStartedButton />
-				</p>
-				<Row align="middle" gutter={[5, 5]}>
-					<Col xs={24} sm={12} md={8} lg={24}>
-						<h3 className="h3-list">
-							<FontAwesomeIcon icon={faEyeSlash} color={COLORS.GREEN} />
-							No hidden fees
-						</h3>
-					</Col>
-					<Col xs={24} sm={12} md={12} lg={24}>
-						<h3 className="h3-list">
-							<WalletSVG />
-							Smart deals based on goals
-						</h3>
-					</Col>
-					<Col xs={24} sm={12} md={8} lg={24}>
-						<h3 className="h3-list">
-							<LockSVG />
-							Respects Privacy
-						</h3>
-					</Col>
-					<Col xs={24} sm={12} md={12} lg={24}>
-						<h3 className="h3-list">
-							<SecuritySVG />
-							Bank-grade Security
-						</h3>
-					</Col>
-				</Row>
-				{defaultCountry !== "IN" && <SocialShare />}
-			</div>
-		</div>
-	);
+  return (
+    <div className="site-banner">
+      <ResImg name="cover" width={1264} height={650} />
+      <div className="site-banner-content">
+        <BannerContent />
+        <p>
+          <GetStartedButton />
+        </p>
+        <Row align="middle" gutter={[5, 5]}>
+          <Col xs={24} sm={12} md={8} lg={24}>
+            <h3 className="h3-list">
+              <FontAwesomeIcon icon={faEyeSlash} color={COLORS.GREEN} />
+              No hidden fees
+            </h3>
+          </Col>
+          <Col xs={24} sm={12} md={12} lg={24}>
+            <h3 className="h3-list">
+              <WalletSVG />
+              Smart deals based on goals
+            </h3>
+          </Col>
+          <Col xs={24} sm={12} md={8} lg={24}>
+            <h3 className="h3-list">
+              <LockSVG />
+              Respects Privacy
+            </h3>
+          </Col>
+          <Col xs={24} sm={12} md={12} lg={24}>
+            <h3 className="h3-list">
+              <SecuritySVG />
+              Bank-grade Security
+            </h3>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
