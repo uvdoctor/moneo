9679862cc997135fb5777d95cbb83739aed144ad
Moneo <emailumangdoctor@gmail.com>
2022-01-27 14:02:02 +0530

    updated get page

--- src/pages/get.tsx
+++ src/pages/get.tsx
@@ -1,29 +1,27 @@
-import React from 'react';
-import { NWContextProvider } from '../components/nw/NWContext';
-import Amplify from 'aws-amplify';
-import awsexports from '../aws-exports';
-import BasicPage from '../components/BasicPage';
-import { InferGetStaticPropsType } from 'next';
-import { getFXData } from '../components/utils';
+import React from "react";
+import { NWContextProvider } from "../components/nw/NWContext";
+import Amplify from "aws-amplify";
+import awsexports from "../aws-exports";
+import BasicPage from "../components/BasicPage";
+import { InferGetStaticPropsType } from "next";
+import { getFXData } from "../components/utils";
 
 Amplify.configure({ ...awsexports, ssr: true });
 
 function Get({ fxRates }: InferGetStaticPropsType<typeof getStaticProps>) {
-	return (
-		<BasicPage title="Moneo - Get" secure>
-			<NWContextProvider fxRates={fxRates} />
-		</BasicPage>
-	);
+  return (
+    <BasicPage title="Moneo - Get" secure>
+      <NWContextProvider fxRates={fxRates} />
+    </BasicPage>
+  );
 }
 
-export async function getStaticProps() {
-	const token = '60d03a689523a3.63944368';
-	let fxRates = await getFXData(token);
-	return {
-		props: {
-			fxRates
-		}
-	};
-}
+export const getStaticProps = async () => {
+  return {
+    props: {
+      fxRates: await getFXData("60d03a689523a3.63944368"),
+    },
+  };
+};
 
 export default Get;
