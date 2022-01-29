5d85ff55ac78a84d56e23841ccc59ac28a5cc1b8
Moneo <emailumangdoctor@gmail.com>
2022-01-29 10:52:19 +0530

    updated captcha api

--- src/pages/api/verifycaptcha.tsx
+++ src/pages/api/verifycaptcha.tsx
@@ -1,37 +1,41 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from "next";
 
-const returnCaptchaResponse = async(token: any, res: NextApiResponse) => {
-	const secret = '6LdTyd8ZAAAAAEB3B2-P2swyDqrqpBQEcY4m0sOf';
-	fetch('https://www.google.com/recaptcha/api/siteverify', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		body: `secret=${secret}&response=${token}`
-	}).then((captchRes: any) => 
-		captchRes.json()
-	).then((data: any) => {
-		res.setHeader('Content-Type', 'application/json');
-		if(!data || !data.score || !data.success){
-			res.status(500).end('Google captcha failed');
-		}
-		if(data.score < 0.5){
-			res.status(403).json({ success: false });
-		}else{
-			res.status(200).json({ success: true });
-		}
-	})
-	.catch((e: any) => {
-		console.log("ERROR", e);
-		res.status(500).end('Google captcha Exception');
-	});
+type Data = {
+  success: boolean;
 };
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-	const { method, body: {token} } = req;
-	if (method === 'POST') {
-		await returnCaptchaResponse(token, res);
-	} else {
-		res.status(405).end(`Method ${method} Not Allowed`);
-	}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const {
+    method,
+    body: { token },
+  } = req;
+  if (method === "POST") {
+    const secret = "6LdTyd8ZAAAAAEB3B2-P2swyDqrqpBQEcY4m0sOf";
+    fetch("https://www.google.com/recaptcha/api/siteverify", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `secret=${secret}&response=${token}`,
+    })
+      .then((captchaResponse) => captchaResponse.json())
+      .then((response) => {
+        res.setHeader("Content-Type", "application/json");
+        if (!response || !response.score || !response.success) {
+          res.status(500).end("Google captcha failed");
+        }
+        if (response.score < 0.5) {
+          res.status(403).json({ success: false });
+        } else {
+          res.status(200).json({ success: true });
+        }
+      })
+      .catch((e) => {
+        console.log("Error while invoking google captcha: ", e);
+        res.status(500).end("Google captcha error");
+      });
+  } else {
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
 };
