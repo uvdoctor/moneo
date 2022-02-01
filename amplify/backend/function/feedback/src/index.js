2352f32924ac0183844bd13e4131a8e672ffbd21
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-02-01 15:11:52 +0530

    docs: updated feedback data

--- amplify/backend/function/feedback/src/index.js
+++ amplify/backend/function/feedback/src/index.js
@@ -7,28 +7,35 @@ const subject = {
 };
 
 const processData = async (data) => {
-	console.log(data);
-	for (record of data) {
-		if (record.eventName == 'INSERT') {
-			const data = record.dynamodb.NewImage;
-			console.log(data);
-			const { name, email, feedback, type, uname } = data;
-			console.log( name, email, feedback, type, uname );
-			const firstName = name.M.fn ? name.M.fn.S : '';
-			const lastName = name.M.ln ? name.M.ln.S : '';
-			const template = `<html>
-      <body>
-        <h3>${firstName} ${lastName}</h3>
-        <div>
-					<p>User:- ${uname.S ? 'Registered' : 'Not Registered'}</p>
-          <p>Email: -${email.S}</p>
-          <p>${feedback.S}</p>
-        </div>
-      </body>
-      </html>`;
-			await sendMail(template, subject[type.S], [ 'emailumangdoctor@gmail.com' ], 'no-reply@moneo.money');
+	return new Promise(async (resolve, reject) => {
+		console.log(data);
+		for (record of data) {
+			try {
+				if (record.eventName == 'INSERT') {
+					const data = record.dynamodb.NewImage;
+					console.log(data);
+					const { name, email, feedback, type, uname } = data;
+					console.log(name, email, feedback, type, uname);
+					const firstName = name.M.fn ? name.M.fn.S : '';
+					const lastName = name.M.ln ? name.M.ln.S : '';
+					const template = `<html>
+						<body>
+							<h3>${firstName} ${lastName}</h3>
+							<div>
+								<p>User:- ${uname.S ? 'Registered' : 'Not Registered'}</p>
+								<p>Email: -${email.S}</p>
+								<p>${feedback.S}</p>
+							</div>
+						</body>
+						</html>`;
+					await sendMail(template, subject[type.S], [ 'emailumangdoctor@gmail.com' ], 'no-reply@moneo.money');
+				}
+			} catch (err) {
+				reject(err);
+			}
 		}
-	}
+		resolve();
+	});
 };
 
 exports.handler = async (event) => {
