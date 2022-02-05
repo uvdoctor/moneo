2e242270931411c8d78ef370bc938748648951ef
RSRawat <ravinder.singh.rawat2008@gmail.com>
2022-02-05 21:24:02 +0530

    Added tradingview widgets on dashboard page

--- src/components/dashboard/DBView.tsx
+++ src/components/dashboard/DBView.tsx
@@ -4,11 +4,16 @@ import ItemDisplay from "../calc/ItemDisplay";
 import { AppContext } from "../AppContext";
 import { DBContext } from "./DBContext";
 import { ROUTES } from "../../CONSTANTS";
+import EconomicCalendar from "./EconomicCalendar";
+import StockMarket from "./StockMarket";
+import MarketOverview from "./MarketOverview";
+
 require("./DBView.less");
 
 export default function DBView() {
   const { totalAssets, totalLiabilities }: any = useContext(DBContext);
   const { appContextLoaded, defaultCurrency }: any = useContext(AppContext);
+
   return appContextLoaded ? (
     <Fragment>
       <div className="primary-header">
@@ -20,45 +25,60 @@ export default function DBView() {
       </div>
       <div className="db-container">
         <Fragment>
-          <Row justify="center" gutter={[16, 16]}>
-            <Col xs={24} sm={24} md={16} lg={12}>
-              <div className="dd-stat">
-                <Row gutter={[10, 10]}>
-                  <Col span={24}>
-                    <ItemDisplay
-                      label="Net Worth"
-                      result={totalAssets - totalLiabilities}
-                      currency={defaultCurrency}
-                      pl
-                      info={"Net Worth equals what you own minus what you owe."}
-                    />
-                    {!(totalAssets-totalLiabilities) ? <a href={ROUTES.GET}>
-                       Discover your networth
-                    </a> : null}
-                  </Col>
-                  {totalAssets || totalLiabilities ? (
-                    <Col xs={12}>
-                      <ItemDisplay
-                        label="You Own"
-                        result={totalAssets}
-                        currency={defaultCurrency}
-                        info="This is the total valuation of the assets you own."
-                      />
-                    </Col>
-                  ) : null}
-                  {totalAssets || totalLiabilities ? (
-                    <Col xs={12}>
-                      <ItemDisplay
-                        label="You Owe"
-                        result={-totalLiabilities}
-                        currency={defaultCurrency}
-                        info="This is the total valuation of all the money you owe."
-                        pl
-                      />
-                    </Col>
-                  ) : null}
-                </Row>
-              </div>
+          <Row justify="center" gutter={[30, 16]}>
+            <Col xs={24} sm={24} md={18}>
+              <Row gutter={[50, 30]}>
+                <Col xs={24}>
+                  <div className="dd-stat">
+                    <Row gutter={[10, 10]}>
+                      <Col span={24}>
+                        <ItemDisplay
+                          label="Net Worth"
+                          result={totalAssets - totalLiabilities}
+                          currency={defaultCurrency}
+                          pl
+                          info={
+                            "Net Worth equals what you own minus what you owe."
+                          }
+                        />
+                        {!(totalAssets - totalLiabilities) ? (
+                          <a href={ROUTES.GET}>Discover your networth</a>
+                        ) : null}
+                      </Col>
+                      {totalAssets || totalLiabilities ? (
+                        <Col xs={12}>
+                          <ItemDisplay
+                            label="You Own"
+                            result={totalAssets}
+                            currency={defaultCurrency}
+                            info="This is the total valuation of the assets you own."
+                          />
+                        </Col>
+                      ) : null}
+                      {totalAssets || totalLiabilities ? (
+                        <Col xs={12}>
+                          <ItemDisplay
+                            label="You Owe"
+                            result={-totalLiabilities}
+                            currency={defaultCurrency}
+                            info="This is the total valuation of all the money you owe."
+                            pl
+                          />
+                        </Col>
+                      ) : null}
+                    </Row>
+                  </div>
+                </Col>
+                <Col xs={24} sm={24} md={12}>
+                  <StockMarket />
+                </Col>
+                <Col xs={24} sm={24} md={12}>
+                  <MarketOverview />
+                </Col>
+              </Row>
+            </Col>
+            <Col xs={24} sm={24} md={6}>
+              <EconomicCalendar />
             </Col>
           </Row>
         </Fragment>
---
+++ src/components/dashboard/EconomicCalendar.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import { Typography } from "antd";
+
+export default function EconomicCalendar() {
+	const { Title } = Typography;
+	const scriptParent = useRef(null);
+	const tradingviewSettings = {
+		width: "100%",
+		height: 600,
+		colorTheme: "light",
+		isTransparent: false,
+		locale: "in",
+		importanceFilter: "0,1",
+	};
+
+	useEffect(() => {
+		const scriptTag = document.createElement("script");
+
+		scriptTag.type = "text/javascript";
+		scriptTag.src =
+			"https://s3.tradingview.com/external-embedding/embed-widget-events.js";
+		scriptTag.text = JSON.stringify(tradingviewSettings);
+		scriptTag.async = true;
+		/* @ts-ignore */
+		scriptParent.current.appendChild(scriptTag);
+	}, []);
+
+	return (
+		<>
+			<Title level={5}>Economic Calendar</Title>
+			<div ref={scriptParent}></div>
+		</>
+	);
+}
