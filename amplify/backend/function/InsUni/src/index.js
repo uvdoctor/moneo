4f2c429b83b7d49cdd6a7dad0117f1448b58333c
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-02-17 15:47:39 +0530

    docs: updated Insuni function

--- amplify/backend/function/InsUni/src/index.js
+++ amplify/backend/function/InsUni/src/index.js
@@ -1,56 +1,58 @@
-const { getFundamentalData } = require("/opt/nodejs/eod");
 const {
   pushData,
   getTableNameFromInitialWord,
-  getDataFromTable,
   appendGenericFields,
 } = require("/opt/nodejs/insertIntoDB");
-const table = "InsUni";
+const table = "InsUserMap";
 
 const getAndPushData = (data, tableName) => {
   return new Promise(async (resolve, reject) => {
-    const insuniTableData = await getDataFromTable(tableName);
     let batches = [];
     let batchRecords = [];
     let count = 0;
     for (let record of data) {
       try {
         if (record.eventName === "INSERT" || record.eventName === "MODIFY") {
+          const uname = record.dynamodb.Keys.uname.S;
           const newRecords = record.dynamodb.NewImage.ins.L;
           for (let item of newRecords) {
-            console.log(item);
             const id = item.M.id.S;
             const sid = item.M.sid ? item.M.sid.S : null;
-            const exchg = item.M.exchg ? item.M.exchg.S : null;
             const type = item.M.type ? item.M.type.S : null;
             const subt = item.M.subt ? item.M.subt.S : null;
-            const idAlreadyExists = insuniTableData.Items.some(
-              (item) => item.id === id
-            );
-            if (!idAlreadyExists) {
-              let fundamentalData;
-              if (
-                (exchg === "NSE" || exchg === "BSE") &&
-                type === "E" &&
-                subt === "S"
-              ) {
-                fundamentalData = await getFundamentalData(sid, exchg);
-                console.log(fundamentalData);
-              }
-              let schema = {
-                id: id,
-                risk: "M",
-                ana: fundamentalData ? fundamentalData : null,
-              };
-              schema = appendGenericFields(schema, table);
-              console.log(schema);
-              batches.push({ PutRequest: { Item: schema } });
-              count++;
-              if (count === 25) {
-                batchRecords.push(batches);
-                batches = [];
-                count = 0;
-              }
+            let schema = {
+              id: id,
+              sid: sid,
+              type: type,
+              subt: subt,
+              user: uname,
+            };
+            schema = appendGenericFields(schema, table);
+            console.log(schema);
+            batches.push({ PutRequest: { Item: schema } });
+            count++;
+            if (count === 25) {
+              batchRecords.push(batches);
+              batches = [];
+              count = 0;
+            }
+          }
+        }
+        if (record.eventName === "REMOVE") {
+          const uname = record.dynamodb.Keys.uname.S;
+          const oldRecords = record.dynamodb.OldImage.ins.L;
+          for (let item of oldRecords) {
+            const id = item.M.id.S;
+            const schema = {
+              id: id,
+              user: uname,
+            };
+            batches.push({ DeleteRequest: { Key: schema } });
+            count++;
+            if (count === 25) {
+              batchRecords.push(batches);
+              batches = [];
+              count = 0;
             }
           }
         }
@@ -61,6 +63,7 @@ const getAndPushData = (data, tableName) => {
     if (count < 25 && count > 0) {
       batchRecords.push(batches);
     }
+    console.log(batchRecords);
     for (let batch in batchRecords) {
       const results = await pushData(batchRecords[batch], tableName);
       console.log(results);
