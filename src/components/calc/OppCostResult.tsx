59df712a74346b6760f6b9793b8d8b08838b00b3
Moneo <emailumangdoctor@gmail.com>
2022-02-18 18:32:34 +0530

    fixed summary

--- src/components/calc/OppCostResult.tsx
+++ src/components/calc/OppCostResult.tsx
@@ -1,65 +1,79 @@
-import React, { Fragment, useContext } from 'react';
-import { GoalType } from '../../api/goals';
-import SelectInput from '../form/selectinput';
-import { CalcContext } from './CalcContext';
-import ItemDisplay from './ItemDisplay';
-import { initOptions, toHumanFriendlyCurrency } from '../utils';
-import { Row } from 'antd';
-import { PlanContext } from '../goals/PlanContext';
+import React, { Fragment, useContext } from "react";
+import { GoalType } from "../../api/goals";
+import SelectInput from "../form/selectinput";
+import { CalcContext } from "./CalcContext";
+import ItemDisplay from "./ItemDisplay";
+import { initOptions, toHumanFriendlyCurrency } from "../utils";
+import { Row } from "antd";
+import { PlanContext } from "../goals/PlanContext";
 
 interface OppCostResultProps {
-	oppCost: number;
-	numOfYears: number;
-	numOfYearsOptions: any;
-	oppCostHandler: Function;
+  oppCost: number;
+  numOfYears: number;
+  numOfYearsOptions: any;
+  oppCostHandler: Function;
 }
 
-export default function OppCostResult({ oppCost, numOfYears, numOfYearsOptions, oppCostHandler }: OppCostResultProps) {
-	const drOptions = initOptions(1, 14);
-	const { dr, setDR, isPublicCalc }: any = useContext(PlanContext);
-	const { goal, startYear, currency }: any = useContext(CalcContext);
+export default function OppCostResult({
+  oppCost,
+  numOfYears,
+  numOfYearsOptions,
+  oppCostHandler,
+}: OppCostResultProps) {
+  const drOptions = initOptions(1, 14);
+  const { dr, setDR, isPublicCalc }: any = useContext(PlanContext);
+  const { goal, startYear }: any = useContext(CalcContext);
 
-	return (
-		<ItemDisplay
-			result={oppCost}
-			currency={currency}
-			label={
-				<Fragment>
-					{`${goal.type === GoalType.B ? 'Buy' : 'Spend'} v/s Invest`}
-					{isPublicCalc && (
-						<Fragment>
-							{` @`}
-							<SelectInput
-								pre=""
-								value={Math.round(dr)}
-								changeHandler={(val: string) => setDR(parseInt(val))}
-								unit="%"
-								options={drOptions}
-							/>
-						</Fragment>
-					)}
-				</Fragment>
-			}
-			pl
-			unit={
-				goal.type !== GoalType.FF &&
-				isPublicCalc && (
-					<Row align="middle">
-						<SelectInput
-							pre="in"
-							value={numOfYears}
-							unit="years"
-							options={numOfYearsOptions}
-							changeHandler={(val: string) => oppCostHandler(parseInt(val))}
-						/>
-					</Row>
-				)
-			}
-			info={`${dr ? `Given that Investment earns ${dr}% Yearly, ` : ''}You ${oppCost < 0
-				? 'Lose'
-				: 'Gain'} about ${toHumanFriendlyCurrency(Math.abs(oppCost), currency)} in ${numOfYears} Years
-    by ${goal.type === GoalType.B ? 'Buying' : 'Spending'} instead of Investing.
-	${!isPublicCalc ? ` This calculation assumes that cash flows are invested according to the the target asset allocation until ${startYear + numOfYears}. ` : ''}`}
-		/>
-	);
+  return (
+    <ItemDisplay
+      result={oppCost}
+      currency={goal.ccy}
+      label={
+        <Fragment>
+          {`${goal.type === GoalType.B ? "Buy" : "Spend"} v/s Invest`}
+          {isPublicCalc && (
+            <Fragment>
+              {` @`}
+              <SelectInput
+                pre=""
+                value={Math.round(dr)}
+                changeHandler={(val: string) => setDR(parseInt(val))}
+                unit="%"
+                options={drOptions}
+              />
+            </Fragment>
+          )}
+        </Fragment>
+      }
+      pl
+      unit={
+        goal.type !== GoalType.FF &&
+        isPublicCalc && (
+          <Row align="middle">
+            <SelectInput
+              pre="in"
+              value={numOfYears}
+              unit="years"
+              options={numOfYearsOptions}
+              changeHandler={(val: string) => oppCostHandler(parseInt(val))}
+            />
+          </Row>
+        )
+      }
+      info={`${dr ? `Given that Investment earns ${dr}% Yearly, ` : ""}You ${
+        oppCost < 0 ? "Lose" : "Gain"
+      } about ${toHumanFriendlyCurrency(
+        Math.abs(oppCost),
+        goal.ccy
+      )} in ${numOfYears} Years
+    by ${goal.type === GoalType.B ? "Buying" : "Spending"} instead of Investing.
+	${
+    !isPublicCalc
+      ? ` This calculation assumes that cash flows are invested according to the the target asset allocation until ${
+          startYear + numOfYears
+        }. `
+      : ""
+  }`}
+    />
+  );
 }
