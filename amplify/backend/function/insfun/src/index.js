956bf0c13579bc40d9023c33bd1403e7dfd92326
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-02-20 13:05:29 +0530

    docs: updated insfun

--- amplify/backend/function/insfun/src/index.js
+++ amplify/backend/function/insfun/src/index.js
@@ -1,15 +1,74 @@
+const {
+  pushData,
+  pushDataForFeed,
+  getTableNameFromInitialWord,
+} = require("/opt/nodejs/insertIntoDB");
+const { getFundamentalDataByLimit } = require("/opt/nodejs/eod");
+const table = "INExchgFun";
+const isinMap = {};
 
+const organizeData = (data, isinMap, exchg) => {
+  let count = 0;
+  let batches = [];
+  let batchRecords = [];
+  for (let index = 0; index < 500; index++) {
+    const exchgData = data[index];
+    if (exchgData) {
+      const id = exchgData.General.ISIN;
+      if (isinMap[id]) return;
+      const sid = exchgData.General.Code;
+      isinMap[id] = id;
+      const schema = {
+        id: id,
+        sid: sid,
+        exchg: exchg,
+        ana: data[index],
+      };
+      batches.push({ PutRequest: { Item: schema } });
+      count++;
+      if (count === 25) {
+        batchRecords.push(batches);
+        batches = [];
+        count = 0;
+      }
+    }
+  }
+  if (count < 25 && count > 0) {
+    batchRecords.push(batches);
+  }
+  return batchRecords;
+};
+
+const getAndPushData = () => {
+  return new Promise(async (resolve, reject) => {
+    const tableName = await getTableNameFromInitialWord(table);
+    console.log("Table name fetched: ", tableName);
+    const exchgList = ["BSE", "NSE"];
+    const offset = [ 0, 500, 1000, 1500 ];
+    try {
+      for (let exchg of exchgList) {
+        for (let i = 0; i < offset.length; i++) {
+          const { data, url } = await getFundamentalDataByLimit(
+            exchg,
+            offset[i]
+          );
+          console.log("data", data, url);
+          const batches = organizeData(data, isinMap, exchg);
+          console.log(batches);
+          for (let batch in batches) {
+            const result = await pushData(batches[batch], tableName);
+            console.log(result);
+          }
+          await pushDataForFeed(table, batches, exchg, url, exchg);
+        }
+      }
+    } catch (err) {
+      reject(err);
+    }
+    resolve();
+  });
+};
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  return await getAndPushData();
 };
--- amplify/backend/function/moneopricelayer/opt/version.js
+++
@@ -1,1 +0,0 @@
-const version = 12;
