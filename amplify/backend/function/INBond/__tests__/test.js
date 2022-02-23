6011105a0ccea3528ff5ed157884e284ada85b4d
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-02-23 17:58:31 +0530

    docs: updated test

--- amplify/backend/function/INBond/__tests__/test.js
+++ amplify/backend/function/INBond/__tests__/test.js
@@ -119,7 +119,11 @@ describe("Test Asset Instype", () => {
   });
   test("Floating Bond", () => {
     const data = calc.calcInsType("GF");
-    expect(data).toEqual("FR");
+    expect(data).toEqual("FRB");
+  });
+  test("Debenture Bond", () => {
+    const data = calc.calcInsType("DB");
+    expect(data).toEqual("DEB");
   });
   test("Index Bond", () => {
     const data = calc.calcInsType("GI");
