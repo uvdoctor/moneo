8e93c6318fd6d697d4e62707b48eaa43a785db06
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-02-27 11:37:43 +0530

    docs: updated Lambda table

--- amplify/backend/function/alerts/src/index.js
+++ amplify/backend/function/alerts/src/index.js
@@ -1,15 +1,123 @@
+const {
+  batchReadItem,
+  getDataFromTable,
+  getTableNameFromInitialWord
+} = require("/opt/nodejs/insertIntoDB");
+const { sendMail } = require("/opt/nodejs/mailUtils");
+// const {
+//   batchReadItem,
+//   getDataFromTable,
+//   getTableNameFromInitialWord,
+// } = require("../../moneopricelayer/lib/nodejs/insertIntoDB");
+// const { sendMail } = require("../../moneoutilslayer/lib/nodejs/mailUtils");
 
+const isFund = (id) => id.substring(2, 3) === "F";
+
+const processData = () => {
+  return new Promise(async (resolve, reject) => {
+    const insunitableName = await getTableNameFromInitialWord("InsUserMap");
+    console.log(insunitableName);
+    const insuniTableData = await getDataFromTable(insunitableName);
+    let userMap = {};
+    let sidMap = {};
+    insuniTableData.Items.forEach((item) => {
+      if (item.subt === "S" && !isFund(item.id) && item.sid !== "542641") {
+        if (!sidMap[item.sid]) sidMap[item.sid] = item.sid;
+        if (userMap[item.user]) {
+          let sidlist = userMap[item.user];
+          sidlist.push(item.sid);
+          userMap[item.user] = sidlist;
+        } else {
+          userMap[item.user] = [item.sid];
+        }
+      }
+    });
+
+    let sidBatchKeys = [];
+    let sidBatchCount = 0;
+    let sidKeys = [];
+    Object.keys(sidMap).forEach((key) => {
+      sidKeys.push({ id: key });
+      sidBatchCount++;
+      if (sidBatchCount === 100) {
+        sidBatchKeys.push(sidKeys);
+        sidKeys = [];
+        sidBatchCount = 0;
+      }
+    });
+    if (sidBatchCount > 0 && sidBatchCount < 100) {
+      sidBatchKeys.push(sidKeys);
+    }
+    const funtableName = await getTableNameFromInitialWord("INExchgFun");
+    console.log(funtableName);
+    let data = [];
+    for (let batch of sidBatchKeys) {
+      const results = await batchReadItem(funtableName, batch);
+      data = [...results, ...data];
+    }
+
+    for (let item of data) {
+      if (sidMap[item.id]) {
+        sidMap[item.id] = item;
+      }
+    }
+
+    let userBatchKeys = [];
+    let userBatchCount = 0;
+    let userkeys = [];
+    Object.keys(userMap).forEach((key) => {
+      userkeys.push({ uname: key });
+      userBatchCount++;
+      if (userBatchCount === 100) {
+        userBatchKeys.push(userkeys);
+        userkeys = [];
+        userBatchCount = 0;
+      }
+    });
+    if (userBatchCount > 0 && userBatchCount < 100) {
+      userBatchKeys.push(userkeys);
+    }
+    const userInfoTableName = await getTableNameFromInitialWord("UserInfo");
+    let userdata = [];
+    for (let batch of userBatchKeys) {
+      const results = await batchReadItem(userInfoTableName, batch);
+      userdata = [...results, ...userdata];
+    }
+
+    const users = Object.keys(userMap);
+    for (let user of users) {
+      const userInfo = userdata.find((re) => re.uname === user);
+      console.log(userInfo);
+      let content = `<p>Name -  Week High  -  Week Low</p>`;
+      userMap[user].forEach((item) => {
+        const data = sidMap[item];
+        if (!data) return;
+        content += `<p>${data.id} - ${data.tech["52WeekLow"]} - ${data.tech["52WeekHigh"]}</p>`;
+      });
+      try {
+        const template = `<html>
+            <body>
+                <div>
+                    ${content}
+                </div>
+            </body>
+            </html>`;
+        console.log(template);
+        const message = await sendMail(
+          template,
+          "Moneo - Week High Low Digest",
+          ["emailumangdoctor@gmail.com", "mehzabeen1526@gmail.com"],
+          "no-reply@moneo.money"
+        );
+        console.log(message);
+      } catch (err) {
+        reject(err);
+      }
+    }
+    resolve();
+  });
+};
 
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  return await processData();
 };
--- amplify/backend/function/moneopricelayer/lib/nodejs/insertIntoDB.js
+++ amplify/backend/function/moneopricelayer/lib/nodejs/insertIntoDB.js
@@ -1,121 +1,136 @@
-const { DynamoDB } = require('@aws-sdk/client-dynamodb');
+const { DynamoDB } = require("@aws-sdk/client-dynamodb");
 const {
-	DynamoDBDocumentClient,
-	BatchWriteCommand,
-	PutCommand,
-	ScanCommand,
-	UpdateCommand
-} = require('@aws-sdk/lib-dynamodb');
+  DynamoDBDocumentClient,
+  BatchWriteCommand,
+  BatchGetCommand,
+  PutCommand,
+  ScanCommand,
+  UpdateCommand,
+} = require("@aws-sdk/lib-dynamodb");
 const dynamodb = new DynamoDB({});
 const docClient = DynamoDBDocumentClient.from(dynamodb);
 
 const getTableNameFromInitialWord = (tableInitial) => {
-	return new Promise((resolve, reject) => {
-		dynamodb.listTables({}, (err, table) => {
-			if (err) reject(err);
-			const tableName = table.TableNames.find((item) => item.startsWith(tableInitial));
-			resolve(tableName);
-		});
-	});
+  return new Promise((resolve, reject) => {
+    dynamodb.listTables({}, (err, table) => {
+      if (err) reject(err);
+      const tableName = table.TableNames.find((item) =>
+        item.startsWith(tableInitial)
+      );
+      resolve(tableName);
+    });
+  });
+};
+
+const batchReadItem = async (tableName, keys) => {
+  try {
+    const params = { RequestItems: { [tableName]: { Keys: keys } } };
+    const results = await docClient.send(new BatchGetCommand(params));
+    return results.Responses[tableName];
+  } catch (err) {
+    console.log(`Error in dynamoDB: ${JSON.stringify(err)}`);
+    return `Error in dynamoDB: ${JSON.stringify(err)}`;
+  }
 };
 
 const updateItem = async (table, id, fields) => {
-	let exp = { UpdateExpression: 'set', ExpressionAttributeValues: {} };
-	Object.entries(fields).forEach(([ key, item ]) => {
-		const valKey = `:${key}`;
-		exp.UpdateExpression += ` ${key} = ${valKey}, `;
-		exp.ExpressionAttributeValues[valKey] = item;
-	});
-	exp.UpdateExpression = exp.UpdateExpression.slice(0, -1);
-	const params = {
-		TableName: table,
-		Key: { id: id },
-		...exp
-	};
-	try {
-		const data = await docClient.send(new UpdateCommand(params));
-		console.log('Success - item added or updated', data);
-	} catch (err) {
-		console.log('Error', err);
-	}
+  let exp = { UpdateExpression: "set", ExpressionAttributeValues: {} };
+  Object.entries(fields).forEach(([key, item]) => {
+    const valKey = `:${key}`;
+    exp.UpdateExpression += ` ${key} = ${valKey}, `;
+    exp.ExpressionAttributeValues[valKey] = item;
+  });
+  exp.UpdateExpression = exp.UpdateExpression.slice(0, -1);
+  const params = {
+    TableName: table,
+    Key: { id: id },
+    ...exp,
+  };
+  try {
+    const data = await docClient.send(new UpdateCommand(params));
+    console.log("Success - item added or updated", data);
+  } catch (err) {
+    console.log("Error", err);
+  }
 };
 
 const getDataFromTable = async (table) => {
-	const params = { TableName: table };
-	try {
-		const data = await docClient.send(new ScanCommand(params));
-		return data;
-	} catch (err) {
-		console.log('Error in dynamoDB:', err);
-	}
+  const params = { TableName: table };
+  try {
+    const data = await docClient.send(new ScanCommand(params));
+    return data;
+  } catch (err) {
+    console.log("Error in dynamoDB:", err);
+  }
 };
 
 const pushDataSingly = async (schema, tableName) => {
-	const params = { TableName: tableName, Item: schema };
-	try {
-		const data = await docClient.send(new PutCommand(params));
-		return data;
-	} catch (err) {
-		console.log('Error in dynamoDB:', err);
-	}
+  const params = { TableName: tableName, Item: schema };
+  try {
+    const data = await docClient.send(new PutCommand(params));
+    return data;
+  } catch (err) {
+    console.log("Error in dynamoDB:", err);
+  }
 };
 
 const pushData = async (data, table) => {
-	const params = { RequestItems: { [table]: data } };
-	try {
-		const data = await docClient.send(new BatchWriteCommand(params));
-		return data;
-	} catch (err) {
-		console.log('Error in dynamoDB:', err);
-	}
+  const params = { RequestItems: { [table]: data } };
+  try {
+    const data = await docClient.send(new BatchWriteCommand(params));
+    return data;
+  } catch (err) {
+    console.log("Error in dynamoDB:", err);
+  }
 };
 
 const appendGenericFields = (schema, tableName) => {
-	let dateStr = new Date().toISOString();
-	schema.createdAt = dateStr;
-	schema.updatedAt = dateStr;
-	schema.__typename = tableName;
-	return schema;
+  let dateStr = new Date().toISOString();
+  schema.createdAt = dateStr;
+  schema.updatedAt = dateStr;
+  schema.__typename = tableName;
+  return schema;
 };
 
 const pushDataForFeed = async (table, data, identifier, url, exchg) => {
-	if (!identifier) identifier = '';
-	const tableName = await getTableNameFromInitialWord('Feeds');
-	const getLength = (arr) => {
-		if (typeof arr === 'number') return arr;
-		const len = arr.flat(Infinity);
-		return len.length;
-	};
-	let schema = {
-		id: `${table}_${identifier}`,
-		tname: table,
-		count: getLength(data)
-	};
-	schema = appendGenericFields(schema, tableName);
-	console.log('Feed schema to be inserted: ', schema);
-	if (exchg) schema.exchg = exchg;
-	if (url) schema.url = url;
-	const results = await pushDataSingly(schema, tableName);
-	console.log(results, 'Data Pushed into Feeds Table');
+  if (!identifier) identifier = "";
+  const tableName = await getTableNameFromInitialWord("Feeds");
+  const getLength = (arr) => {
+    if (typeof arr === "number") return arr;
+    const len = arr.flat(Infinity);
+    return len.length;
+  };
+  let schema = {
+    id: `${table}_${identifier}`,
+    tname: table,
+    count: getLength(data),
+  };
+  schema = appendGenericFields(schema, tableName);
+  console.log("Feed schema to be inserted: ", schema);
+  if (exchg) schema.exchg = exchg;
+  if (url) schema.url = url;
+  const results = await pushDataSingly(schema, tableName);
+  console.log(results, "Data Pushed into Feeds Table");
 };
 
 const deleteData = async (data, table) => {
-	const params = { RequestItems: { [table]: data } };
-	try {
-		const data = await docClient.send(new BatchWriteCommand(params));
-		return data;
-	} catch (error) {
-		console.log('Error in dynamoDB:', err);
-	}
+  const params = { RequestItems: { [table]: data } };
+  try {
+    const data = await docClient.send(new BatchWriteCommand(params));
+    return data;
+  } catch (error) {
+    console.log("Error in dynamoDB:", err);
+  }
 };
 
 module.exports = {
-	getDataFromTable,
-	pushData,
-	pushDataForFeed,
-	pushDataSingly,
-	appendGenericFields,
-	getTableNameFromInitialWord,
-	deleteData,
-	updateItem
+  getDataFromTable,
+  pushData,
+  pushDataForFeed,
+  pushDataSingly,
+  appendGenericFields,
+  getTableNameFromInitialWord,
+  deleteData,
+  updateItem,
+  batchReadItem,
 };
