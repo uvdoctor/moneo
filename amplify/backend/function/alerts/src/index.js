84cf9ba50a1672dc7c3dca080acd80ba5f578556
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-02-28 16:33:18 +0530

    docs: updated alerts function

--- amplify/backend/function/alerts/src/index.js
+++ amplify/backend/function/alerts/src/index.js
@@ -1,123 +1,123 @@
+const fs = require("fs");
+const fsPromise = require("fs/promises");
 const {
+  getDataByFilter,
   batchReadItem,
-  getDataFromTable,
-  getTableNameFromInitialWord
+  getTableNameFromInitialWord,
 } = require("/opt/nodejs/insertIntoDB");
 const { sendMail } = require("/opt/nodejs/mailUtils");
+const { tempDir } = require("/opt/nodejs/utility");
+const { cleanDirectory, downloadZip } = require("/opt/nodejs/bhavUtils");
+const constructedApiArray = require("./utils");
+const extractDataFromCSV = require("./bhavUtils");
+const { mkdir } = fsPromise;
 // const {
+//   getDataByFilter,
 //   batchReadItem,
-//   getDataFromTable,
 //   getTableNameFromInitialWord,
 // } = require("../../moneopricelayer/lib/nodejs/insertIntoDB");
+// const { tempDir } = require("../../moneopricelayer/lib/nodejs/utility");
+// const {
+//   cleanDirectory,
+//   downloadZip,
+// } = require("../../moneopricelayer/lib/nodejs/bhavUtils");
 // const { sendMail } = require("../../moneoutilslayer/lib/nodejs/mailUtils");
 
-const isFund = (id) => id.substring(2, 3) === "F";
-
-const processData = () => {
+const sidMap = {};
+const processData = (diff) => {
   return new Promise(async (resolve, reject) => {
-    const insunitableName = await getTableNameFromInitialWord("InsUserMap");
-    console.log(insunitableName);
-    const insuniTableData = await getDataFromTable(insunitableName);
-    let userMap = {};
-    let sidMap = {};
-    insuniTableData.Items.forEach((item) => {
-      if (item.subt === "S" && !isFund(item.id) && item.sid !== "542641") {
-        if (!sidMap[item.sid]) sidMap[item.sid] = item.sid;
+    const { apiArray } = constructedApiArray(diff);
+    try {
+      if (fs.existsSync(tempDir)) {
+        await cleanDirectory(tempDir, "Initial cleaning completed");
+      }
+      for (let i = 0; i < apiArray.length; i++) {
+        const { fileName, url, codes } = apiArray[i];
+        console.log(url);
+        const csvFile = `${tempDir}/${fileName}`;
+        await mkdir(tempDir);
+        await downloadZip(url, tempDir, csvFile);
+        await extractDataFromCSV(fileName, codes, sidMap);
+      }
+
+      const insunitableName = await getTableNameFromInitialWord("InsUserMap");
+      console.log(insunitableName);
+      const sidkeys = Object.keys(sidMap);
+      const results = await getDataByFilter(insunitableName, sidkeys, "sid");
+
+      let userMap = {};
+      results.forEach((item) => {
         if (userMap[item.user]) {
-          let sidlist = userMap[item.user];
-          sidlist.push(item.sid);
-          userMap[item.user] = sidlist;
+          userMap[item.user] = { ...userMap[item.user], ...item.sid };
         } else {
           userMap[item.user] = [item.sid];
         }
-      }
-    });
-
-    let sidBatchKeys = [];
-    let sidBatchCount = 0;
-    let sidKeys = [];
-    Object.keys(sidMap).forEach((key) => {
-      sidKeys.push({ id: key });
-      sidBatchCount++;
-      if (sidBatchCount === 100) {
-        sidBatchKeys.push(sidKeys);
-        sidKeys = [];
-        sidBatchCount = 0;
-      }
-    });
-    if (sidBatchCount > 0 && sidBatchCount < 100) {
-      sidBatchKeys.push(sidKeys);
-    }
-    const funtableName = await getTableNameFromInitialWord("INExchgFun");
-    console.log(funtableName);
-    let data = [];
-    for (let batch of sidBatchKeys) {
-      const results = await batchReadItem(funtableName, batch);
-      data = [...results, ...data];
-    }
-
-    for (let item of data) {
-      if (sidMap[item.id]) {
-        sidMap[item.id] = item;
-      }
-    }
-
-    let userBatchKeys = [];
-    let userBatchCount = 0;
-    let userkeys = [];
-    Object.keys(userMap).forEach((key) => {
-      userkeys.push({ uname: key });
-      userBatchCount++;
-      if (userBatchCount === 100) {
-        userBatchKeys.push(userkeys);
-        userkeys = [];
-        userBatchCount = 0;
-      }
-    });
-    if (userBatchCount > 0 && userBatchCount < 100) {
-      userBatchKeys.push(userkeys);
-    }
-    const userInfoTableName = await getTableNameFromInitialWord("UserInfo");
-    let userdata = [];
-    for (let batch of userBatchKeys) {
-      const results = await batchReadItem(userInfoTableName, batch);
-      userdata = [...results, ...userdata];
-    }
-
-    const users = Object.keys(userMap);
-    for (let user of users) {
-      const userInfo = userdata.find((re) => re.uname === user);
-      console.log(userInfo);
-      let content = `<p>Name -  Week High  -  Week Low</p>`;
-      userMap[user].forEach((item) => {
-        const data = sidMap[item];
-        if (!data) return;
-        content += `<p>${data.id} - ${data.tech["52WeekLow"]} - ${data.tech["52WeekHigh"]}</p>`;
       });
-      try {
-        const template = `<html>
-            <body>
-                <div>
-                    ${content}
-                </div>
-            </body>
-            </html>`;
-        console.log(template);
-        const message = await sendMail(
-          template,
-          "Moneo - Week High Low Digest",
-          ["emailumangdoctor@gmail.com", "mehzabeen1526@gmail.com"],
-          "no-reply@moneo.money"
-        );
-        console.log(message);
-      } catch (err) {
-        reject(err);
+
+      let userBatchKeys = [];
+      let userBatchCount = 0;
+      let userkeys = [];
+      Object.keys(userMap).forEach((key) => {
+        userkeys.push({ uname: key });
+        userBatchCount++;
+        if (userBatchCount === 100) {
+          userBatchKeys.push(userkeys);
+          userkeys = [];
+          userBatchCount = 0;
+        }
+      });
+      if (userBatchCount > 0 && userBatchCount < 100) {
+        userBatchKeys.push(userkeys);
       }
+      const userInfoTableName = await getTableNameFromInitialWord("UserInfo");
+      let userdata = [];
+      for (let batch of userBatchKeys) {
+        const results = await batchReadItem(userInfoTableName, batch);
+        userdata = [...results, ...userdata];
+      }
+
+      const users = Object.keys(userMap);
+      for (let user of users) {
+        const userInfo = userdata.find((re) => re.uname === user);
+        console.log(userInfo);
+        let content = "";
+        userMap[user].forEach((item) => {
+          const data = sidMap[item];
+          if (!data) return;
+          content += `<p>${item} ${data.yhigh ? "- Week High -" : ""} ${
+            data.yhigh ? data.yhigh : ""
+          } ${data.ylow ? "- Week Low -" : ""} ${
+            data.ylow ? data.ylow : ""
+          } </p></br>`;
+        });
+        if (!content) continue;
+        try {
+          const template = `<html>
+              <body>
+                  <div>
+                      ${content}
+                  </div>
+              </body>
+              </html>`;
+          console.log(template);
+          const message = await sendMail(
+            template,
+            "Moneo - Week High Low Digest",
+            ["emailumangdoctor@gmail.com", "mehzabeen1526@gmail.com"],
+            "no-reply@moneo.money"
+          );
+          console.log(message);
+        } catch (err) {
+          reject(err);
+        }
+      }
+    } catch (err) {
+      reject(err);
     }
     resolve();
   });
 };
 
 exports.handler = async (event) => {
-  return await processData();
+  return await processData(event.diff);
 };
