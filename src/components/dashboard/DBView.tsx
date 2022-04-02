c601ced6f0d8c0f7b2c4db96a671f64c1b5b8c08
Moneo <emailumangdoctor@gmail.com>
2022-04-02 12:24:53 +0530

    fixed

--- src/components/dashboard/DBView.tsx
+++ src/components/dashboard/DBView.tsx
@@ -8,7 +8,6 @@ import EconomicCalendar from "./EconomicCalendar";
 import StockMarket from "./StockMarket";
 import MarketOverview from "./MarketOverview";
 import InvestmentAlerts from "./InvestmentAlerts";
-import Link from "next/link";
 
 require("./DBView.less");
 
