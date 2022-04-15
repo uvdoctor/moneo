f18d3cfdf25e3ce579af5bc1d5c48200fa6c3af8
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-04-15 12:45:31 +0530

    docs: updated getCommodityRate

--- src/components/nw/nwutils.tsx
+++ src/components/nw/nwutils.tsx
@@ -513,7 +513,7 @@ export const getCommodityRate = async (
     .then((result) => {
       if (!result || isNaN(result)) return 0;
       let rate = Number((result / 31.1).toFixed(2));
-      if(subtype === APIt.AssetSubType.Gold || subtype === "SI") rate = rate * 1.1;
+      if(subtype === APIt.AssetSubType.Gold || subtype === "SI") rate = rate * 1.11;
       return (
         rate *
         ((getFXRate(fxRates, currency) * Number.parseFloat(purity)) /
