8b1fdb23cff6665f827b7a7a537b38e8b6faecd3
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-04-17 20:02:07 +0530

    docs: updated sendalerts

--- amplify/backend/function/sendalert/src/index.js
+++ amplify/backend/function/sendalert/src/index.js
@@ -1,10 +1,24 @@
-const { sendEmail, PRICE_TEMPLATE_NAME, WATCH_TEMPLATE_NAME } = require("/opt/nodejs/sendMail/EmailSender");
+const {
+  sendEmail,
+  PRICE_TEMPLATE_NAME,
+  WATCH_TEMPLATE_NAME,
+} = require("/opt/nodejs/sendMail/EmailSender");
 const { deleteMessage } = require("/opt/nodejs/sqsUtils");
 
-const processData = (queue, templateName, records) => {
+const processData = (records) => {
   return new Promise(async (resolve, reject) => {
     for (let item of records) {
       let queueData = JSON.parse(item.body);
+      const queueARN = item.eventSourceARN;
+      let queue = "";
+      let templateName = "";
+      if (queueARN.includes("price")) {
+        queue = PRICE_ALERTS_QUEUE;
+        templateName = PRICE_TEMPLATE_NAME;
+      } else {
+        queue = WATCH_ALERTS_QUEUE;
+        templateName = WATCH_TEMPLATE_NAME;
+      }
       try {
         const message = await sendEmail({
           templateName: templateName,
@@ -22,15 +36,5 @@ const processData = (queue, templateName, records) => {
 };
 
 exports.handler = async (event) => {
-  const queueARN = event.eventSourceARN;
-  let queue = "";
-  let templateName = "";
-  if (queueARN.contains("price")) {
-    queue = PRICE_ALERTS_QUEUE;
-    templateName = PRICE_TEMPLATE_NAME;
-  } else {
-    queue = WATCH_ALERTS_QUEUE;
-    templateName = WATCH_TEMPLATE_NAME;
-  }
-  return await processData(queue, template, event.Records);
+  return await processData(event.Records);
 };
