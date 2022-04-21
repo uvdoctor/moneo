9e5ced0071e49498e6b093f994618ed3106db3dd
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-04-21 13:31:02 +0530

    docs: updated search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -23,18 +23,18 @@ export default function Search({
   const { BOND, MF, ETF, GOLDB, REIT, OIT, CRYPTO, STOCK } = TAB;
   const [exchange, setExchange] = useState("NSE");
   const [searchText, setSearchText] = useState("");
-  const [searchResults, setSearchResults] = useState([
-    {
-      Code: "SBIN",
-      Exchange: "NSE",
-      Name: "State Bank of India",
-      Type: "Common Stock",
-      Country: "India",
-      Currency: "INR",
-      ISIN: "INE062A01020",
-      previousClose: 529.6,
-      previousCloseDate: "2022-02-11",
-    },
+  const [searchResults, setSearchResults] = useState<Array<any>>([
+    // {
+    //   Code: "SBIN",
+    //   Exchange: "NSE",
+    //   Name: "State Bank of India",
+    //   Type: "Common Stock",
+    //   Country: "India",
+    //   Currency: "INR",
+    //   ISIN: "INE062A01020",
+    //   previousClose: 529.6,
+    //   previousCloseDate: "2022-02-11",
+    // },
   ]);
 
   const hasOnlyIndiaIns = (tab: string) => [GOLDB, REIT, OIT, "index"].includes(tab);
