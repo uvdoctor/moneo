34ad1aa87e050a562a40443c7871d0dd898c8627
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-05-11 10:43:28 +0530

    updated holdings

--- src/components/nw/PerfHistFeedback.tsx
+++ src/components/nw/PerfHistFeedback.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Rate } from "antd";
-import { calculateDiffPercent } from "./valuationutils";
 import simpleStorage from "simplestorage.js";
 import { LOCAL_INDEX_PERF_KEY } from "../../CONSTANTS";
 
@@ -16,20 +15,23 @@ export default function PerfHistFeedback({
   const indexPerf = simpleStorage.get(LOCAL_INDEX_PERF_KEY);
   const [star, setStar] = useState<number>(1);
 
+  const calcDiff = (curr: number, prev: number, years: number) => {
+    return (curr - prev) / years;
+  };
+
   useEffect(() => {
     let star = 1;
-    const diffOne = calculateDiffPercent(instrument?.price, performance.p1y);
-    const diffThree = calculateDiffPercent(instrument?.price, performance.p3y);
-    const diffFive = calculateDiffPercent(instrument?.price, performance.p5y);
+    const diffOne = calcDiff(instrument?.price, performance.p1y, 1);
+    const diffThree = calcDiff(instrument?.price, performance.p3y, 3);
+    const diffFive = calcDiff(instrument?.price, performance.p5y, 5);
     const indexDiffOne = indexPerf && indexPerf["NIFTY 50"]?.p1y;
     const indexDiffThree = indexPerf && indexPerf["NIFTY 50"]?.p3y;
-    const indexDiffFive =indexPerf && indexPerf["NIFTY 50"]?.p5y;
-    if(diffOne > indexDiffOne) star += 2;
-    if(diffThree > indexDiffThree) star += 1;
-    if(diffFive > indexDiffFive) star += 1;
+    const indexDiffFive = indexPerf && indexPerf["NIFTY 50"]?.p5y;
+    if (diffOne > indexDiffOne) star += 2;
+    if (diffThree > indexDiffThree) star += 1;
+    if (diffFive > indexDiffFive) star += 1;
     setStar(star);
   }, []);
-
 
   return <Rate value={star} />;
 }
