4789de1d9e91ecdc2ebbde1669f5b6745c42128c
Moneo <emailumangdoctor@gmail.com>
2022-05-14 12:05:04 +0530

    updated perf hist feedback

--- src/components/nw/PerfHistFeedback.tsx
+++ src/components/nw/PerfHistFeedback.tsx
@@ -1,48 +1,42 @@
-import React, { useContext, useEffect, useState } from "react";
+import React from "react";
 import { Rate, Tooltip } from "antd";
 import simpleStorage from "simplestorage.js";
-import { LOCAL_INDEX_PERF_KEY } from "../../CONSTANTS";
-import { initializeFundata, isStock } from "./nwutils";
-import { NWContext } from "./NWContext";
+import { LOCAL_FUN_DATA_KEY, LOCAL_INDEX_PERF_KEY } from "../../CONSTANTS";
 
 interface PerfHistFeedbackProps {
   instrument: any;
   performance: any;
 }
 
+const getIndexBySector = (ind: string) => {
+  const data: { [key: string]: string } = {
+    "Basic Materials": "NIFTY Metal",
+    "Consumer Cyclical": "NIFTY India Consumption",
+    "Consumer Defensive": "NIFTY Consumer Durables",
+    "Consumer Goods": "NIFTY FMCG",
+    Financial: "NIFTY Financial Services",
+    "Financial Services": "NIFTY Financial Services",
+    Services: "NIFTY Services Sector",
+    Technology: "NIFTY IT",
+    Utilities: "NIFTY Oil & Gas",
+    HealthCare: "NIFTY Healthcare Index",
+    Energy: "NIFTY Energy",
+    "Communication Services": "NIFTY Media",
+    "Real Estate": "NIFTY Realty",
+  };
+  return data[ind] ? data[ind] : null;
+};
+
+const calcDiff = (curr: number, prev: number, years: number) =>
+  (curr - prev) / years;
+
 export default function PerfHistFeedback({
   instrument,
   performance,
 }: PerfHistFeedbackProps) {
-  const { instruments }: any = useContext(NWContext);
   const indexPerf = simpleStorage.get(LOCAL_INDEX_PERF_KEY);
-  const [niftyRating, setNiftyRating] = useState<number>(1);
-  const [sectorRating, setSectorRating] = useState<number>(1);
 
-  const getIndexBySector = (ind: string) => {
-    const data: { [key: string]: string } = {
-      "Basic Materials": "NIFTY Metal",
-      "Consumer Cyclical": "NIFTY India Consumption",
-      "Consumer Defensive": "NIFTY Consumer Durables",
-      "Consumer Goods": "NIFTY FMCG",
-      Financial: "NIFTY Financial Services",
-      "Financial Services": "NIFTY Financial Services",
-      Services: "NIFTY Services Sector",
-      Technology: "NIFTY IT",
-      Utilities: "NIFTY Oil & Gas",
-      HealthCare: "NIFTY Healthcare Index",
-      Energy: "NIFTY Energy",
-      "Communication Services": "NIFTY Media",
-      "Real Estate": "NIFTY Realty",
-    };
-    return data[ind] ? data[ind] : null;
-  };
-
-  const calcDiff = (curr: number, prev: number, years: number) => {
-    return (curr - prev) / years;
-  };
-
-  const setRating = async () => {
+  const calculateOverallRating = () => {
     let niftyRating = 1;
     const diffOne = calcDiff(instrument?.price, performance.p1y, 1);
     const diffThree = calcDiff(instrument?.price, performance.p3y, 3);
@@ -53,46 +47,47 @@ export default function PerfHistFeedback({
     if (diffOne > indexDiffOne) niftyRating += 2;
     if (diffThree > indexDiffThree) niftyRating += 1;
     if (diffFive > indexDiffFive) niftyRating += 1;
-    setNiftyRating(niftyRating);
+    return niftyRating;
+  };
 
-    let sectorRating = 1;
-    let fundata = await initializeFundata(instruments);
+  const calculateSectorRating = () => {
+    let fundata = simpleStorage.get(LOCAL_FUN_DATA_KEY);
     let data =
       fundata && fundata[instrument.sid as string]
         ? fundata[instrument.sid as string]
         : null;
-    if (!data) return;
+    if (!data) return 0;
     const sectorIndex = getIndexBySector(data.sector);
-    if (!sectorIndex) return;
+    if (!sectorIndex) return 0;
+    let sectorRating = 1;
+    const diffOne = calcDiff(instrument?.price, performance.p1y, 1);
+    const diffThree = calcDiff(instrument?.price, performance.p3y, 3);
+    const diffFive = calcDiff(instrument?.price, performance.p5y, 5);
     const sectorDiffOne = indexPerf && indexPerf[sectorIndex]?.p1y;
     const setorDiffThree = indexPerf && indexPerf[sectorIndex]?.p3y;
     const sectorDiffFive = indexPerf && indexPerf[sectorIndex]?.p5y;
     if (diffOne > sectorDiffOne) sectorRating += 2;
     if (diffThree > setorDiffThree) sectorRating += 1;
     if (diffFive > sectorDiffFive) sectorRating += 1;
-    setSectorRating(sectorRating);
+    return sectorRating;
   };
 
-  useEffect(() => {
-    setRating();
-  }, []);
+  const niftyRating = calculateOverallRating();
+  const sectorRating = calculateSectorRating();
 
   return (
     <>
       <Tooltip title="Performance consistency compared to NIFTY 50 index over last 5 years">
-        Market performance consistency&nbsp;&nbsp;
+        Market rating&nbsp;&nbsp;
         <Rate value={niftyRating} disabled />
       </Tooltip>
       &nbsp;&nbsp;
-      {instrument?.itype !== "ETF" &&
-      isStock(instrument.subt, instrument.id) ? (
+      {sectorRating ? (
         <Tooltip title="Performance consistency compared to the index of respective sector over last 5 years">
-          Sector performance consistency&nbsp;&nbsp;
+          Sector rating&nbsp;&nbsp;
           <Rate value={sectorRating} disabled />
         </Tooltip>
-      ) : (
-        <></>
-      )}
+      ) : null}
     </>
   );
 }
