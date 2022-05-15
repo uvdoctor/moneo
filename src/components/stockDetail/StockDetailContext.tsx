ab310f8ea48aa72d2f853357d437a1f9cd5fd4f6
Moneo <emailumangdoctor@gmail.com>
2022-05-15 22:47:35 +0530

    updated stock detail with price

--- src/components/stockDetail/StockDetailContext.tsx
+++ src/components/stockDetail/StockDetailContext.tsx
@@ -1,4 +1,4 @@
-import { Col, PageHeader, Row, Tooltip } from "antd";
+import { Col, PageHeader, Row, Spin, Tooltip } from "antd";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import CoachingRequest from "../CoachingRequest";
 import useFetch from "../useFetch";
@@ -6,6 +6,9 @@ import { ShoppingCartOutlined } from "@ant-design/icons";
 import { AppContext } from "../AppContext";
 import { loadInsHoldings } from "../nw/nwutils";
 import { InstrumentInput } from "../../api/goals";
+import { loadInstruments } from "../nw/valuationutils";
+import YearlyLowHigh from "../nw/YearlyLowHigh";
+
 const StockDetailContext = createContext({});
 
 function StockDetailContextProvider({ name, children }: any) {
@@ -13,12 +16,30 @@ function StockDetailContextProvider({ name, children }: any) {
   const [state, dispatch, loadData] = useFetch(`/api/details?name=${name}`);
   const { owner }: any = useContext(AppContext);
   const [quantity, setQuantity] = useState<number>(0);
+  const [instrument, setInstrument] = useState<any | null>(null);
+  const [currency, setCurrency] = useState<string>("");
   const ticker = name && name.split(".")?.length ? name.split(".")[0] : name;
 
   useEffect(() => {
     if (!name) return;
     loadData();
   }, [name]);
+
+  useEffect(() => {
+    if (state.isLoading) {
+      setInstrument(null);
+      setCurrency("");
+      return;
+    }
+    const isin = state.data?.General?.ISIN;
+    const currency = state.data?.General?.CurrencyCode;
+    if (!isin && !currency) return;
+    setCurrency(currency);
+    loadInstruments([isin]).then((allInsData: any) => {
+      if (allInsData[isin]) setInstrument(allInsData[isin]);
+      console.log(allInsData[isin]);
+    });
+  }, [state.isLoading]);
 
   useEffect(() => {
     if (!owner) {
@@ -54,7 +75,18 @@ function StockDetailContextProvider({ name, children }: any) {
             </Col>
             <Col span={24} className="secondary-header">
               <Row justify="space-between">
-                <Col>Price</Col>
+                <Col xs={20} sm={12} md={6}>
+                  {instrument && currency ? (
+                    <YearlyLowHigh
+                      instrument={instrument}
+                      currency={currency}
+                      price={instrument.price}
+                      previousPrice={instrument.prev}
+                    />
+                  ) : (
+                    <Spin />
+                  )}
+                </Col>
                 <Col>
                   <Tooltip title={`Total quantity held is ${quantity}`}>
                     <ShoppingCartOutlined />
