1c082ea6bd89d8f661a8b5d502c446c79c03677b
Mehzabeen Choudhari <76725207+mehzabeen000@users.noreply.github.com>
2022-05-31 09:21:04 +0530

    updated layers

--- amplify/backend/function/INBond/src/bhavUtils.js
+++ amplify/backend/function/INBond/src/bhavUtils.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 const csv = require("csv-parser");
-// const { tempDir } = require("/opt/nodejs/utility");
-// const { cleanDirectory } = require("/opt/nodejs/downloadUtils");
+const { tempDir } = require("/opt/nodejs/utility");
+const { cleanDirectory } = require("/opt/nodejs/downloadUtils");
 const { calcSchema } = require("./calculate");
-const { tempDir } = require("../../moneoutilslayer/lib/nodejs/utility");
-const {
-  cleanDirectory,
-} = require("../../moneoutilslayer/lib/nodejs/downloadUtils");
+// const { tempDir } = require("../../moneoutilslayer/lib/nodejs/utility");
+// const {
+  // cleanDirectory,
+// } = require("../../moneoutilslayer/lib/nodejs/downloadUtils");
 
 const extractDataFromCSV = async (
   fileName,
--- amplify/backend/function/INBond/src/calculate.js
+++ amplify/backend/function/INBond/src/calculate.js
@@ -1,4 +1,5 @@
-const { appendGenericFields } = require("../../moneoutilslayer/lib/nodejs/utility");
+const { appendGenericFields } = require("/opt/nodejs/utility");
+// const { appendGenericFields } = require("../../moneoutilslayer/lib/nodejs/utility");
 
 const calc = {
   calcSubType: (subt) => {
