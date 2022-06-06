12d5e94009b39159a647116a801882089d3f7ca3
RSRawat <ravinder.singh.rawat2008@gmail.com>
2022-06-06 10:45:01 +0530

    updated test cases

--- amplify/backend/function/moneoutilslayer/__tests__/sqsUtils.test.js
+++ amplify/backend/function/moneoutilslayer/__tests__/sqsUtils.test.js
@@ -1,36 +1,37 @@
-const { sendMessage, receiveMessage, deleteMessage } = require("../lib/nodejs/sqsUtils");
-const  {
-    SQSClient,
-    SendMessageCommand,
-    ReceiveMessageCommand,
-    DeleteMessageCommand,
-  } = require("@aws-sdk/client-sqs");
+const {
+  sendMessage,
+  receiveMessage,
+  deleteMessage,
+} = require("../lib/nodejs/sqsUtils");
+const {
+  SQSClient,
+  SendMessageCommand,
+  ReceiveMessageCommand,
+  DeleteMessageCommand,
+} = require("@aws-sdk/client-sqs");
+
 jest.mock("@aws-sdk/client-sqs");
 
-// const client = new SQSClient({ apiVersion: "2012-11-05" });
-
-// const sendMessage = async (data, queueUrl) => {
-//   try {
-//     const params = {
-//       MessageBody: JSON.stringify(data),
-//       QueueUrl: queueUrl,
-//     };
-//     console.log(params);
-//     const result = await client.send(new SendMessageCommand(params));
-//     console.log(result);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 describe("Test Divide array by size", () => {
-  test("", async() => {
-    // SQSClient.mockReturnValue(true);
-    // client.mockReturnValue(true);
-    await sendMessage("shdhasd", "url")
+  test("should call client send function", async () => {
+    await sendMessage("shdhasd", "url");
 
-    expect(SQSClient).toHaveBeenCalled()  
-    // expect(client.SendMessageCommand).toHaveBeenCalled()  
+    expect(SQSClient.prototype.send).toHaveBeenCalled();
   });
-  
+
+  test("should call SendMessageCommand with given params", async () => {
+    const name = "Moneo";
+    const queueUrl = "/testing";
+    const params = {
+      MessageBody: name,
+      QueueUrl: queueUrl,
+    };
+
+    await sendMessage(name, queueUrl);
+
+    const sendMessageCommandParams =
+      SendMessageCommand.prototype.constructor.mock.calls[1];
+
+    expect(sendMessageCommandParams[0].QueueUrl).toEqual(queueUrl);
+  });
 });
--- jest.amplify.config.js
+++ jest.amplify.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   //"roots": ["<rootDir>/amplify"],
-  "roots": ["<rootDir>/amplify/backend/function/INBond/__tests__"],
-  "moduleDirectories": ["node_modules", "amplify/backend/function/INBound/src/node_modules"],
-  "testRegex": "(/*.test.js?$)",
+  "roots": ["<rootDir>/amplify/backend/function/moneoutilslayer/__tests__"],
+  "moduleDirectories": ["node_modules", "amplify/backend/function/INBound/src/node_modules", "amplify/backend/function/moneoutilslayer/lib/nodejs/node_modules"],
+  "testRegex": "(/sqsUtils.test.js?$)",
 };
